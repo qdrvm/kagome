d9d53f1cc7daf88dfac290bcde77c2bb94bd7817
Bogdan Vaneev <warchantua@gmail.com>
2019-02-20 16:48:01 +0200

    Add docs
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- core/common/buffer.hpp
+++ core/common/buffer.hpp
@@ -15,19 +15,18 @@ namespace kagome::common {
    * @brief Class represents arbitrary (including empty) byte buffer.
    */
   class Buffer {
-   private:
-
-
    public:
     using iterator = std::vector<uint8_t>::iterator;
     using const_iterator = std::vector<uint8_t>::const_iterator;
 
     /**
-     * @brief
-     * @param size
-     * @param byte
+     * @brief allocates buffer of size={@param size}, filled with {@param byte}
      */
     explicit Buffer(size_t size, uint8_t byte);
+
+    /**
+     * @brief construct buffer from a byte vector
+     */
     explicit Buffer(const std::vector<uint8_t> &v);
 
     Buffer() = default;
@@ -35,33 +34,107 @@ namespace kagome::common {
     Buffer(Buffer &&b) noexcept = default;
     Buffer(std::initializer_list<uint8_t> b);
 
+    /**
+     * @brief Accessor of byte elements given {@param index} in bytearray
+     */
     const uint8_t operator[](size_t index) const;
+
+    /**
+     * @brief Accessor of byte elements given {@param index} in bytearray
+     */
     uint8_t &operator[](size_t index);
+
+    /**
+     * @brief Lexicographical comparison of two buffers
+     */
     bool operator==(const Buffer &b) const noexcept;
+
+    /**
+     * @brief Lexicographical comparison of buffer and vector of bytes
+     */
     bool operator==(const std::vector<uint8_t> &b) const noexcept;
 
+    /**
+     * @brief Iterator, which points to begin of this buffer.
+     */
     iterator begin();
+
+    /**
+     * @brief Iterator, which points to the element next to the last in this
+     * buffer.
+     */
     iterator end();
 
+    /**
+     * @brief Iterator, which points to begin of this buffer.
+     */
     const_iterator begin() const;
+
+    /**
+     * @brief Iterator, which points to the element next to the last in this
+     * buffer.
+     */
     const_iterator end() const;
 
+    /**
+     * @brief Getter for size of this buffer.
+     */
     size_t size() const;
-    Buffer &put_uint8(uint8_t n);
+
+    /**
+     * @brief Put a 8-bit {@param byte} in this buffer.
+     * @return this buffer, suitable for chaining.
+     */
+    Buffer &put_uint8(uint8_t byte);
+
+    /**
+     * @brief Put a 32-bit {@param n} number in this buffer. Will be serialized
+     * as big-endian number.
+     * @return this buffer, suitable for chaining.
+     */
     Buffer &put_uint32(uint32_t n);
+
+    /**
+     * @brief Put a 64-bit {@param n} number in this buffer. Will be serialized
+     * as big-endian number.
+     * @return this buffer, suitable for chaining.
+     */
     Buffer &put_uint64(uint64_t n);
 
+    /**
+     * @brief Put any container which consists of bytes number in this buffer. Will be serialized
+     * as big-endian number.
+     * @return this buffer, suitable for chaining.
+     */
+     // TODO(@warchant): add compile check that iterator_type is uint8_t
     template <typename T>
     Buffer &put_bytes(const T &begin, const T &end) {
       data_.insert(std::end(data_), begin, end);
       return *this;
     }
 
+    /**
+     * @brief getter for raw array of bytes
+     */
     const uint8_t *to_bytes() const;
 
+    /**
+     * @brief getter for vector of vytes
+     */
     const std::vector<uint8_t> &to_vector() const;
 
+    /**
+     * @brief encode bytearray as hex
+     * @return hexencoded string
+     */
     const std::string to_hex() const;
+
+    /**
+     * @brief Construct Buffer from hexstring
+     * @param hex hexencoded string
+     * @return constructed buffer
+     * @throws boost::algorithm::hex_decode_error if input string is not hex
+     */
     static Buffer from_hex(const std::string &hex);
 
    private:
