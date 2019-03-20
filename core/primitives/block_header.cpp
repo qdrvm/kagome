af9ff34fdecd83a5a3a846f6aac5aa600a4bcb1b
Yura Zarudniy <yura.zarudny@gmail.com>
2019-03-20 20:17:09 +0300

    pre-48 create primitives interfaces implemented (#43)
    * pre-48 create primitives interfaces implemented
    
    * mistake getter not by ref fixed
    
    * digest made buffer, not vector of buffers, some renames made, comments refined
    
    * getter number() definition moved to cpp

---
+++ core/primitives/block_header.cpp
@@ -0,0 +1,37 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "primitives/block_header.hpp"
+
+namespace kagome::primitives {
+
+  BlockHeader::BlockHeader(Buffer parent_hash, size_t number, Buffer state_root,
+                           Buffer extrinsics_root, Buffer digest)
+      : parent_hash_(std::move(parent_hash)),
+        number_(number),
+        stateRoot_(std::move(state_root)),
+        extrinsics_root_(std::move(extrinsics_root)),
+        digest_(std::move(digest)) {}
+
+  const Buffer &BlockHeader::parentHash() const {
+    return parent_hash_;
+  }
+
+  size_t BlockHeader::number() const {
+    return number_;
+  }
+
+  const Buffer &BlockHeader::stateRoot() const {
+    return stateRoot_;
+  }
+
+  const Buffer &BlockHeader::extrinsicsRoot() const {
+    return extrinsics_root_;
+  }
+  const Buffer &BlockHeader::digest() const {
+    return digest_;
+  }
+
+}  // namespace kagome::primitives
---
+++ core/primitives/block_header.hpp
@@ -0,0 +1,64 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_PRIMITIVES_BLOCK_HEADER_HPP
+#define KAGOME_PRIMITIVES_BLOCK_HEADER_HPP
+
+#include "common/buffer.hpp"
+
+using namespace kagome::common;
+
+namespace kagome::primitives {
+  /**
+   * @class BlockHeader represents header of a block
+   */
+  class BlockHeader {
+   public:
+    /**
+     * @brief BlockHeader class constructor
+     * @param parent_hash parent hash
+     * @param state_root state root
+     * @param extrinsics_root extrinsics root
+     * @param digest digest collection
+     */
+    BlockHeader(Buffer parent_hash, size_t number, Buffer state_root,
+                Buffer extrinsics_root, Buffer digest);
+
+    /**
+     * @return parent hash const reference
+     */
+    const Buffer &parentHash() const;
+
+    /**
+     * @return number
+     */
+    size_t number() const;
+
+    /**
+     * @return state root const reference
+     */
+    const Buffer &stateRoot() const;
+
+    /**
+     * @return extrinsics root const reference
+     */
+    const Buffer &extrinsicsRoot() const;
+
+    /**
+     * @return digest const reference
+     */
+    const Buffer &digest() const;
+
+   private:
+    Buffer parent_hash_;  ///< 32-byte Blake2s hash of the header of the parent
+    size_t number_;       ///< index of current block in the chain
+    Buffer stateRoot_;    ///< root of the Merkle trie
+    Buffer extrinsics_root_;  ///< field for validation integrity
+    Buffer digest_;           ///< chain-specific auxiliary data
+  };
+
+}  // namespace kagome::primitives
+
+#endif  // KAGOME_PRIMITIVES_BLOCK_HEADER_HPP
