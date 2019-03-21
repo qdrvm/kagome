605b03a95bf3dc8bb2989f4f882afeb01e9f07fe
kamilsa <kamilsa16@gmail.com>
2019-03-21 15:03:28 +0300

    Update interface connection
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

---
+++ core/libp2p/basic/readable.hpp
@@ -0,0 +1,23 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_READABLE_HPP
+#define KAGOME_READABLE_HPP
+
+#include "libp2p/common/network_message.hpp"
+
+namespace libp2p::basic {
+
+class Readable {
+  /**
+     * Read message from the connection
+     * @return optional to message, received by that connection
+     */
+  virtual boost::optional<common::NetworkMessage> read() const = 0;
+};
+
+}
+
+#endif //KAGOME_READABLE_HPP
--- core/libp2p/common/network_message.hpp
+++ core/libp2p/common/network_message.hpp
@@ -6,11 +6,13 @@
 #ifndef KAGOME_NETWORK_MESSAGE_HPP
 #define KAGOME_NETWORK_MESSAGE_HPP
 
+#include "common/buffer.hpp"
+
 namespace libp2p::common {
   /**
    * Encoded message, which is sent through the network
    */
-  class NetworkMessage {};
+    using NetworkMessage = kagome::common::Buffer;
 }  // namespace libp2p::common
 
 #endif  // KAGOME_NETWORK_MESSAGE_HPP
