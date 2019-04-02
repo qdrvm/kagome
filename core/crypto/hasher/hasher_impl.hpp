924da7fe86b324bbecb4c23b2d0d1acc4a140b3a
Yura Zarudniy <yura.zarudny@gmail.com>
2019-04-02 18:33:25 +0300

    Feature/hasher (#60)
    * partially implemented
    
    * impelmented, test to go
    
    * tests implemented
    
    * tests description added
    
    * redundant comments removed
    
    * functions renamed, tests split
    
    * peer review comments taken into account
    
    * extra- tests removed.

---
+++ core/crypto/hasher/hasher_impl.hpp
@@ -0,0 +1,32 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_CORE_CRYPTO_HASHER_HASHER_IMPL_HPP_
+#define KAGOME_CORE_CRYPTO_HASHER_HASHER_IMPL_HPP_
+
+#include "crypto/hasher.hpp"
+
+namespace kagome::hash {
+
+  class HasherImpl : public Hasher {
+    using Hash128 = kagome::common::Hash128;
+    using Hash256 = kagome::common::Hash256;
+    using Buffer = kagome::common::Buffer;
+
+   public:
+    ~HasherImpl() override = default;
+
+    Hash128 twox_128(const Buffer &buffer) const override;
+
+    Hash256 twox_256(const Buffer &buffer) const override;
+
+    Hash256 blake2_256(const Buffer &buffer) const override;
+
+    Hash256 sha2_256(const Buffer &buffer) const override;
+  };
+
+}  // namespace kagome::hash
+
+#endif  // KAGOME_CORE_CRYPTO_HASHER_HASHER_IMPL_HPP_
