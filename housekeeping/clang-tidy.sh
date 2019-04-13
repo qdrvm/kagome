fb7d673e1f81354c6122db67ef0fb0bdcedcfc57
Bogdan Vaneev <warchantua@gmail.com>
2019-04-13 11:18:20 +0300

    Add filter for clang-ttidy
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

---
+++ housekeeping/clang-tidy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash -xe
+
+# this script filters build/compile_commands.json and runs clang-tidy on files changed in the last commit
+
+BUILD_DIR=$(echo "$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")")
+
+BN=$(dirname $0)
+cd ${BN}
+
+# list of cpp files changed in last commit; tests are ignored
+FILES=$(git show --name-only --format=oneline | tail -n+2 | grep "cpp" | grep -v "test")
+CLANG_TIDY=$(find /usr/local/Cellar/llvm -type f -name clang-tidy | head -n 1)
+RUN_CLANG_TIDY=$(find /usr/local/Cellar/llvm -type f -name run-clang-tidy.py | head -n 1)
+
+# filter compile_commands.json
+echo ${FILES} | ./filter_compile_commands.py -p ${BUILD_DIR}
+
+${RUN_CLANG_TIDY} -clang-tidy-binary=${CLANG_TIDY} -p ${BUILD_DIR}
