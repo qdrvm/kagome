c7f443deaa88dbd9ce20bbb0150b9c9ee1345946
kamilsa <kamilsa16@gmail.com>
2019-04-15 18:12:44 +0300

    Add more primitives (#77)
    * Add more primitives

---
+++ core/primitives/common.hpp
@@ -0,0 +1,13 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_CORE_PRIMITIVES_COMMON_HPP
+#define KAGOME_CORE_PRIMITIVES_COMMON_HPP
+
+namespace kagome::primitives {
+  using BlockNumber = uint64_t;
+}
+
+#endif  // KAGOME_CORE_PRIMITIVES_COMMON_HPP
