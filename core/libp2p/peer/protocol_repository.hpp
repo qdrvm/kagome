f5b7d225cd312c502208f20d6f5fa7ef28f626ea
Bogdan Vaneev <warchantua@gmail.com>
2019-04-27 16:20:57 +0300

    Add docs
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- core/libp2p/peer/protocol_repository.hpp
+++ core/libp2p/peer/protocol_repository.hpp
@@ -16,22 +16,59 @@
 
 namespace libp2p::peer {
 
+  /**
+   * @brief Storage for mapping between peer and its known protocols.
+   */
   class ProtocolRepository : public basic::GarbageCollectable {
    public:
     ~ProtocolRepository() override = default;
 
+    /**
+     * @brief Add protocols to a peer.
+     * param p peer
+     * @param ms list of protocols
+     * @return peer error, if no peer {@param p} found
+     */
     virtual outcome::result<void> addProtocols(
         const PeerId &p, gsl::span<const Protocol> ms) = 0;
 
+    /**
+     * @brief Removes protocols from a peer.
+     * @param p peer
+     * @param ms list of protocols
+     * @return peer error, if no peer {@param p} found
+     */
     virtual outcome::result<void> removeProtocols(
         const PeerId &p, gsl::span<const Protocol> ms) = 0;
 
+    /**
+     * @brief Get all supported protocols by given peer {@param p}
+     * @param p peer
+     * @return list of protocols (may be empty) or peer error, if no peer
+     * @param p} found
+     */
     virtual outcome::result<std::list<Protocol>> getProtocols(
         const PeerId &p) const = 0;
 
+    /**
+     * @brief Calculates set intersection between {@param protocols} and stored
+     * rotocols.
+     * @param p peer
+     * @param protocols check if given protocols are supported by a peer
+
+     * eturn list of supported protocols (may be empty) or peer error, if no p
+     * er {@param p} found
+     */
     virtual outcome::result<std::list<Protocol>> supportsProtocols(
         const PeerId &p, gsl::span<const Protocol> protocols) const = 0;
 
+    /**
+     * @brief Remove all associated protocols for given peer
+     * @param p peer
+     *
+     * @note does not remove peer from a list of known peers. So, peer can
+     * contain "0 protocols".
+     */
     virtual void clear(const PeerId &p) = 0;
   };
 
