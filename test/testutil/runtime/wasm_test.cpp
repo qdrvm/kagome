410ddebc13e06320ecf8fa200ac057194f346e8d
Harrm <abrehchs@gmail.com>
2019-05-03 17:46:24 +0300

    Fixes from review

--- test/testutil/runtime/wasm_test.cpp
+++ test/testutil/runtime/wasm_test.cpp
@@ -18,7 +18,7 @@ namespace test {
     int size = ifd.tellg();
     ifd.seekg(0, std::ios::beg);
     Buffer b(size, 0);
-    ifd.read((char *)b.toBytes(), size);
+    ifd.read((char *)b.toBytes(), size); // NOLINT
     state_code_ = b;
   }
 
