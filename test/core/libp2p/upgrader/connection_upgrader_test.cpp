e47a35d42b1b6752a5017b62fc533d87f2391809
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2019-05-06 17:16:52 +0300

    added blank for tests

---
+++ test/core/libp2p/upgrader/connection_upgrader_test.cpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "libp2p/upgrader/impl/connection_upgrader_impl.hpp"
+
+#include <gtest/gtest.h>
+
+/**
+ * @given
+ * @when
+ * @then
+ */
+TEST(ConnectionUpgraderTest, DISABLED_Simple) {
+  FAIL();
+}
