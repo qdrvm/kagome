9be472a7625a319b991ddd3edf92448372bc9d7a
Akvinikym <anarant12@gmail.com>
2019-05-14 17:10:52 +0300

    Expected::Result is removed (#108)
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- core/libp2p/multi/multistream.cpp
+++ core/libp2p/multi/multistream.cpp
@@ -12,13 +12,33 @@
 
 #include "libp2p/multi/uvarint.hpp"
 
-using kagome::common::Buffer;
-using kagome::expected::Error;
-using kagome::expected::Result;
-using kagome::expected::Value;
-using libp2p::multi::UVarint;
+OUTCOME_CPP_DEFINE_CATEGORY(libp2p::multi, Multistream::Error, e) {
+  using E = libp2p::multi::Multistream::Error;
+  switch (e) {
+    case E::NEW_LINE_EXPECTED:
+      return "new line delimiter is not found";
+    case E::NEW_LINE_NOT_EXPECTED:
+      return "codec path must not contain the new line symbol";
+    case E::SLASH_EXPECTED:
+      return "codec path must begin with '/'";
+    case E::WRONG_DATA_SIZE:
+      return "data size specified in the multistream header is not equal to "
+             "the actual size";
+    case E::PREFIX_ILL_FORMATTED:
+      return "prefix must not contain line breaks or forward slashes, as it is "
+             "a part of the protocol URI";
+    case E::REMOVE_LEAVES_EMPTY_PATH:
+      return "Attempt to remove the only part of the path; empty protocol path "
+             "is prohibited";
+    case E::PREFIX_NOT_FOUND:
+      return "prefix to be removed is not found in the protocol path";
+  }
+  return "unknown error";
+}
 
 namespace libp2p::multi {
+  using kagome::common::Buffer;
+  using libp2p::multi::UVarint;
 
   Multistream::Multistream(Path codecPath, const Buffer &data)
       : protocol_path_{std::move(codecPath)} {
@@ -26,32 +46,29 @@ namespace libp2p::multi {
     initData();
   }
 
-  auto Multistream::create(Path protocol_path, const Buffer &data)
-      -> Result<Multistream, std::string> {
+  outcome::result<Multistream> Multistream::create(Path protocol_path,
+                                                   const Buffer &data) {
     if (protocol_path.find('\n') != std::string::npos) {
-      return Error{"Codec path must not contain the new line symbol"};
+      return Error::NEW_LINE_NOT_EXPECTED;
     }
     if (protocol_path.front() != '/') {
-      return Error{"Codec path must begin with '/'"};
+      return Error::SLASH_EXPECTED;
     }
-    return Value{Multistream{std::move(protocol_path), data}};
+    return Multistream{std::move(protocol_path), data};
   }
 
-  auto Multistream::create(const Buffer &bytes)
-      -> Result<Multistream, std::string> {
+  outcome::result<Multistream> Multistream::create(const Buffer &bytes) {
     size_t varint_length = UVarint::calculateSize(bytes.toVector());
     UVarint size =
         UVarint(gsl::span<const uint8_t>(bytes.toBytes(), varint_length));
 
     auto path_end = std::find(bytes.begin() + varint_length, bytes.end(), '\n');
     if (path_end == bytes.end()) {
-      return Error{"New line delimiter is not found"};
+      return Error::NEW_LINE_EXPECTED;
     }
     if (std::distance(bytes.begin() + varint_length, bytes.end())
         != size.toUInt64()) {
-      return Error{
-          "Data size specified in the multistream header does not equal the "
-          "actual size"};
+      return Error::WRONG_DATA_SIZE;
     }
 
     Multistream m;
@@ -59,16 +76,14 @@ namespace libp2p::multi {
               std::back_inserter(m.protocol_path_));
     m.multistream_buffer_ = bytes;
     m.initData();
-    return Value{std::move(m)};
+    return m;
   }
 
-  auto Multistream::addPrefix(std::string_view prefix)
-      -> Result<std::reference_wrapper<Multistream>, std::string> {
+  outcome::result<std::reference_wrapper<Multistream>> Multistream::addPrefix(
+      std::string_view prefix) {
     if (prefix.empty() || prefix.find("\n") != std::string::npos
         || prefix.find("/") != std::string::npos) {
-      return Error{
-          "Prefix must not contain line breaks or forward slashes, as it is a "
-          "part of the protocol URI"};
+      return Error::PREFIX_ILL_FORMATTED;
     }
     protocol_path_.insert(std::begin(protocol_path_), prefix.begin(),
                           prefix.end());
@@ -77,31 +92,23 @@ namespace libp2p::multi {
 
     initBuffer(protocol_path_, data_);
     initData();
-    return Value{(*this)};
+    return std::ref(*this);
   }
 
-  auto Multistream::removePrefix(std::string_view prefix)
-      -> kagome::expected::Result<std::reference_wrapper<Multistream>,
-                                  std::string> {
+  outcome::result<std::reference_wrapper<Multistream>>
+  Multistream::removePrefix(std::string_view prefix) {
     if (prefix.empty() or prefix.find("\n") != std::string::npos
         or prefix.find("/") != std::string::npos) {
-      return Error{
-          "Prefix must not contain line breaks or forward slashes, as it is a "
-          "part of the protocol URI"};
+      return Error::PREFIX_ILL_FORMATTED;
     }
-    auto prefix_pos = protocol_path_.find(prefix);
 
+    auto prefix_pos = protocol_path_.find(prefix);
     if (prefix_pos == std::string::npos) {
-      std::stringstream ss;
-      ss << "The prefix " << prefix << " is not found in the protocol path "
-         << protocol_path_;
-      return Error{ss.str()};
+      return Error::PREFIX_NOT_FOUND;
     }
 
     if (prefix.size() == protocol_path_.size() - 1) {
-      return Error{
-          "Attempt to remove the only part of the path; Empty protocol path is "
-          "prohibited"};
+      return Error::REMOVE_LEAVES_EMPTY_PATH;
     }
 
     // -1/+1 because / preceding the prefix must also be removed
@@ -109,7 +116,7 @@ namespace libp2p::multi {
 
     initBuffer(protocol_path_, data_);
     initData();
-    return Value{(*this)};
+    return std::ref(*this);
   }
 
   const Multistream::Path &Multistream::getProtocolPath() const {
