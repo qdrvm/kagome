585fb53700ceb8fde70f20eebf3f94df7a6eb4c8
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2019-05-14 18:22:53 +0300

    TODO added, reformatted

--- test/core/runtime/parachain_test.cpp
+++ test/core/runtime/parachain_test.cpp
@@ -6,31 +6,31 @@
 #include "runtime/impl/parachain_host_impl.hpp"
 
 #include <gtest/gtest.h>
+#include "core/runtime/runtime_test.hpp"
 #include "extensions/extension_impl.hpp"
 #include "primitives/impl/scale_codec_impl.hpp"
 #include "runtime/impl/wasm_memory_impl.hpp"
 #include "testutil/outcome.hpp"
 #include "testutil/runtime/wasm_test.hpp"
-#include "core/runtime/runtime_test.hpp"
 
 using kagome::common::Buffer;
 using kagome::extensions::ExtensionImpl;
 using kagome::primitives::ScaleCodecImpl;
+using kagome::primitives::parachain::Chain;
+using kagome::primitives::parachain::DutyRoster;
+using kagome::primitives::parachain::Parachain;
+using kagome::primitives::parachain::ParaId;
+using kagome::primitives::parachain::Relay;
+using kagome::primitives::parachain::ValidatorId;
 using kagome::runtime::ParachainHost;
 using kagome::runtime::ParachainHostImpl;
-using kagome::primitives::parachain::ParaId;
-using kagome::primitives::parachain::DutyRoster;
-using kagome::primitives::parachain::ValidatorId;
-using kagome::primitives::parachain::Relay;
-using kagome::primitives::parachain::Chain;
-using kagome::primitives::parachain::Parachain;
 
 using ::testing::_;
 using ::testing::Return;
 
 namespace fs = boost::filesystem;
 
-class ParachainHostTest: public RuntimeTest {
+class ParachainHostTest : public RuntimeTest {
  public:
   void SetUp() override {
     RuntimeTest::SetUp();
@@ -46,6 +46,7 @@ class ParachainHostTest: public RuntimeTest {
   std::shared_ptr<ParachainHost> api_;
 };
 
+// TODO(yuraz): PRE-*** find out do we need to give block_id to api functions
 /**
  * @given initialized parachain host api
  * @when dutyRoster() is invoked
