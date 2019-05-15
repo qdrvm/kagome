dfe35e244ad3473708c851a145d6020a17c5783a
Harrm <abrehchs@gmail.com>
2019-05-15 15:30:58 +0300

    Remove irrelevant changes

--- test/core/primitives/primitives_codec_test.cpp
+++ test/core/primitives/primitives_codec_test.cpp
@@ -15,12 +15,12 @@
 #include "testutil/outcome.hpp"
 
 using kagome::common::Buffer;
+using kagome::primitives::AuthorityId;
 using kagome::common::Hash256;
 using kagome::primitives::Block;
 using kagome::primitives::BlockHeader;
 using kagome::primitives::BlockId;
 using kagome::primitives::Extrinsic;
-using kagome::primitives::AuthorityId;
 using kagome::primitives::Invalid;
 using kagome::primitives::ScaleCodec;
 using kagome::primitives::ScaleCodecImpl;
@@ -43,9 +43,9 @@ Hash256 createHash(std::initializer_list<uint8_t> bytes) {
 class Primitives : public testing::Test {
   void SetUp() override {
     block_id_hash_ = Hash256::fromHex(
-                         "000102030405060708090A0B0C0D0E0F"
-                         "101112131415161718191A1B1C1D1E1F")
-                         .value();
+        "000102030405060708090A0B0C0D0E0F"
+        "101112131415161718191A1B1C1D1E1F")
+        .value();
 
     codec_ = std::make_shared<ScaleCodecImpl>();
   }
@@ -57,7 +57,7 @@ class Primitives : public testing::Test {
                             createHash({1}),  // state_root
                             createHash({2}),  // extrinsic root
                             {5}};             // buffer: digest;
-  Buffer encoded_header_ = []() {
+  Buffer encoded_header_ = [](){
     Buffer h;
     // SCALE-encoded
     h.put(std::vector<uint8_t>(32, 0)); // parent_hash: hash256 with value 0
@@ -87,26 +87,26 @@ class Primitives : public testing::Test {
        {{'8', '7', '6', '5', '4', '3', '2', '1'}, 2}}  // ApiId_2
   };
   Buffer encoded_version_{
-      12,  'q', 'w', 'e',                      // spec name
-      12,  'a', 's', 'd',                      // impl name
-      1,   0,   0,   0,                        // auth version
-      2,   0,   0,   0,                        // impl version
-      8,                                       // collection of 2 items
+      12, 'q', 'w', 'e',                           // spec name
+      12, 'a', 's', 'd',                           // impl name
+      1,  0,   0,   0,                             // auth version
+      2,  0,   0,   0,                             // impl version
+      8,                                           // collection of 2 items
       '1', '2', '3', '4', '5', '6', '7', '8',  // id1
-      1,   0,   0,   0,                        // id1 version
+      1,  0,   0,   0,                             // id1 version
       '8', '7', '6', '5', '4', '3', '2', '1',  // id2
-      2,   0,   0,   0,                        // id2 version
+      2,  0,   0,   0,                             // id2 version
   };
   /// block id variant number alternative and corresponding scale representation
   BlockId block_id_number_{1ull};
   Buffer encoded_block_id_number_{1, 1, 0, 0, 0, 0, 0, 0, 0};
   /// block id variant hash alternative and corresponding scale representation
   BlockId block_id_hash_;
-  Buffer encoded_block_id_hash_{0,  // variant type order
-                                0x0,  0x1,  0x2,  0x3,  0x4,  0x5,  0x6,  0x7,
-                                0x8,  0x9,  0xA,  0xB,  0xC,  0xD,  0xE,  0xF,
-                                0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
-                                0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};
+  Buffer encoded_block_id_hash_{
+      0,    // variant type order
+      0x0,  0x1,  0x2,  0x3,  0x4,  0x5,  0x6,  0x7,  0x8,  0x9,  0xA,
+      0xB,  0xC,  0xD,  0xE,  0xF,  0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
+      0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};
   /// TransactionValidity variant instance as Valid alternative and
   /// corresponding scale representation
   Valid valid_transaction_{1,                    // priority
