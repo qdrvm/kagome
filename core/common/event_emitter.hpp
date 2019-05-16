374da45a91d5983d4d75f591f17b39fee9c7b01e
Akvinikym <anarant12@gmail.com>
2019-05-16 13:33:26 +0300

    Start
    Signed-off-by: Akvinikym <anarant12@gmail.com>

---
+++ core/common/event_emitter.hpp
@@ -0,0 +1,13 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_EVENT_EMITTER_HPP
+#define KAGOME_EVENT_EMITTER_HPP
+
+namespace kagome::common {
+  class EventEmitter {};
+}  // namespace kagome::common
+
+#endif  // KAGOME_EVENT_EMITTER_HPP
