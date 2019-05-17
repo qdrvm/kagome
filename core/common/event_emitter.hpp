cd30c9bebb6e08c9a303a3478d159e6f21bc9e86
Akvinikym <anarant12@gmail.com>
2019-05-17 15:48:56 +0300

    First draft
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- core/common/event_emitter.hpp
+++ core/common/event_emitter.hpp
@@ -6,8 +6,33 @@
 #ifndef KAGOME_EVENT_EMITTER_HPP
 #define KAGOME_EVENT_EMITTER_HPP
 
+#include <functional>
+
+#include <boost/signals2.hpp>
+
 namespace kagome::common {
-  class EventEmitter {};
+  template <typename Tag, typename... EventArgs>
+  class EventEmitter {
+   protected:
+    using EventHandler = void(EventArgs...);
+    using EventSignal = boost::signals2::signal<EventHandler>;
+
+    static void subscribe(const std::function<EventHandler> &handler) {
+      signal_.connect(handler);
+    }
+
+    static void fire(EventArgs... args) {
+      signal_(std::forward<EventArgs...>(args...));
+    }
+
+   private:
+    static EventSignal signal_;
+  };
+
+  template <typename Tag, typename... EventArgs>
+  typename EventEmitter<Tag, EventArgs...>::EventSignal
+      EventEmitter<Tag, EventArgs...>::signal_{};
+
 }  // namespace kagome::common
 
 #endif  // KAGOME_EVENT_EMITTER_HPP
