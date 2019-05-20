fb0e4f0e7f2e799d0e642ef7b060272f6f9375d7
Akvinikym <anarant12@gmail.com>
2019-05-20 17:23:26 +0300

    Emitter - version 3
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- core/libp2p/event/emitter.hpp
+++ core/libp2p/event/emitter.hpp
@@ -12,23 +12,107 @@
 
 namespace libp2p::event {
 
-/**
- * Make an event emitter with a specified type as an event argument
- * @param Tag - type (should be a structure) to be emitted by this instance
- * @note usage example can be found in event_emitter_test.cpp
- */
-#define KAGOME_EMITS(Tag)                             \
- private:                                             \
-  boost::signals2::signal<void(Tag)> signal_##Tag##_; \
-                                                      \
- public:                                              \
-  void on(const std::function<void(Tag)> &handler) {  \
-    signal_##Tag##_.connect(handler);                 \
-  }                                                   \
-                                                      \
-  void emit(Tag tag) {                                \
-    signal_##Tag##_(std::move(tag));                  \
-  }
+  namespace detail {
+
+    /**
+     * Base template prototype
+     */
+    template <int EventsAmount, typename... Events>
+    class EmitterBase;
+
+    /**
+     * "Main" recursive template of the Emitter
+     * @tparam EventsAmount - how much events there are totally
+     * @tparam ThisEvent - event, for which this Emitter template is responsible
+     * @tparam OtherEvents - zero or more events, which go after (\param
+     * ThisEvent)
+     */
+    template <int EventsAmount, typename ThisEvent, typename... OtherEvents>
+    class EmitterBase<EventsAmount, ThisEvent, OtherEvents...>
+        : public EmitterBase<EventsAmount, OtherEvents...> {
+      /// type of underlying signal
+      template <typename EventType>
+      using Signal = boost::signals2::signal<void(EventType)>;
+
+      /// base of this template - in fact, the base's "ThisEvent" is the the
+      /// first of this "OtherEvents"
+      using Base = EmitterBase<EventsAmount, OtherEvents...>;
+
+     protected:
+      /**
+       * Get an underlying signal, if (\tparam ExpectedEvent) is the same as
+       * ThisEvent type
+       * @return reference to signal
+       */
+      template <typename ExpectedEvent>
+      std::enable_if_t<std::is_same_v<ThisEvent, ExpectedEvent>,
+                       Signal<ExpectedEvent>>
+          &getSignal() {
+        return signal_;
+      }
+
+      /**
+       * SFINAE failure handler - continue the recursion, getting the base's
+       * signal, if ThisEvent is not the same as (\tparam ExpectedEvent)
+       * @return reference to signal
+       */
+      template <typename ExpectedEvent>
+      std::enable_if_t<!std::is_same_v<ThisEvent, ExpectedEvent>,
+                       Signal<ExpectedEvent>>
+          &getSignal() {
+        return Base::template getSignal<ExpectedEvent>();
+      }
+
+     private:
+      /// the underlying signal
+      Signal<ThisEvent> signal_;
+    };
+
+    /**
+     * End of template recursion
+     */
+    template <int EventsAmount>
+    class EmitterBase<EventsAmount> {
+     protected:
+      virtual ~EmitterBase() = default;
+    };
+
+  }  // namespace detail
+
+  /**
+   * Emitter, allowing to subscribe to events and emit them
+   * @tparam Events, for which this Emitter is responsible; should be empty
+   * structs or structs with desired params of the events
+   */
+  template <typename... Events>
+  class Emitter : public detail::EmitterBase<sizeof...(Events), Events...> {
+    /// underlying recursive base of the emitter
+    using Base = detail::EmitterBase<sizeof...(Events), Events...>;
+
+   public:
+    /**
+     * Subscribe to the specified event
+     * @tparam Event, for which the subscription is to be registered
+     * @param handler to be called, when a corresponding event is triggered
+     */
+    template <typename Event>
+    void on(const std::function<void(Event)> &handler) {
+      auto &signal = Base::template getSignal<Event>();
+      signal.connect(handler);
+    }
+
+    /**
+     * Trigger the specified event
+     * @tparam Event, which is to be triggered
+     * @param event - params (typically, a struct), with which the event is to
+     * be triggered
+     */
+    template <typename Event>
+    void emit(Event &&event) {
+      auto &signal = Base::template getSignal<Event>();
+      signal(std::forward<Event>(event));
+    }
+  };
 
 }  // namespace libp2p::event
 
--- test/core/libp2p/event/event_emitter_test.cpp
+++ test/core/libp2p/event/event_emitter_test.cpp
@@ -11,48 +11,46 @@ using namespace libp2p::event;
 
 class EventEmitterTest : public ::testing::Test {
  public:
-  /// events, which are to be emitted by the test emitter
   struct ConnectionOpened {
     std::string my_str;
   };
+  struct ConnectionPaused {};
   struct ConnectionClosed {
     int code1;
     int code2;
   };
-  struct CoffeeIsPrepared {};  // compile failure if tried to be emitted
-
-  struct ConnectionStateEmitter {
-    KAGOME_EMITS(ConnectionOpened)
-    KAGOME_EMITS(ConnectionClosed)
-  };
 };
 
 /**
- * @given event emitter with two events
+ * @given event emitter with several events
  * @when subscribing to those events @and emitting them
  * @then events are successfully emitted
  */
 TEST_F(EventEmitterTest, EmitEvents) {
   std::string connection_opened{};
+  auto connection_paused = -1;
   auto connection_closed_n = -1;
   auto connection_closed_k = -1;
 
-  ConnectionStateEmitter emitter{};
+  Emitter<ConnectionOpened, ConnectionPaused, ConnectionClosed> emitter{};
 
-  emitter.on([&connection_opened](ConnectionOpened event) {
+  emitter.on<ConnectionOpened>([&connection_opened](ConnectionOpened event) {
     connection_opened = event.my_str;
   });
-
-  emitter.on(
+  emitter.on<ConnectionPaused>(
+      [&connection_paused](ConnectionPaused) { connection_paused = 10; });
+  emitter.on<ConnectionClosed>(
       [&connection_closed_n, &connection_closed_k](ConnectionClosed event) {
         connection_closed_n = event.code1;
         connection_closed_k = event.code2;
       });
 
   emitter.emit(ConnectionOpened{"foo"});
+  emitter.emit(ConnectionPaused{});
   emitter.emit(ConnectionClosed{2, 5});
 
   ASSERT_EQ(connection_opened, "foo");
+  ASSERT_EQ(connection_paused, 10);
   ASSERT_EQ(connection_closed_n, 2);
   ASSERT_EQ(connection_closed_k, 5);
 }
