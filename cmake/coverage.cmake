30631f793c0b08d114f0bb57e6c9a124e11ab114
Bogdan Vaneev <warchantua@gmail.com>
2019-05-25 00:40:33 +0300

    Fix coverage and deduplicate cargo
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -4,10 +4,12 @@ include(cmake/3rdparty/CodeCoverage.cmake)
 
 append_coverage_compiler_flags()
 
+
 set(COVERAGE_LCOV_EXCLUDES
     'deps/*'
     'build/*'
     )
+set(COVERAGE_GCOVR_EXCLUDES ${COVERAGE_LCOV_EXCLUDES})
 
 setup_target_for_coverage_gcovr_xml(
     NAME ctest_coverage                    # New target name
