7c4f3e60c9659bdc13be3425cdbbc9e13378886e
Akvinikym <anarant12@gmail.com>
2019-06-03 13:21:53 +0300

    Start the process
    Signed-off-by: Akvinikym <anarant12@gmail.com>

---
+++ core/libp2p/muxer/yamux/yamux.hpp
@@ -0,0 +1,23 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_YAMUXED_CONNECTION_HPP
+#define KAGOME_YAMUXED_CONNECTION_HPP
+
+#include "libp2p/muxer/muxer_adaptor.hpp"
+
+namespace libp2p::muxer {
+  class Yamux : public MuxerAdaptor {
+   public:
+    peer::Protocol getProtocolId() const override;
+
+    outcome::result<std::shared_ptr<connection::CapableConnection>>
+    muxConnection(std::shared_ptr<connection::SecureConnection> conn) const override;
+
+    ~Yamux() override = default;
+  };
+}  // namespace libp2p::muxer
+
+#endif  //KAGOME_YAMUXED_CONNECTION_HPP
