44a8c1b03fb7185c3d92837e3b5fa904de9bfa35
Harrm <abrehchs@gmail.com>
2019-06-05 13:53:15 +0300

    Fix include

--- test/core/storage/merkle/mock_trie_db.hpp
+++ test/core/storage/merkle/mock_trie_db.hpp
@@ -7,22 +7,21 @@
 #define KAGOME_TEST_CORE_STORAGE_MERKLE_MOCK_TRIE_DB_HPP_
 
 #include <gmock/gmock.h>
-#include "core/storage/merkle/trie_db/trie_db_test.hpp"
+#include "storage/merkle/trie_db.hpp"
 
 namespace kagome::storage::merkle {
 
   class MockTrieDb : public TrieDb {
    public:
     MOCK_CONST_METHOD0(getRoot, common::Buffer());
-    MOCK_METHOD1(clearPrefix, void(const common::Buffer &));
+    MOCK_METHOD1(clearPrefix, outcome::result<void>(const common::Buffer &));
 
     MOCK_METHOD2(put,
                  outcome::result<void>(const common::Buffer &,
                                        const common::Buffer &));
     MOCK_CONST_METHOD1(get,
                        outcome::result<common::Buffer>(const common::Buffer &));
-    MOCK_METHOD1(remove, outcome::result<void>(
-        const common::Buffer &));
+    MOCK_METHOD1(remove, outcome::result<void>(const common::Buffer &));
     MOCK_CONST_METHOD1(contains, bool(const common::Buffer &));
 
     MOCK_METHOD0(cursor, std::unique_ptr<BufferMapCursor>());
