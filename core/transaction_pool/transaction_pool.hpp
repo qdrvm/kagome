26ff681345133b5c6c6f0e1d54f12760304d1432
Harrm <abrehchs@gmail.com>
2019-06-11 17:44:50 +0300

    Add transaction primitive and start working on TransactionPool interface

---
+++ core/transaction_pool/transaction_pool.hpp
@@ -0,0 +1,9 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_TRANSACTION_POOL_HPP
+#define KAGOME_TRANSACTION_POOL_HPP
+
+#endif //KAGOME_TRANSACTION_POOL_HPP
