dd0588ee4ac23e5dbdf6c518d8b15d1959f780f9
Harrm <abrehchs@gmail.com>
2019-06-11 17:45:41 +0300

    Add transaction primitive and start working on TransactionPool interface

--- core/transaction_pool/transaction_pool.hpp
+++ core/transaction_pool/transaction_pool.hpp
@@ -6,4 +6,19 @@
 #ifndef KAGOME_TRANSACTION_POOL_HPP
 #define KAGOME_TRANSACTION_POOL_HPP
 
-#endif //KAGOME_TRANSACTION_POOL_HPP
+#include <outcome/outcome.hpp>
+#include "primitives/block_id.hpp"
+#include "primitives/transaction.hpp"
+
+namespace kagome::transaction_pool {
+
+  class TransactionPool {
+   public:
+
+    outcome::result<void> submit_at(primitives::BlockId at, Transaction t);
+
+    std::vector<Transaction> getReadyTransactions();
+  };
+}
+
+#endif  // KAGOME_TRANSACTION_POOL_HPP
