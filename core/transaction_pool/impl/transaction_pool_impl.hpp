16119d8b7e98194ff3f44dec9084228bb0f5fe76
Harrm <abrehchs@gmail.com>
2019-06-12 10:17:27 +0300

    Drafts for implementation

---
+++ core/transaction_pool/impl/transaction_pool_impl.hpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_TRANSACTION_POOL_IMPL_HPP
+#define KAGOME_TRANSACTION_POOL_IMPL_HPP
+
+
+
+class TransactionPoolImpl {
+
+};
+
+
+
+#endif //KAGOME_TRANSACTION_POOL_IMPL_HPP
