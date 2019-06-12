acbeac002a922a7b6ceaebaebc32d57108d9bcd8
Harrm <abrehchs@gmail.com>
2019-06-12 10:17:27 +0300

    Drafts for implementation

---
+++ core/transaction_pool/impl/pool_moderator.hpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_POOL_MODERATOR_HPP
+#define KAGOME_POOL_MODERATOR_HPP
+
+
+
+class PoolModerator {
+
+};
+
+
+
+#endif //KAGOME_POOL_MODERATOR_HPP
