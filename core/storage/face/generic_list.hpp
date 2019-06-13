3ea81efff48297262f73d8fa71eda8ff3e19f522
Harrm <abrehchs@gmail.com>
2019-06-13 12:41:13 +0300

    Introduce GenericList interface with GenericIterator

--- core/storage/face/generic_list.hpp
+++ core/storage/face/generic_list.hpp
@@ -3,39 +3,137 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef KAGOME_QUEUE_HPP
-#define KAGOME_QUEUE_HPP
+#ifndef KAGOME_GENERIC_LIST_HPP
+#define KAGOME_GENERIC_LIST_HPP
+
+#include <cstddef>
+#include <memory>
+
+#include "storage/face/generic_iterator.hpp"
 
 namespace kagome::face {
 
-    /// An interface for a generic FIFO container
-    template <typename T>
-    class Queue {
-    public:
-        /**
-         * Put an element to the tail of the queue
-         * @param t the element
-         */
-        virtual void push(T &&t) = 0;
-        virtual void push(const T &t) = 0;
+  template <typename T>
+  class ForwardIterator;
 
-        /**
-         * Get the head element of the queue and then remove it
-         * @return the element
-         */
-        virtual T pop() = 0;
+  /// An interface for a generic list
+  template <typename T>
+  class GenericList {
+   public:
+    using size_type = size_t;
+    using value_type = T;
+    using iterator = ForwardIterator<GenericList<T>>;
 
-        /** Get the head element of the queue
-         * @return the const ref to the element
-         */
-        virtual const T& peek() const = 0;
+    virtual ~GenericList() = default;
 
-        /**
-         * Tell whether queue is empty
-         */
-        virtual bool isEmpty() const = 0;
-    };
+    /**
+     * Put an element to the back of the list
+     */
+    virtual void push_back(value_type &&t) = 0;
+    virtual void push_back(const value_type &t) = 0;
 
-}
+    /**
+     * Put an element to the front of the list
+     */
+    virtual void push_front(value_type &&t) = 0;
+    virtual void push_front(const value_type &t) = 0;
 
-#endif //KAGOME_QUEUE_HPP
+    /**
+     * Get the back of the list and then remove it
+     */
+    virtual value_type pop_back() = 0;
+
+    /**
+     * Get the front of the list and then remove it
+     */
+    virtual value_type pop_front() = 0;
+
+    /**
+     * Erase a range of elements
+     */
+    virtual void erase(const iterator &begin, const iterator &end) = 0;
+
+    /*
+     * Obtain an iterator to the list
+     */
+    virtual iterator begin() = 0;
+    virtual iterator end() = 0;
+
+    /**
+     * Tell whether list is empty
+     */
+    virtual bool empty() const = 0;
+
+    /**
+     * Obtain the size of the list
+     */
+    virtual size_type size() const = 0;
+  };
+
+  template <typename Container>
+  class ForwardIterator {
+   public:
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = ssize_t;
+    using pointer = typename Container::value_type *;
+    using const_pointer = const typename Container::value_type *;
+    using reference = typename Container::value_type &;
+    using const_reference = const typename Container::value_type &;
+    using value_type = typename Container::value_type;
+
+    ForwardIterator(std::shared_ptr<GenericIterator<Container>> it)
+        : it_{std::move(it)} {}
+
+    ForwardIterator(ForwardIterator &&it) noexcept : it_{std::move(it.it_)} {}
+    ForwardIterator(ForwardIterator const &it) : it_{it.it_} {}
+
+    GenericIterator<Container> &get_iterator() {
+      return *it_;
+    }
+
+    GenericIterator<Container> const &get_iterator() const {
+      return *it_;
+    }
+
+    ForwardIterator &operator=(const ForwardIterator &it) {
+      it_ = it.it_;
+      return *this;
+    }
+
+    ForwardIterator &operator=(ForwardIterator &&it) noexcept {
+      it_ = std::move(it.it_);
+      return *this;
+    }
+
+    bool operator!=(const ForwardIterator &other) {
+      return *it_ != *other.it_;
+    }
+
+    bool operator==(const ForwardIterator &other) {
+      return *it_ == *other.it_;
+    }
+
+    reference operator*() {
+      return **it_;
+    }
+
+    const_reference operator*() const {
+      return **it_;
+    }
+
+    pointer operator->() {
+      return it_->get();
+    }
+
+    ForwardIterator &operator++() {
+      ++(*it_);
+      return *this;
+    }
+
+   private:
+    std::shared_ptr<GenericIterator<Container>> it_;
+  };
+
+}  // namespace kagome::face
+
+#endif  // KAGOME_GENERIC_LIST_HPP
