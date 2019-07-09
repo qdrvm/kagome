4dc997aab251e4c054a3de63419cdbcdc52f15fc
Bogdan Vaneev <warchantua@gmail.com>
2019-07-09 15:30:18 +0300

    Add Listener implementation
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>
    
    Add more tests
    
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>
    
    Add tcp listener test
    
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>
    
    Fix build
    
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>
    
    Fix clang-tidy issues
    
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

---
+++ core/libp2p/network/impl/listener_impl.hpp
@@ -0,0 +1,85 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_LISTENER_IMPL_HPP
+#define KAGOME_LISTENER_IMPL_HPP
+
+#include "libp2p/connection/capable_connection.hpp"
+#include "libp2p/multi/multiaddress.hpp"
+#include "libp2p/network/connection_manager.hpp"
+#include "libp2p/network/listener.hpp"
+#include "libp2p/network/transport_manager.hpp"
+#include "libp2p/peer/identity_manager.hpp"
+#include "libp2p/peer/peer_id.hpp"
+#include "libp2p/peer/peer_repository.hpp"
+#include "libp2p/protocol_muxer/protocol_muxer.hpp"
+#include "libp2p/transport/transport_listener.hpp"
+#include "libp2p/transport/upgrader.hpp"
+
+namespace libp2p::network {
+
+  class ListenerImpl : public Listener {
+   public:
+    ~ListenerImpl() override = default;
+
+    ListenerImpl(std::shared_ptr<peer::AddressRepository> addrrepo,
+                 std::shared_ptr<protocol_muxer::ProtocolMuxer> multiselect,
+                 std::shared_ptr<Router> router,
+                 std::shared_ptr<TransportManager> tmgr,
+                 std::shared_ptr<ConnectionManager> cmgr);
+
+    bool isStarted() const override;
+
+    // starts listening on all provided multiaddresses
+    void start() override;
+
+    // stops listening on all multiaddresses
+    void stop() override;
+
+    outcome::result<void> closeListener(const multi::Multiaddress &ma) override;
+
+    // Listen tells the network to start listening on given multiaddr.
+    // May be executed multiple times (on different addresses/protocols).
+    outcome::result<void> listen(const multi::Multiaddress &ma) override;
+
+    // get all addresses we are listening on. will be exactly as we supplied to
+    // listen.
+    std::vector<multi::Multiaddress> getListenAddresses() const override;
+
+    // get all addresses we are listening on. may be different from those
+    // supplied to `listen`. example: /ip4/0.0.0.0/tcp/0 ->
+    // /ip4/127.0.0.1/tcp/30000 and /ip4/192.168.1.2/tcp/30000
+    outcome::result<std::vector<multi::Multiaddress>>
+    getListenAddressesInterfaces() const override;
+
+    void handleProtocol(
+        std::shared_ptr<protocol::BaseProtocol> protocol) override;
+
+    void setProtocolHandler(const peer::Protocol &protocol,
+                            StreamResultFunc cb) override;
+
+    void setProtocolHandler(const peer::Protocol &protocol, StreamResultFunc cb,
+                            Router::ProtoPredicate predicate) override;
+
+   private:
+    bool started = false;
+
+    // clang-format off
+    std::unordered_map<multi::Multiaddress, std::shared_ptr<transport::TransportListener>> listeners_;
+    // clang-format on
+
+    std::shared_ptr<peer::AddressRepository> addrrepo_;
+    std::shared_ptr<protocol_muxer::ProtocolMuxer> multiselect_;
+    std::shared_ptr<network::Router> router_;
+    std::shared_ptr<TransportManager> tmgr_;
+    std::shared_ptr<ConnectionManager> cmgr_;
+
+    void onConnection(
+        outcome::result<std::shared_ptr<connection::CapableConnection>>);
+  };
+
+}  // namespace libp2p::network
+
+#endif  // KAGOME_LISTENER_IMPL_HPP
---
+++ test/core/libp2p/network/listener_test.cpp
@@ -0,0 +1,195 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <gtest/gtest.h>
+#include <boost/di.hpp>
+
+#include "libp2p/network/impl/listener_impl.hpp"
+
+#include "testutil/gmock_actions.hpp"
+#include "testutil/literals.hpp"
+#include "testutil/outcome.hpp"
+
+#include "mock/libp2p/connection/stream_mock.hpp"
+#include "mock/libp2p/network/connection_manager_mock.hpp"
+#include "mock/libp2p/network/router_mock.hpp"
+#include "mock/libp2p/network/transport_manager_mock.hpp"
+#include "mock/libp2p/peer/address_repository_mock.hpp"
+#include "mock/libp2p/protocol/base_protocol_mock.hpp"
+#include "mock/libp2p/protocol_muxer/protocol_muxer_mock.hpp"
+#include "mock/libp2p/transport/transport_listener_mock.hpp"
+#include "mock/libp2p/transport/transport_mock.hpp"
+
+using namespace libp2p;
+using namespace network;
+using namespace protocol_muxer;
+using namespace transport;
+using namespace protocol;
+using namespace connection;
+using ::testing::_;
+using ::testing::Eq;
+using ::testing::MockFunction;
+using ::testing::Return;
+
+struct ListenerTest : public ::testing::Test {
+  ~ListenerTest() override = default;
+
+  std::shared_ptr<StreamMock> stream = std::make_shared<StreamMock>();
+
+  std::shared_ptr<BaseProtocolMock> protocol =
+      std::make_shared<BaseProtocolMock>();
+
+  std::shared_ptr<TransportListenerMock> transport_listener =
+      std::make_shared<TransportListenerMock>();
+
+  std::shared_ptr<TransportMock> transport = std::make_shared<TransportMock>();
+
+  std::shared_ptr<peer::AddressRepository> addrepo =
+      std::make_shared<peer::AddressRepositoryMock>();
+
+  std::shared_ptr<ProtocolMuxerMock> proto_muxer =
+      std::make_shared<ProtocolMuxerMock>();
+
+  std::shared_ptr<RouterMock> router = std::make_shared<RouterMock>();
+
+  std::shared_ptr<TransportManagerMock> tmgr =
+      std::make_shared<TransportManagerMock>();
+
+  std::shared_ptr<ConnectionManagerMock> cmgr =
+      std::make_shared<ConnectionManagerMock>();
+
+  std::shared_ptr<Listener> listener =
+      std::make_shared<ListenerImpl>(addrepo, proto_muxer, router, tmgr, cmgr);
+};
+
+/**
+ * @given 0 transport listeners
+ * @when listen on valid address
+ * @then new transport listener is created
+ */
+TEST_F(ListenerTest, ListenValidAddr) {
+  EXPECT_CALL(*tmgr, findBest(_)).Times(2).WillRepeatedly(Return(transport));
+  EXPECT_CALL(*transport, createListener(_))
+      .WillOnce(Return(transport_listener));
+
+  auto supported = "/ip4/127.0.0.1/tcp/0"_multiaddr;
+  EXPECT_OUTCOME_TRUE_1(listener->listen(supported));
+
+  EXPECT_EQ(listener->getListenAddresses(),
+            std::vector<multi::Multiaddress>{supported});
+
+  auto random_port_resolved = "/ip4/127.0.0.1/tcp/12345"_multiaddr;
+  EXPECT_CALL(*transport_listener, getListenMultiaddr())
+      .WillOnce(Return(random_port_resolved));
+
+  EXPECT_OUTCOME_TRUE(addrs_resolved, listener->getListenAddressesInterfaces());
+  EXPECT_EQ(addrs_resolved,
+            std::vector<multi::Multiaddress>{random_port_resolved});
+
+  // do listen on the same addr
+  EXPECT_OUTCOME_FALSE_1(listener->listen(supported));
+}
+
+/**
+ * @given 0 transport listeners
+ * @when listen on unsupported addr
+ * @then listen fails
+ */
+TEST_F(ListenerTest, ListenInvalidAddr) {
+  EXPECT_CALL(*tmgr, findBest(_)).WillOnce(Return(nullptr));
+
+  auto unsupported = "/ip4/127.0.0.1/udp/0"_multiaddr;
+  EXPECT_OUTCOME_FALSE_1(listener->listen(unsupported));
+
+  EXPECT_TRUE(listener->getListenAddresses().empty());
+
+  EXPECT_OUTCOME_TRUE(addrs_resolved, listener->getListenAddressesInterfaces());
+  EXPECT_TRUE(addrs_resolved.empty());
+}
+
+/**
+ * @given 1 transport listener
+ * @when start then stop the listener
+ * @then listener started, then stopped without error
+ */
+TEST_F(ListenerTest, StartStop) {
+  EXPECT_CALL(*tmgr, findBest(_)).WillOnce(Return(transport));
+  EXPECT_CALL(*transport, createListener(_))
+      .WillOnce(Return(transport_listener));
+  EXPECT_CALL(*transport_listener, listen(_))
+      .WillOnce(Return(outcome::success()));
+  EXPECT_CALL(*transport_listener, close())
+      .WillOnce(Return(outcome::success()));
+  EXPECT_CALL(*transport_listener, isClosed()).WillOnce(Return(false));
+
+  // given 1 listener
+  auto supported = "/ip4/127.0.0.1/tcp/0"_multiaddr;
+  EXPECT_OUTCOME_TRUE_1(listener->listen(supported));
+
+  // when start
+  ASSERT_NO_FATAL_FAILURE(listener->start());
+  ASSERT_TRUE(listener->isStarted());
+  ASSERT_NO_FATAL_FAILURE(listener->stop());
+  ASSERT_FALSE(listener->isStarted());
+}
+
+/**
+ * @given listener
+ * @when add new handler for protocol
+ * @then handler is registered
+ */
+TEST_F(ListenerTest, HandleProtocol) {
+  using std::string_literals::operator""s;
+  auto p = "/test/1.0.0"s;
+
+  EXPECT_CALL(*protocol, getProtocolId()).WillOnce(Return(p));
+  EXPECT_CALL(*protocol, handle(Eq(outcome::success(stream)))).Times(1);
+
+  // execute `handle` provided by protocol
+  EXPECT_CALL(*router, setProtocolHandler(Eq(p), _))
+      .WillOnce(Arg1CallbackWithArg(stream));
+
+  listener->handleProtocol(protocol);
+}
+
+/**
+ * @given listener
+ * @when setProtocolHandler is executed
+ * @then router successfully binds this protocol
+ */
+TEST_F(ListenerTest, SetProtocolHandler) {
+  using std::string_literals::operator""s;
+  auto p = "/test/1.0.0"s;
+
+  MockFunction<void(Listener::StreamResult)> cb;
+
+  EXPECT_CALL(cb, Call(Eq(outcome::success(stream)))).Times(1);
+
+  EXPECT_CALL(*router, setProtocolHandler(Eq(p), _))
+      .WillOnce(Arg1CallbackWithArg(stream));
+
+  listener->setProtocolHandler(p, [&](auto &&s) { cb.Call(s); });
+}
+
+/**
+ * @given listener
+ * @when setProtocolHandler is executed
+ * @then router successfully binds this protocol
+ */
+TEST_F(ListenerTest, SetProtocolHandlerWithMatcher) {
+  using std::string_literals::operator""s;
+  auto p = "/test/1.0.0"s;
+
+  MockFunction<void(Listener::StreamResult)> cb;
+
+  EXPECT_CALL(cb, Call(Eq(outcome::success(stream)))).Times(1);
+
+  EXPECT_CALL(*router, setProtocolHandler(Eq(p), _, _))
+      .WillOnce(Arg1CallbackWithArg(stream));
+
+  listener->setProtocolHandler(
+      p, [&](auto &&s) { cb.Call(s); },
+      [p](auto &&proto) { return proto == p; });
+}
