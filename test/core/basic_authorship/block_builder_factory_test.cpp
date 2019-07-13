4f34203925116e20b439e91e99e6246ab9774a44
kamilsa <kamilsa16@gmail.com>
2019-07-13 12:13:28 +0300

    Add more test

--- test/core/basic_authorship/block_builder_factory_test.cpp
+++ test/core/basic_authorship/block_builder_factory_test.cpp
@@ -21,32 +21,71 @@ using kagome::primitives::Digest;
 using kagome::runtime::BlockBuilderApiMock;
 using kagome::runtime::CoreMock;
 
-TEST(BlockBuilderFactoryTest, CreateTest) {
-  auto core = std::make_shared<CoreMock>();
-  auto block_builder_api = std::make_shared<BlockBuilderApiMock>();
-  auto header_backend = std::make_shared<HeaderBackendMock>();
+class BlockBuilderFactoryTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    expected_hash_.fill(0);
+    block_id_ = expected_hash_;
 
-  BlockNumber expected_number{42};
+    expected_header_.parent_hash = expected_hash_;
+    expected_header_.number = expected_number_;
+    expected_header_.digest = inherent_digest_;
 
-  kagome::common::Hash256 expected_hash;
-  expected_hash.fill(0);
-  BlockId block_id = expected_hash;
+    EXPECT_CALL(*header_backend_, hashFromBlockId(block_id_))
+        .WillOnce(Return(expected_hash_));
+    EXPECT_CALL(*header_backend_, numberFromBlockId(block_id_))
+        .WillOnce(Return(expected_number_));
+  }
 
-  Digest inherent_digest{0, 1, 2, 3};
+  std::shared_ptr<CoreMock> core_ = std::make_shared<CoreMock>();
+  std::shared_ptr<BlockBuilderApiMock> block_builder_api_ =
+      std::make_shared<BlockBuilderApiMock>();
+  std::shared_ptr<HeaderBackendMock> header_backend_ =
+      std::make_shared<HeaderBackendMock>();
 
-  BlockHeader expected_header;
-  expected_header.parent_hash = expected_hash;
-  expected_header.number = expected_number;
-  expected_header.digest = inherent_digest;
+  BlockNumber expected_number_{42};
+  kagome::common::Hash256 expected_hash_;
+  BlockId block_id_;
+  Digest inherent_digest_{0, 1, 2, 3};
+  BlockHeader expected_header_;
+};
 
-  BlockBuilderFactoryImpl factory(core, block_builder_api, header_backend);
-  EXPECT_CALL(*header_backend, hashFromBlockId(block_id))
-      .WillOnce(Return(expected_hash));
-  EXPECT_CALL(*header_backend, numberFromBlockId(block_id))
-      .WillOnce(Return(expected_number));
-  EXPECT_CALL(*core, initialise_block(expected_header))
+/**
+ * @given HeaderBackend providing expected_hash and expected_number of the
+ * block, which become part of expected_block_header
+ * @when core runtime successfully initialises expected_block_header
+ * @then BlockBuilderFactory that uses this core runtime and HeaderBackend
+ * successfully creates BlockBuilder
+ */
+TEST_F(BlockBuilderFactoryTest, CreateSuccessful) {
+  // given
+  EXPECT_CALL(*core_, initialise_block(expected_header_))
       .WillOnce(Return(outcome::success()));
+  BlockBuilderFactoryImpl factory(core_, block_builder_api_, header_backend_);
 
-  auto block_builder_res = factory.create(block_id, inherent_digest);
-  EXPECT_OUTCOME_TRUE(block_builder, block_builder_res);
+  // when
+  auto block_builder_res = factory.create(block_id_, inherent_digest_);
+
+  // then
+  ASSERT_TRUE(block_builder_res);
+}
+
+/**
+ * @given HeaderBackend providing expected_hash and expected_number of the
+ * block, which become part of expected_block_header
+ * @when core runtime does not initialise expected_block_header
+ * @then BlockBuilderFactory that uses this core runtime and HeaderBackend
+ * does not create BlockBuilder
+ */
+TEST_F(BlockBuilderFactoryTest, CreateFailed) {
+  // given
+  EXPECT_CALL(*core_, initialise_block(expected_header_))
+      .WillOnce(Return(outcome::failure(boost::system::error_code{})));
+  BlockBuilderFactoryImpl factory(core_, block_builder_api_, header_backend_);
+
+  // when
+  auto block_builder_res = factory.create(block_id_, inherent_digest_);
+
+  // then
+  ASSERT_FALSE(block_builder_res);
 }
