ab62a072ae818af580f2e97c439e6535a1abe413
Akvinikym <anarant12@gmail.com>
2019-07-15 13:41:50 +0300

    Test is implemented
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- test/core/libp2p/protocol/ping_test.cpp
+++ test/core/libp2p/protocol/ping_test.cpp
@@ -6,8 +6,50 @@
 #include "libp2p/protocol/ping.hpp"
 
 #include <gtest/gtest.h>
+#include "libp2p/peer/peer_id.hpp"
+#include "libp2p/protocol/ping/common.hpp"
+#include "mock/libp2p/connection/capable_connection_mock.hpp"
+#include "mock/libp2p/connection/stream_mock.hpp"
+#include "mock/libp2p/crypto/random_generator_mock.hpp"
+#include "mock/libp2p/host_mock.hpp"
+#include "mock/libp2p/peer/peer_repository_mock.hpp"
+#include "testutil/literals.hpp"
 
-class PingTest : public testing::Test {};
+using namespace libp2p;
+using namespace protocol;
+using namespace crypto::random;
+using namespace connection;
+using namespace protocol::detail;
+using namespace peer;
+
+using testing::_;
+using testing::InvokeArgument;
+using testing::Return;
+using testing::ReturnRef;
+
+class PingTest : public testing::Test {
+ public:
+  std::shared_ptr<Ping> ping_ = std::make_shared<Ping>(host_, rand_gen_);
+
+  HostMock host_;
+  std::shared_ptr<RandomGeneratorMock> rand_gen_ =
+      std::make_shared<RandomGeneratorMock>();
+
+  std::shared_ptr<CapableConnectionMock> conn_ =
+      std::make_shared<CapableConnectionMock>();
+  std::shared_ptr<StreamMock> stream_ = std::make_shared<StreamMock>();
+
+  PeerId peer_id_ = "xxxMyPeerxxx"_peerid;
+  PeerInfo peer_info_{peer_id_, {}};
+  PeerRepositoryMock peer_repo_;
+
+  std::vector<uint8_t> buffer_ = std::vector<uint8_t>(kPingMsgSize, 0xE3);
+};
+
+ACTION_P(ReadPut, buf) {
+  std::copy(buf.begin(), buf.end(), arg0.begin());
+  arg2(arg0.size());
+}
 
 /**
  * @given Ping protocol handler
@@ -16,7 +58,15 @@ class PingTest : public testing::Test {};
  * writes it back
  */
 TEST_F(PingTest, PingServer) {
-  
+  EXPECT_CALL(*stream_, read(_, kPingMsgSize, _))
+      .WillOnce(ReadPut(buffer_))
+      .WillOnce(  // no second read
+          InvokeArgument<2>(outcome::failure(boost::system::error_code{})));
+  EXPECT_CALL(*stream_,
+              write(gsl::span<const uint8_t>(buffer_), kPingMsgSize, _))
+      .WillOnce(InvokeArgument<2>(buffer_.size()));
+
+  ping_->handle(stream_);
 }
 
 /**
@@ -24,4 +74,21 @@ TEST_F(PingTest, PingServer) {
  * @when a stream over the Ping protocol is initiated from our side
  * @then a Ping message is sent over that stream @and we expect to get it back
  */
-TEST_F(PingTest, PingClient) {}
+TEST_F(PingTest, PingClient) {
+  EXPECT_CALL(*conn_, remotePeer()).WillOnce(Return(peer_id_));
+  EXPECT_CALL(host_, getPeerRepository()).WillOnce(ReturnRef(peer_repo_));
+  EXPECT_CALL(peer_repo_, getPeerInfo(peer_id_)).WillOnce(Return(peer_info_));
+  EXPECT_CALL(host_, newStream(peer_info_, kPingProto, _))
+      .WillOnce(InvokeArgument<2>(stream_));
+
+  EXPECT_CALL(*rand_gen_, randomBytes(kPingMsgSize)).WillOnce(Return(buffer_));
+  EXPECT_CALL(*stream_,
+              write(gsl::span<const uint8_t>(buffer_), kPingMsgSize, _))
+      .WillOnce(InvokeArgument<2>(buffer_.size()))
+      .WillOnce(  // no second write
+          InvokeArgument<2>(outcome::failure(boost::system::error_code{})));
+  EXPECT_CALL(*stream_, read(_, kPingMsgSize, _)).WillOnce(ReadPut(buffer_));
+
+  ping_->startPinging(conn_,
+                      [](auto &&session_res) { ASSERT_TRUE(session_res); });
+}
