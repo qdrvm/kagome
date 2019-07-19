ab4e5ea100f5fa6efdc7d93334f2ae34e9de613b
kamilsa <kamilsa16@gmail.com>
2019-07-19 15:58:15 +0300

    Fix block builder test

--- test/core/authorship/block_builder_test.cpp
+++ test/core/authorship/block_builder_test.cpp
@@ -45,13 +45,15 @@ class BlockBuilderTest : public ::testing::Test {
  * @given BlockBuilderApi that fails to apply extrinsic @and BlockBuilder that
  * uses that BlockBuilderApi
  * @when BlockBuilder tries to push extrinsic @and BlockBuilder bakes a block
- * @then push fails
+ * @then push fails @and created block is empty
  */
 TEST_F(BlockBuilderTest, PushWhenApplyFails) {
   // given
   Extrinsic xt{};
   EXPECT_CALL(*block_builder_api_, apply_extrinsic(xt))
       .WillOnce(Return(outcome::failure(boost::system::error_code{})));
+  EXPECT_CALL(*block_builder_api_, finalise_block())
+      .WillOnce(Return(expected_header_));
 
   // when
   auto res = block_builder_->pushExtrinsic(xt);
@@ -72,6 +74,8 @@ TEST_F(BlockBuilderTest, PushWhenApplySucceedsWithTrue) {
   // given
   Extrinsic xt{};
   EXPECT_CALL(*block_builder_api_, apply_extrinsic(xt)).WillOnce(Return(true));
+  EXPECT_CALL(*block_builder_api_, finalise_block())
+      .WillOnce(Return(expected_header_));
 
   // when
   auto res = block_builder_->pushExtrinsic(xt);
@@ -94,13 +98,14 @@ TEST_F(BlockBuilderTest, PushWhenApplySucceedsWithFalse) {
   // given
   Extrinsic xt{};
   EXPECT_CALL(*block_builder_api_, apply_extrinsic(xt)).WillOnce(Return(false));
-  EXPECT_CALL(*block_builder_api_, finalise_block()).WillOnce(Return(false));
+  EXPECT_CALL(*block_builder_api_, finalise_block())
+      .WillOnce(Return(expected_header_));
 
   // when
   auto res = block_builder_->pushExtrinsic(xt);
 
   // then
-  ASSERT_TRUE(res);
+  ASSERT_FALSE(res);
   EXPECT_OUTCOME_TRUE(block, block_builder_->bake());
   ASSERT_EQ(block.header, expected_header_);
   ASSERT_THAT(block.extrinsics, IsEmpty());
