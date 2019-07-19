040b213f986031a38b661c090d3793d199fa1e0f
Bogdan Vaneev <warchantua@gmail.com>
2019-07-19 17:42:53 +0300

    Fix codecov script
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- housekeeping/codecov.sh
+++ housekeeping/codecov.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -xe
 
-bash <(curl -s https://codecov.io/bash) -s $1 -t $2
+if [ -z "$1" ]; then
+    echo "1 arg is empty"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "2 arg is empty"
+    exit 2
+fi
+
+cd $1
+bash <(curl -s https://codecov.io/bash) -t $2
