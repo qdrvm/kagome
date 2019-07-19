18d0797ac9cb082c6a6cff1ee3ea8c6e4ee5ef43
Bogdan Vaneev <warchantua@gmail.com>
2019-07-19 17:49:22 +0300

    Set search dir = buildDir
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- housekeeping/codecov.sh
+++ housekeeping/codecov.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -xe
 
-if [ -z "$1" ]; then
-    echo "1 arg is empty"
+buildDir=$1
+token=$2
+
+
+if [ -z "$buildDir" ]; then
+    echo "buildDir is empty"
     exit 1
 fi
 
-if [ -z "$2" ]; then
-    echo "2 arg is empty"
+if [ -z "$token" ]; then
+    echo "token arg is empty"
     exit 2
 fi
 
-cd $1
-bash <(curl -s https://codecov.io/bash) -t $2
+cd $buildDir
+bash <(curl -s https://codecov.io/bash) -s $buildDir -t $token
