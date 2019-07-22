d37b70bd7501d1e40615340f6ab99f0a24a7d52a
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2019-07-22 17:28:41 +0700

    removed include iostream

--- core/api/transport/impl/listener_impl.cpp
+++ core/api/transport/impl/listener_impl.cpp
@@ -5,8 +5,6 @@
 
 #include "api/transport/impl/listener_impl.hpp"
 
-#include <iostream>
-
 #include "api/transport/impl/session_impl.hpp"
 
 namespace kagome::server {
@@ -16,30 +14,33 @@ namespace kagome::server {
                              const ListenerImpl::Endpoint &endpoint,
                              Configuration config)
       : context_(context), acceptor_(context_, endpoint), config_(config) {
-    onError().connect([this](auto&&) {
+    onError().connect([this](auto &&) {
       // TODO(yuraz): pre-249 log error
       stop();
     });
   }
 
-  void ListenerImpl::doAccept(std::function<void(std::shared_ptr<Session>)> on_new_session) {
-    acceptor_.async_accept(
-        [self = shared_from_this(), on_new_session](boost::system::error_code ec, Session::Socket socket) {
-          if (ec) {
-            return self->onError()(outcome::failure(ec));
-          }
-          if (self->state_ != State::WORKING) {
-            // TODO(yuraz): PRE-249 add log
-            return;
-          }
-          auto session = std::make_shared<SessionImpl>(
-              std::move(socket), self->context_, self->config_.operation_timeout);
-          on_new_session(session);
-          session->start();
-        });
+  void ListenerImpl::doAccept(
+      std::function<void(std::shared_ptr<Session>)> on_new_session) {
+    acceptor_.async_accept([self = shared_from_this(), on_new_session](
+                               boost::system::error_code ec,
+                               Session::Socket socket) {
+      if (ec) {
+        return self->onError()(outcome::failure(ec));
+      }
+      if (self->state_ != State::WORKING) {
+        // TODO(yuraz): PRE-249 add log
+        return;
+      }
+      auto session = std::make_shared<SessionImpl>(
+          std::move(socket), self->context_, self->config_.operation_timeout);
+      on_new_session(session);
+      session->start();
+    });
   }
 
-  void ListenerImpl::start(std::function<void(std::shared_ptr<Session>)> on_new_session) {
+  void ListenerImpl::start(
+      std::function<void(std::shared_ptr<Session>)> on_new_session) {
     state_ = State::WORKING;
     doAccept(on_new_session);
   }
