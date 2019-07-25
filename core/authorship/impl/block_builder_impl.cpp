9f7f8ab0e06c017b8049f06eda25278d3126e695
kamilsa <kamilsa16@gmail.com>
2019-07-25 10:48:36 +0300

    Add one more log to block builder

--- core/authorship/impl/block_builder_impl.cpp
+++ core/authorship/impl/block_builder_impl.cpp
@@ -22,8 +22,16 @@ namespace kagome::authorship {
 
   outcome::result<void> BlockBuilderImpl::pushExtrinsic(
       const primitives::Extrinsic &extrinsic) {
-    OUTCOME_TRY(ok, r_block_builder_->apply_extrinsic(extrinsic));
+    auto ok_res = r_block_builder_->apply_extrinsic(extrinsic);
+    if (not ok_res) {
+      logger_->warn(
+          "Extrinsic {} was not pushed to block. Error during xt application: "
+          "{}",
+          extrinsic.data.toHex(), ok_res.error().message());
+      return ok_res.error();
+    }
 
+    bool ok = ok_res.value();
     if (ok) {
       extrinsics_.push_back(extrinsic);
       return outcome::success();
