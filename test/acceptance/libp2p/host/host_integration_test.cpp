30aaeb5e5609d5af312dd861b1f8cd46fddc247a
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2019-08-14 17:12:41 +0300

    correct messages count in test

--- test/acceptance/libp2p/host/host_integration_test.cpp
+++ test/acceptance/libp2p/host/host_integration_test.cpp
@@ -120,4 +120,4 @@ INSTANTIATE_TEST_CASE_P(AllTestCases,
                         ::testing::Values(
                             // ports are not freed, so new ports each time
                             Config{1u, 1u, 40510u, 2s, 2s},
-                            Config{5u, 500u, 40510u, 5s, 2s}));
+                            Config{5u, 50u, 40510u, 5s, 2s}));
