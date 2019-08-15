3bb38f2bb7f5250f5dc80660b5d7a7f6b9ca6903
Akvinikym <anarant12@gmail.com>
2019-08-15 11:52:11 +0300

    Two more tests
    Signed-off-by: Akvinikym <anarant12@gmail.com>

--- test/core/consensus/synchronizer/synchronizer_test.cpp
+++ test/core/consensus/synchronizer/synchronizer_test.cpp
@@ -5,7 +5,10 @@
 
 #include "consensus/synchronizer/impl/synchronizer_impl.hpp"
 
+#include <functional>
+
 #include <gtest/gtest.h>
+#include <blockchain/block_tree_error.hpp>
 #include <boost/optional.hpp>
 #include "mock/core/blockchain/block_tree_mock.hpp"
 #include "mock/core/blockchain/header_backend_mock.hpp"
@@ -27,18 +30,25 @@ using namespace libp2p::peer;
 using testing::_;
 using testing::Ref;
 using testing::Return;
+using testing::ReturnRef;
 
 class SynchronizerTest : public testing::Test {
  public:
   void SetUp() override {
-    block_.header.parent_hash.fill(2);
-    block_hash_.fill(3);
+    block1_.header.parent_hash.fill(2);
+    block1_hash_.fill(3);
+    block2_.header.parent_hash = block1_hash_;
+    block2_hash_.fill(4);
 
-    EXPECT_CALL(*server_, onBlocksRequest(_));
+    EXPECT_CALL(*server_, onBlocksRequest(_))
+        .WillOnce(testing::SaveArg<0>(&on_blocks_request));
 
     synchronizer_ =
         std::make_shared<SynchronizerImpl>(tree_, headers_, network_state_);
   }
+
+  std::function<outcome::result<BlockResponse>(const BlockRequest &)>
+      on_blocks_request;
 
   PeerInfo peer1_info_{"peer1"_peerid, {}}, peer2_info_{"peer2"_peerid, {}};
 
@@ -54,8 +64,9 @@ class SynchronizerTest : public testing::Test {
 
   std::shared_ptr<Synchronizer> synchronizer_;
 
-  Block block_{{{}, 2}, {{{0x11, 0x22}}, {{0x55, 0x66}}}};
-  Hash256 block_hash_{};
+  Block block1_{{{}, 2}, {{{0x11, 0x22}}, {{0x55, 0x66}}}},
+      block2_{{{}, 3}, {{{0x13, 0x23}}, {{0x35, 0x63}}}};
+  Hash256 block1_hash_{}, block2_hash_;
 };
 
 /**
@@ -65,29 +76,43 @@ class SynchronizerTest : public testing::Test {
  */
 TEST_F(SynchronizerTest, Announce) {
   // GIVEN
-  EXPECT_CALL(*peer1_, blockAnnounce(BlockAnnounce{block_.header}, _))
+  EXPECT_CALL(*peer1_, blockAnnounce(BlockAnnounce{block1_.header}, _))
       .WillOnce(Arg1CallbackWithArg(outcome::success()));
-  EXPECT_CALL(*peer2_, blockAnnounce(BlockAnnounce{block_.header}, _))
+  EXPECT_CALL(*peer2_, blockAnnounce(BlockAnnounce{block1_.header}, _))
       .WillOnce(Arg1CallbackWithArg(outcome::success()));
 
   // WHEN
-  synchronizer_->announce(block_.header);
+  synchronizer_->announce(block1_.header);
 
   // THEN
 }
 
+/**
+ * @given synchronizer
+ * @when requesting for blocks without specifying a hash, which is to be in the
+ * response chain
+ * @then a returned chain consists of maximum number of blocks, returned in the
+ * specified order
+ */
 TEST_F(SynchronizerTest, RequestWithoutHash) {
   // GIVEN
-  EXPECT_CALL(*tree_, deepestLeaf()).WillOnce(Return(Ref(block_hash_)));
+  EXPECT_CALL(*tree_, deepestLeaf()).WillOnce(ReturnRef(block1_hash_));
   BlockRequest expected_request{0,
                                 BlockRequest::kBasicAttributes,
-                                block_hash_,
+                                block1_hash_,
                                 boost::none,
                                 Direction::DESCENDING,
                                 boost::none};
 
   EXPECT_CALL(*peer1_, blocksRequest(expected_request, _))
-      .WillOnce(Arg1CallbackWithArg(outcome::success()));
+      .WillOnce(Arg1CallbackWithArg(
+          BlockResponse{0,
+                        {{block1_hash_, block1_.header, block1_.body},
+                         {block2_hash_, block2_.header, block2_.body}}}));
+
+  EXPECT_CALL(*tree_, addBlock(block1_))
+      .WillOnce(Return(BlockTreeError::BLOCK_EXISTS));
+  EXPECT_CALL(*tree_, addBlock(block2_)).WillOnce(Return(outcome::success()));
 
   // WHEN
   auto finished = false;
@@ -100,6 +125,56 @@ TEST_F(SynchronizerTest, RequestWithoutHash) {
   ASSERT_TRUE(finished);
 }
 
-TEST_F(SynchronizerTest, RequestWithHash) {}
+/**
+ * @given synchronizer
+ * @when requesting for blocks, specifying a hash, which is to be in the
+ * response chain
+ * @then a returned chain has that block
+ */
+TEST_F(SynchronizerTest, RequestWithHash) {
+  // GIVEN
+  EXPECT_CALL(*tree_, getLastFinalized()).WillOnce(Return(block1_hash_));
+  BlockRequest expected_request{0,
+                                BlockRequest::kBasicAttributes,
+                                block1_hash_,
+                                block2_hash_,
+                                Direction::DESCENDING,
+                                boost::none};
 
-TEST_F(SynchronizerTest, ProcessRequest) {}
+  EXPECT_CALL(*peer2_, blocksRequest(expected_request, _))
+      .WillOnce(Arg1CallbackWithArg(
+          BlockResponse{0,
+                        {{block1_hash_, block1_.header, block1_.body},
+                         {block2_hash_, block2_.header, block2_.body}}}));
+
+  EXPECT_CALL(*tree_, addBlock(block1_))
+      .WillOnce(Return(BlockTreeError::BLOCK_EXISTS));
+  EXPECT_CALL(*tree_, addBlock(block2_)).WillOnce(Return(outcome::success()));
+
+  // WHEN
+  auto finished = false;
+  synchronizer_->requestBlocks(
+      peer2_info_, block2_hash_, [&finished](auto &&res) mutable {
+        ASSERT_TRUE(res);
+        finished = true;
+      });
+
+  // THEN
+  ASSERT_TRUE(finished);
+}
+
+/**
+ * @given synchronizer
+ * @when a request for blocks arrives
+ * @then an expected response is formed @and sent
+ */
+TEST_F(SynchronizerTest, ProcessRequest) {
+  // GIVEN
+  BlockRequest received_request{};
+
+  // WHEN
+  auto request_res = on_blocks_request(received_request);
+
+  // THEN
+  ASSERT_TRUE(request_res);
+}
