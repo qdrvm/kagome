689dd13b939fc3fc52f4f2b7ef6c7ce21ad8836e
Harrm <abrehchs@gmail.com>
2019-09-16 15:01:16 +0300

    Fix GTest deprecated-copy errors on GCC 9

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,3 +2,15 @@ hunter_config(
     Boost
     VERSION 1.70.0-p0
 )
+
+hunter_config(
+    GTest
+    VERSION 1.8.0-hunter-p11
+    CMAKE_ARGS CMAKE_CXX_FLAGS=-Wno-deprecated-copy
+)
+
+hunter_config(
+    GMock
+    VERSION 1.8.0-hunter-p11
+    CMAKE_ARGS CMAKE_CXX_FLAGS=-Wno-deprecated-copy
+)
