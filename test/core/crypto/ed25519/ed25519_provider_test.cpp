6304a3c5287aa6dc4c77536a915ba825b04c25c7
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2019-09-16 10:39:48 +0300

    add tests

--- test/core/crypto/ed25519/ed25519_provider_test.cpp
+++ test/core/crypto/ed25519/ed25519_provider_test.cpp
@@ -30,7 +30,7 @@ struct ED25519ProviderTest : public ::testing::Test {
 };
 
 /**
- * @given sr25519 provider instance configured with boost random generator
+ * @given ed25519 provider instance configured with boost random generator
  * @when generate 2 keypairs, repeat it 10 times
  * @then each time keys are different
  */
@@ -41,4 +41,73 @@ TEST_F(ED25519ProviderTest, GenerateKeysNotEqual) {
     ASSERT_NE(kp1.public_key, kp2.public_key);
     ASSERT_NE(kp1.private_key, kp2.private_key);
   }
+}
+
+/**
+ * @given ed25519 provider instance configured with boost random generator
+ * @and a predefined message
+ * @when generate a keypair @and sign message
+ * @and verify signed message with generated public key
+ * @then verification succeeds
+ */
+TEST_F(ED25519ProviderTest, SignVerifySuccess) {
+  EXPECT_OUTCOME_TRUE(kp, ed25519_provider->generateKeypair());
+  EXPECT_OUTCOME_TRUE(signature, ed25519_provider->sign(kp, message_span));
+  EXPECT_OUTCOME_TRUE(
+      res, ed25519_provider->verify(signature, message_span, kp.public_key));
+  ASSERT_EQ(res, true);
+}
+
+/**
+ * Don't try to sign a message using invalid key pair, this may lead to
+ * program termination
+ *
+ * @given ed25519 provider instance configured with boost random generator
+ * @and a predefined message
+ * @when generate a keypair @and make public key invalid @and sign message
+ * @then sign fails
+ */
+TEST_F(ED25519ProviderTest, DISABLED_SignWithInvalidKeyFails) {
+  EXPECT_OUTCOME_TRUE(kp, ed25519_provider->generateKeypair());
+  kp.public_key.fill(1);
+  EXPECT_OUTCOME_FALSE_1(ed25519_provider->sign(kp, message_span));
+}
+
+/**
+ * @given ed25519 provider instance configured with boost random generator
+ * @and and a predefined message
+ * @when generate keypair @and sign message @and take another public key
+ * @and verify signed message
+ * @then verification succeeds, but verification result is false
+ */
+TEST_F(ED25519ProviderTest, VerifyWrongKeyFail) {
+  EXPECT_OUTCOME_TRUE(kp, ed25519_provider->generateKeypair());
+  EXPECT_OUTCOME_TRUE(signature, ed25519_provider->sign(kp, message_span));
+  // generate another valid key pair and take public one
+  EXPECT_OUTCOME_TRUE(kp1, ed25519_provider->generateKeypair());
+  EXPECT_OUTCOME_TRUE(
+      ver_res,
+      ed25519_provider->verify(signature, message_span, kp1.public_key));
+
+  ASSERT_FALSE(ver_res);
+}
+
+/**
+ * Don't try to verify a message and signature against an invalid key, this may
+ * lead to program termination
+ *
+ * @given ed25519 provider instance configured with boost random generator
+ * @and and a predefined message
+ * @when generate keypair @and sign message
+ * @and generate another keypair and take public part for verification
+ * @and verify signed message
+ * @then verification fails
+ */
+TEST_F(ED25519ProviderTest, DISABLED_VerifyInvalidKeyFail) {
+  EXPECT_OUTCOME_TRUE(kp, ed25519_provider->generateKeypair());
+  EXPECT_OUTCOME_TRUE(signature, ed25519_provider->sign(kp, message_span));
+  // make public key invalid
+  kp.public_key.fill(1);
+  EXPECT_OUTCOME_FALSE_1(
+      ed25519_provider->verify(signature, message_span, kp.public_key));
 }
