f055425cb772cdfa50b8e4baa1d175a12fe418f0
Bogdan Vaneev <warchantua@gmail.com>
2019-09-19 21:09:02 +0300

    Fix include
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -5,5 +5,5 @@ hunter_config(
 
 hunter_config(sr25519
     URL https://github.com/Warchant/sr25519-crust/archive/1.0.1.tar.gz
-    SHA1 b0530930a441658c8449efc917b332189f955570
+    SHA1 3005d79b23b92ff27848c24a7751543a03a2dd13
     )
