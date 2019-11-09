0c6d0cbaafb38cbf7ce98bc895f4890045e20f2f
artyom-yurin <artem_yrin@mail.ru>
2019-11-09 14:49:00 +0300

    Fixed get method result when no value (#246)
    * Fixed get method result when no value
    
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

---
+++ core/storage/trie/impl/trie_error.hpp
@@ -0,0 +1,22 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_TRIE_ERROR_HPP
+#define KAGOME_TRIE_ERROR_HPP
+
+#include <outcome/outcome.hpp>
+
+namespace kagome::storage::trie {
+  /**
+   * @brief TrieDbError enum provides error codes for TrieDb methods
+   */
+  enum class TrieError {
+    NO_VALUE = 1,  // no stored value found by the given key
+  };
+}  // namespace kagome::storage::trie
+
+OUTCOME_HPP_DECLARE_ERROR(kagome::storage::trie, TrieError)
+
+#endif  // KAGOME_TRIE_ERROR_HPP
