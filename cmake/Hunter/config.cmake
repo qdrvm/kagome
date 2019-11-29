afbf32d0089e7040b4f40ab2d3aa41b3dcc4574d
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2019-11-29 19:19:03 +0300

    get back special version of GTest
    Signed-off-by: Yura Zarudniy <zarudniy@soramitsu.co.jp>

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,6 +4,12 @@ hunter_config(
     CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=ON
 )
 
+hunter_config(
+    GTest
+    VERSION 1.8.0-hunter-p11
+    CMAKE_ARGS "CMAKE_CXX_FLAGS=-Wno-deprecated-copy"
+)
+
 hunter_config(sr25519
     URL https://github.com/Warchant/sr25519-crust/archive/1.0.1.tar.gz
     SHA1 3005d79b23b92ff27848c24a7751543a03a2dd13
