9076becee439007cbd43687fa5b5a08ce49ea894
artyom-yurin <artem_yrin@mail.ru>
2019-12-11 12:44:08 +0300

    Fixed calculateOrderedTrieHash function
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- core/storage/trie/impl/ordered_trie_hash.hpp
+++ core/storage/trie/impl/ordered_trie_hash.hpp
@@ -6,6 +6,7 @@
 #ifndef KAGOME_ORDERED_TRIE_HASH_HPP
 #define KAGOME_ORDERED_TRIE_HASH_HPP
 
+#include <boost/endian/conversion.hpp>
 #include "common/buffer.hpp"
 #include "crypto/hasher.hpp"
 #include "scale/scale.hpp"
@@ -24,24 +25,27 @@ namespace kagome::storage::trie {
   template <typename It>
   outcome::result<common::Buffer> calculateOrderedTrieHash(const It &begin,
                                                            const It &end) {
+    PolkadotTrie trie;
+    PolkadotCodec codec;
+    // empty root
+    if (begin == end) return common::Buffer{}.put(codec.hash256({0}));
+
     // clang-format off
     static_assert(
         std::is_same_v<std::decay_t<decltype(*begin)>, common::Buffer>);
     // clang-format on
-    PolkadotTrie trie;
     It it = begin;
-    uint32_t key = 0;
+    scale::CompactInteger key = 0;
     while (it != end) {
       OUTCOME_TRY(enc, scale::encode(key++));
       OUTCOME_TRY(trie.put(common::Buffer{enc}, *it));
       it++;
     }
-    PolkadotCodec codec;
     OUTCOME_TRY(enc, codec.encodeNode(*trie.getRoot()));
     auto merkleValue = codec.merkleValue(enc);
-    auto merkleHash = merkleValue.size() > 32
-                          ? merkleValue
-                          : common::Buffer{codec.hash256(merkleValue)};
+    auto merkleHash = merkleValue.size() < 32
+                          ? common::Buffer{codec.hash256(merkleValue)}
+                          : merkleValue;
     return merkleHash;
   }
 
