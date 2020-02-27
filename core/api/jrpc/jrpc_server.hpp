af4aad7485e8a800c60e69d878d9b754f32c2997
Harrm <shcherba@soramitsu.co.jp>
2020-02-27 18:59:58 +0300

    Refactor json rpc api

---
+++ core/api/jrpc/jrpc_server.hpp
@@ -0,0 +1,11 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_J_RPC_SERVER_HPP
+#define KAGOME_J_RPC_SERVER_HPP
+
+class JRpcServer {};
+
+#endif  // KAGOME_J_RPC_SERVER_HPP
