9f1675aceb20de39409f206ead09c30c1c69fd4b
Harrm <shcherba@soramitsu.co.jp>
2020-03-23 13:54:24 +0300

    Implement polkadot trie cursor

---
+++ test/core/storage/trie/trie_db/polkadot_trie_cursor_test.cpp
@@ -0,0 +1,14 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "storage/trie/impl/polkadot_trie_cursor.hpp"
+
+#include <gtest/gtest.h>
+
+namespace trie = kagome::storage::trie;
+
+TEST(PolkadotTrieCursorTest, TODO) {
+  trie::PolkadotTrieCursor cursor {};
+}
