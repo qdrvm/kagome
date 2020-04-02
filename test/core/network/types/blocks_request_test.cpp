0b0c04779c61335ccdd9084b327e38349834ba8b
kamilsa <kamilsa16@gmail.com>
2020-04-02 20:59:00 +0300

    Fix blocks request test
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- test/core/network/types/blocks_request_test.cpp
+++ test/core/network/types/blocks_request_test.cpp
@@ -12,49 +12,32 @@
 #include "testutil/testparam.hpp"
 
 using kagome::common::Buffer;
+using kagome::common::Hash256;
 using kagome::network::BlockAttributes;
 using kagome::network::BlockAttributesBits;
 using kagome::network::BlocksRequest;
 using kagome::network::Direction;
 
-using testutil::createHash256;
-
 using kagome::scale::decode;
 using kagome::scale::encode;
-
-struct BlocksRequestTest : public ::testing::Test {
-  using Bits = BlockAttributesBits;
-
-  BlocksRequest block_request{1,
-                              {Bits::BODY | Bits::HEADER | Bits::RECEIPT},
-                              2u,
-                              createHash256({3, 4, 5}),
-                              Direction::DESCENDING,
-                              {5u}};
-  std::vector<uint8_t> encoded_value =
-      "010000000000000007010200000000000000"
-      "010304050000000000000000000000000000"
-      "00000000000000000000000000000001"
-      "0105000000"_unhex;
-};
 
 /**
  * @given sample `block request` instance @and encoded value buffer
  * @when scale-encode `block request` instance
  * @then result of encoding matches predefined buffer
  */
-TEST_F(BlocksRequestTest, EncodeSuccess) {
-  EXPECT_OUTCOME_TRUE(buffer, encode(block_request));
-  ASSERT_EQ(buffer, encoded_value);
-}
+TEST(BlocksRequestTest, EncodeSuccess) {
+  using Bits = BlockAttributesBits;
+  Hash256 hash;
+  hash.fill(2);
+  BlocksRequest block_request{1,
+                              {Bits::BODY | Bits::HEADER | Bits::RECEIPT},
+                              2u,
+                              hash,
+                              Direction::DESCENDING,
+                              {5u}};
 
-/**
- * @given buffer containing encoded `block request` instance
- * @and predefined `block request` instance
- * @when scale-decode that buffer
- * @then result of decoding matches predefined `block request` instance
- */
-TEST_F(BlocksRequestTest, DecodeSuccess) {
-  EXPECT_OUTCOME_TRUE(br, decode<BlocksRequest>(encoded_value));
-  ASSERT_EQ(br, block_request);
+  EXPECT_OUTCOME_TRUE(buffer, encode(block_request));
+  EXPECT_OUTCOME_TRUE(decoded, decode<BlocksRequest>(buffer));
+  ASSERT_EQ(block_request, decoded);
 }
