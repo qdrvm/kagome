102999622b9cc799f84b9413d779f3cfed644e37
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2020-04-06 00:51:36 +0300

    Change cmake-files for using internal fork of Hunter package manager
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -5,25 +5,33 @@ string(COMPARE EQUAL "$ENV{GITHUB_HUNTER_USERNAME}" "" username_is_empty)
 
 # binary cache can be uploaded to soramitsu/hunter-binary-cache so others will not build same dependencies twice
 if (NOT password_is_empty AND NOT username_is_empty)
-  option(HUNTER_RUN_UPLOAD "Upload cache binaries" YES)
-  message("Binary cache uploading is ENABLED.")
+    option(HUNTER_RUN_UPLOAD "Upload cache binaries" YES)
+    message("Binary cache uploading is ENABLED.")
+else ()
+    option(HUNTER_RUN_UPLOAD "Upload cache binaries" NO)
+    message("Binary cache uploading is DISABLED.")
 endif ()
 
 set(
-  HUNTER_PASSWORDS_PATH
-  "${CMAKE_CURRENT_LIST_DIR}/passwords.cmake"
-  CACHE
-  FILEPATH
-  "Hunter passwords"
+    HUNTER_PASSWORDS_PATH
+    "${CMAKE_CURRENT_LIST_DIR}/passwords.cmake"
+    CACHE
+    FILEPATH
+    "Hunter passwords"
 )
 
 set(
-  HUNTER_CACHE_SERVERS
-  "https://github.com/soramitsu/hunter-binary-cache"
-  CACHE
-  STRING
-  "Binary cache server"
+    HUNTER_CACHE_SERVERS
+    "https://github.com/soramitsu/hunter-binary-cache;https://github.com/Warchant/hunter-binary-cache;https://github.com/elucideye/hunter-cache;https://github.com/ingenue/hunter-cache"
+    CACHE
+    STRING
+    "Binary cache server"
 )
 
-
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
+
+HunterGate(
+    URL "https://github.com/soramitsu/soramitsu-hunter/archive/v0.23.253-soramitsu2.tar.gz"
+    SHA1 a34534537ff900b68c9304d5419b500430a5d304
+    LOCAL
+)
