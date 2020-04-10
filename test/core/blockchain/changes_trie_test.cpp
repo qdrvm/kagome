20039019404aa5e1034b63adb9f40b754bd16cc4
Harrm <shcherba@soramitsu.co.jp>
2020-04-10 13:03:29 +0300

    Start implementing tests

--- test/core/blockchain/changes_trie_test.cpp
+++ test/core/blockchain/changes_trie_test.cpp
@@ -7,6 +7,7 @@
 
 #include "blockchain/impl/changes_trie_builder_impl.hpp"
 #include "mock/core/blockchain/block_header_repository_mock.hpp"
+#include "scale/scale.hpp"
 #include "storage/trie/impl/in_memory_trie_db_factory.hpp"
 #include "storage/trie_db_overlay/impl/trie_db_overlay_impl.hpp"
 #include "testutil/literals.hpp"
@@ -14,10 +15,11 @@
 
 using kagome::blockchain::BlockHeaderRepositoryMock;
 using kagome::blockchain::ChangesTrieBuilderImpl;
+using kagome::common::Buffer;
 using kagome::storage::trie::InMemoryTrieDbFactory;
 using kagome::storage::trie_db_overlay::TrieDbOverlayImpl;
-using testing::Return;
 using testing::_;
+using testing::Return;
 
 TEST(ChangesTrieTest, IntegrationWithOverlay) {
   auto factory = std::make_shared<InMemoryTrieDbFactory>();
@@ -33,6 +35,42 @@ TEST(ChangesTrieTest, IntegrationWithOverlay) {
   auto hash = changes_trie_builder.finishAndGetHash();
   ASSERT_EQ(
       hash,
-      kagome::common::Hash256::fromHex("44cb1f5b9a98567dad54fca3a11444aa6f4d191c5dba8f2efba98dd9944da67f").value()
-      );
+      kagome::common::Hash256::fromHex(
+          "44cb1f5b9a98567dad54fca3a11444aa6f4d191c5dba8f2efba98dd9944da67f")
+          .value());
+}
+
+TEST(ChangesTrieTest, TrieCompat) {
+    auto factory = std::make_shared<InMemoryTrieDbFactory>();
+    auto trie = factory->makeTrieDb();
+    EXPECT_EQ(trie->getRootHash().toHex(), "a");
+    trie->put(Buffer{1}, Buffer{2});
+    EXPECT_EQ(trie->getRootHash().toHex(), "a");
+    trie->put(Buffer{5, 7}, Buffer{8, 6});
+    EXPECT_EQ(trie->getRootHash().toHex(), "a");
+}
+
+TEST(ChangesTrieTest, SubstrateCompatibility) {
+  auto factory = std::make_shared<InMemoryTrieDbFactory>();
+  auto repo = std::make_shared<BlockHeaderRepositoryMock>();
+  EXPECT_CALL(*repo, getNumberByHash(_)).WillRepeatedly(Return(99));
+  auto changes_trie_builder = ChangesTrieBuilderImpl({}, {}, factory, repo);
+
+  std::vector<
+      std::pair<Buffer, std::vector<kagome::primitives::ExtrinsicIndex>>>
+      changes {
+          {Buffer{1}, {1}},
+          {":extrinsic_index"_buf, {1}}
+  };
+  for (auto &change : changes) {
+    auto &key = change.first;
+    EXPECT_OUTCOME_TRUE_1(
+        changes_trie_builder.insertExtrinsicsChange(key, change.second));
+  }
+  auto hash = changes_trie_builder.finishAndGetHash();
+  ASSERT_EQ(
+      hash,
+      kagome::common::Hash256::fromHex(
+          "bb0c2ef6e1d36d5490f9766cfcc7dfe2a6ca804504c3bb206053890d6dd02376")
+          .value());
 }
