c432dbacbbd8b510ae2326309318ca5455518d0c
Harrm <shcherba@soramitsu.co.jp>
2020-04-12 13:21:41 +0300

    Update Merkle Hash calculation (#300)

--- core/storage/trie/impl/polkadot_trie.cpp
+++ core/storage/trie/impl/polkadot_trie.cpp
@@ -33,7 +33,7 @@ namespace kagome::storage::trie {
     return put(key, std::move(value_copy));
   }
 
-  PolkadotTrie::NodePtr PolkadotTrie::getRoot() {
+  PolkadotTrie::NodePtr PolkadotTrie::getRoot() const {
     return root_;
   }
 
