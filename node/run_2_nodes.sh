ea7c9f79a68d5ca09d837820102edb7a4e84de05
Harrm <shcherba@soramitsu.co.jp>
2020-05-18 17:28:15 +0300

    Working on making two nodes work

--- node/run_2_nodes.sh
+++ node/run_2_nodes.sh
@@ -17,7 +17,7 @@ $NODE_BIN_DIR/kagome_full \
     --p2p_port 30363 \
     --rpc_http_port 40363 \
     -v 2 \
-    --genesis_epoch
+    --genesis_epoch &
 
 if [[ $RUN_SECOND_NODE == "ON" ]]; then
   echo 'Run second node'
@@ -25,7 +25,7 @@ $NODE_BIN_DIR/kagome_full \
     --genesis $NODE_SRC_DIR/config/polkadot-v06-2nodes.json \
     --keystore $NODE_SRC_DIR/config/keystore2.json \
     --leveldb $BUILD_DIR/ldb1 \
-    --p2p_port 40365 \
+    --p2p_port 40010 \
     --rpc_http_port 40366 \
     -v 2
 fi
