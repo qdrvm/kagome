dd8aecd74e1a64a6c03a5015288c028f62cdc234
Bulat Saifullin <bulat@saifullin.ru>
2020-05-21 13:59:46 +0300

    clean travis.yml
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>

--- housekeeping/indocker.sh
+++ housekeeping/indocker.sh
@@ -2,7 +2,7 @@
 
 readonly DIR=$( cd $(dirname $0)/.. ; pwd -P )
 DOCKER_IMAGE="${DOCKER_IMAGE:?DOCKER_IMAGE variable is not defined}"
-JENKINS_ENV=$(env | awk -F= -v key='^(BRANCH_|BUILD_|HUDSON_|JENKINS_|JOB_|CHANGE_)' '$1 ~ key {print "-e " $1}')
+CI_ENV=$(env | awk -F= -v key='^(BRANCH_|BUILD_|HUDSON_|JENKINS_|JOB_|CHANGE_|TRAVIS_)' '$1 ~ key {print "-e " $1}')
 docker run -i --rm \
    --cap-add SYS_PTRACE \
    -v /tmp/cache/hunter:/root/.hunter \
@@ -16,5 +16,5 @@ docker run -i --rm \
    -e GIT_HUB_USERNAME \
    -e CODECOV_TOKEN \
    -e SONAR_TOKEN \
-    $JENKINS_ENV \
+    $CI_ENV \
    "${DOCKER_IMAGE}" $@
--- housekeeping/makeBuild.sh
+++ housekeeping/makeBuild.sh
@@ -2,6 +2,7 @@
 
 BUILD_DIR="${BUILD_DIR:?BUILD_DIR variable is not defined}"
 BUILD_TARGET="${BUILD_TARGET:-test}"
+BUILD_TREADS="${BUILD_TREADS:-4}"
 
 which git
 
@@ -10,5 +11,5 @@ cd "$(dirname $0)/.."
 git submodule update --init --recursive
 
 cmake . -B${BUILD_DIR} "$@"
-cmake --build "${BUILD_DIR}" -- -j4
+cmake --build "${BUILD_DIR}" -- -j${BUILD_TREADS}
 cmake --build "${BUILD_DIR}" --target "${BUILD_TARGET}"
