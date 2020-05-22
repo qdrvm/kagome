4e0ccb48e28399d434b1598a26e43e1dac6fb364
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2020-05-22 18:55:27 +0300

    fix: unique path for leveldb
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- test/integration/block/application_test_suite.hpp
+++ test/integration/block/application_test_suite.hpp
@@ -14,9 +14,8 @@ class ApplicationTestSuite : public testing::Test {
  protected:
   auto &getInjector() const {
     static auto injector = kagome::injector::makeApplicationInjector(
-        "../../node/config/localchain.json",
-        boost::filesystem::temp_directory_path().native()
-            + boost::filesystem::unique_path().native() + "ldb",
+        (boost::filesystem::current_path().parent_path().parent_path()/"node"/"config"/"localchain.json").native(),
+        (boost::filesystem::temp_directory_path()/boost::filesystem::unique_path()).native(),
         {},
         {});
     return injector;
