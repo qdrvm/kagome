05ccc7110f6f8a3c6484dc489062ecec9f5d508c
Dmitriy Khaustov <khaustov.dm@gmail.com>
2020-05-27 17:00:12 +0300

    Application state manager (#330)
    * feature: AppStateManager
    * fix: some cmake clang config
    * fix: using sanitizers locally
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -2,13 +2,15 @@ file(GLOB_RECURSE
   ALL_CXX_SOURCE_FILES
   core/*.[ch]pp
   core/*.[ch]
+  node/*.[ch]pp
+  node/*.[ch]
   test/*.[ch]pp
   test/*.[ch]
   )
 
 # Adding clang-format target if executable is found
 if(NOT CLANG_FORMAT_BIN)
-  find_program(CLANG_FORMAT_BIN "clang-format")
+  find_program(CLANG_FORMAT_BIN NAMES clang-format clang-format-9 clang-format-8 clang-format-7)
 endif()
 
 if(CLANG_FORMAT_BIN)
--- core/application/kagome_application.hpp
+++ core/application/kagome_application.hpp
@@ -19,9 +19,6 @@ namespace kagome::application {
      * @brief runs application
      */
     virtual void run() = 0;
-
-    /// Doing shutting down
-    virtual void shutdown() = 0;
   };
 }  // namespace kagome::application
 
