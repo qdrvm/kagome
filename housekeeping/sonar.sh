6782c81819a558879fb232973f7e71098791c738
Bulat Saifullin <bulat@saifullin.ru>
2020-06-04 11:47:00 +0300

    fix sonar
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>

--- housekeeping/sonar.sh
+++ housekeeping/sonar.sh
@@ -3,8 +3,16 @@
 BUILD_DIR="${BUILD_DIR:?BUILD_DIR variable is not defined}"
 GITHUB_REPOSITORY="${GITHUB_REPOSITORY:?GITHUB_REPOSITORY variable is not defined}"
 GITHUB_TOKEN="${GITHUB_TOKEN:?GITHUB_TOKEN variable is not defined}"
-BRANCH_NAME="${BRANCH_NAME:?BRANCH_NAME variable is not defined}"
 SONAR_TOKEN="${SONAR_TOKEN:?SONAR_TOKEN variable is not defined}"
+BRANCH_NAME="${BRANCH_NAME:?BRANCH_NAME variable is not defined}"
+# For github action we need remove ref prefix
+if [[ "$BRANCH_NAME"  == refs/heads/* ]]; then
+  BRANCH_NAME="${BRANCH_NAME#refs/heads/}"
+elif [[ "$BRANCH_NAME"  == refs/tags/* ]]; then
+  BRANCH_NAME="${BRANCH_NAME#refs/tags/}"
+elif [[ "$BRANCH_NAME"  == refs/pull/* ]]; then
+  BRANCH_NAME="${GITHUB_HEAD_REF}"
+fi
 
 sonar_option=""
 if [ -n "${CHANGE_ID}" ]; then
