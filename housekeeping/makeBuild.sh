fd48623e6cc39b073ed68110f6a09f6415d05b59
Bulat Saifullin <bulat@saifullin.ru>
2020-06-04 10:45:48 +0300

    DOPS-1005  Kagome update ci, add docker build and push (#327)
    * move to script
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * indocker
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * clang-tidy
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * clean travis.yml
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * github actions
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * coverage
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * sonar
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * docker
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * Delete Jenkinsfile
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * libasan5
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>
    
    * fix review comments
    
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>

---
+++ housekeeping/makeBuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+BUILD_DIR="${BUILD_DIR:?BUILD_DIR variable is not defined}"
+BUILD_FINAL_TARGET="${BUILD_FINAL_TARGET:-test}"
+BUILD_TREADS="${BUILD_TREADS:-4}"
+
+which git
+
+cd "$(dirname $0)/.."
+
+cmake . -B${BUILD_DIR} "$@"
+cmake --build "${BUILD_DIR}" -- -j${BUILD_TREADS}
+cmake --build "${BUILD_DIR}" --target "${BUILD_FINAL_TARGET}"
