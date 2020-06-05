0d4f8ea26efd6c76f1fb3bc744fefe76c560c2b9
Harrm <shcherba@soramitsu.co.jp>
2020-06-05 17:08:43 +0300

    Introduce runtime::TrieStorageProvider

---
+++ core/runtime/common/trie_storage_provider_impl.hpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_CORE_RUNTIME_COMMON_TRIE_STORAGE_PROVIDER_IMPL
+#define KAGOME_CORE_RUNTIME_COMMON_TRIE_STORAGE_PROVIDER_IMPL
+
+#include "runtime/trie_storage_provider.hpp"
+
+namespace kagome::runtime {
+
+  class StorageProviderImpl {};
+
+}  // namespace kagome::
+
+#endif  // KAGOME_CORE_RUNTIME_COMMON_TRIE_STORAGE_PROVIDER_IMPL
