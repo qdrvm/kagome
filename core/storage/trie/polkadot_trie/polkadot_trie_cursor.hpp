8acb0c68262cdb4f21c3e2dd27c95f196cc2d5eb
Harrm <shcherba@soramitsu.co.jp>
2020-06-09 12:09:51 +0300

    Create blank polkadot cursor

---
+++ core/storage/trie/polkadot_trie/polkadot_trie_cursor.hpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_CORE_STORAGE_TRIE_POLKADOT_TRIE_POLKADOT_TRIE_CURSOR
+#define KAGOME_CORE_STORAGE_TRIE_POLKADOT_TRIE_POLKADOT_TRIE_CURSOR
+
+#include "storage/face/map_cursor.hpp"
+
+namespace kagome::storage::trie {
+
+  class PolkadotTrieCursor {};
+
+}  // namespace kagome::trie
+
+#endif  // KAGOME_CORE_STORAGE_TRIE_POLKADOT_TRIE_POLKADOT_TRIE_CURSOR
---
+++ core/storage/trie/polkadot_trie/polkadot_trie_cursor_impl.cpp
@@ -0,0 +1,8 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "storage/trie/polkadot_trie/polkadot_trie_cursor_impl.hpp"
+
+namespace kagome::storage::trie{}
