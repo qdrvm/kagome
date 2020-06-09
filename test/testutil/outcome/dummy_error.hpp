1a30b7279cee995bee8891b645d9317ac991fb30
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2020-06-09 22:15:42 +0300

    feature: dummy_error as testutil
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

---
+++ test/testutil/outcome/dummy_error.hpp
@@ -0,0 +1,8 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+namespace testutil {
+  enum class DummyError { ERROR = 1, ERROR_2, ERROR_3, ERROR_4, ERROR_5 };
+}
