d8a02b645c19156bd13336c5b884b95c2875f0d4
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2020-06-10 15:32:17 +0300

    doc: add comment as explanation
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- test/testutil/outcome/dummy_error.hpp
+++ test/testutil/outcome/dummy_error.hpp
@@ -4,5 +4,11 @@
  */
 
 namespace testutil {
+  /**
+   * @name Dummy error
+   * @brief Special error for using instead special error and avoid need linkage
+   * additional external library, i.e. in mock-object, call expectation, etc.
+   * This provide several error codes for cases with different error.
+   */
   enum class DummyError { ERROR = 1, ERROR_2, ERROR_3, ERROR_4, ERROR_5 };
-}
+}  // namespace testutil
