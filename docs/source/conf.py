7a08ba25bb2efcf2a5e03784251e307b3d29fa3c
kamilsa <kamilsa16@gmail.com>
2020-06-10 15:31:59 +0300

    Add markdown tables support
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,7 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
+    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
