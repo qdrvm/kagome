bf7eb1ba857dbcda596e95d92462413f44bbbbe8
kamilsa <kamilsa16@gmail.com>
2020-06-11 05:35:04 +0300

    Add kagome_block_producing to build targets
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- housekeeping/docker/release/makeRelease.sh
+++ housekeeping/docker/release/makeRelease.sh
@@ -6,4 +6,4 @@ BUILD_TREADS="${BUILD_TREADS:-4}"
 cd "$(dirname $0)/../../.."
 
 cmake . -B${BUILD_DIR} -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=Release
-cmake --build "${BUILD_DIR}" --target kagome_full_syncing kagome_validating -- -j${BUILD_TREADS}
+cmake --build "${BUILD_DIR}" --target kagome_full_syncing kagome_validating kagome_block_producing -- -j${BUILD_TREADS}
