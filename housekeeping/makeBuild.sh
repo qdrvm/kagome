dada96791070d97464358e08a7342e562c55014f
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2020-06-19 14:00:35 +0300

    fix CI
    Signed-off-by: Yura Zarudniy <zarudniy@soramitsu.co.jp>

--- housekeeping/makeBuild.sh
+++ housekeeping/makeBuild.sh
@@ -7,7 +7,7 @@ BUILD_TREADS="${BUILD_TREADS:-4}"
 which git
 
 cd "$(dirname $0)/.."
-git submodule update --init --recursive
+
 cmake . -B${BUILD_DIR} "$@"
 cmake --build "${BUILD_DIR}" -- -j${BUILD_TREADS}
 cmake --build "${BUILD_DIR}" --target "${BUILD_FINAL_TARGET}"
