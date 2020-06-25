0313a2200f2064adcc7e6d7db596f315899a750d
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2020-06-26 01:25:01 +0300

    add crypto_store interface
    Signed-off-by: Yura Zarudniy <zarudniy@soramitsu.co.jp>

---
+++ core/crypto/crypto_store.hpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_CRYPTO_STORE_HPP
+#define KAGOME_CRYPTO_STORE_HPP
+
+namespace kagome::crypto {
+  class CryptoStore {
+   public:
+    CryptoStore() = default;
+
+  };
+}  // namespace kagome::crypto
+
+#endif  // KAGOME_CRYPTO_STORE_HPP
