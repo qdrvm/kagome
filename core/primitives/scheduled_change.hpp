075ef744467e0e79d85acce5ecb52dd56861a0bb
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2020-07-16 19:07:08 +0300

    refactoring: structures of scheduled changes mechanism
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/primitives/scheduled_change.hpp
+++ core/primitives/scheduled_change.hpp
@@ -3,52 +3,59 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef KAGOME_CORE_PRIMITIVES_SCHEDULED_CHANGE_HPP
-#define KAGOME_CORE_PRIMITIVES_SCHEDULED_CHANGE_HPP
+#ifndef KAGOME_CORE_PRIMITIVES_SCHEDULED_CHANGE
+#define KAGOME_CORE_PRIMITIVES_SCHEDULED_CHANGE
 
-#include <boost/optional.hpp>
 #include "primitives/authority.hpp"
 #include "primitives/common.hpp"
-#include "primitives/session_key.hpp"
 
 namespace kagome::primitives {
-  /// @struct ScheduledChange is used by Grandpa api runtime
-  struct ScheduledChange {
-    std::vector<std::pair<AuthorityId, uint64_t>> next_authorities;
-    BlockNumber delay = 0;
+  struct DelayInChain {
+    uint32_t subchain_lenght = 0;
   };
 
-  /// @brief api function returns optional value
-  using ScheduledChangeOptional = boost::optional<ScheduledChange>;
+  struct AuthorityListChange {
+    AuthorityList authorities{};
+    uint32_t subchain_lenght = 0;
+  };
 
-  /// @brief result type for grandpa forced_change function
-  using ForcedChange = std::pair<BlockNumber, ScheduledChange>;
+  struct ScheduledChange : public AuthorityListChange {};
+  struct ForcedChange : public AuthorityListChange {};
+  struct OnDisabled {
+    uint64_t authority_index = 0;
+  };
+  struct Pause : public DelayInChain {};
+  struct Resume : public DelayInChain {};
 
-  /**
-   * @brief outputs ScheduledChange instance to stream
-   * @tparam Stream stream type
-   * @param s reference to stream
-   * @param v value to output
-   * @return reference to stream
-   */
-  template <class Stream,
-            typename = std::enable_if_t<Stream::is_encoder_stream>>
-  Stream &operator<<(Stream &s, const ScheduledChange &v) {
-    return s << v.next_authorities << v.delay;
+  template <class Stream>
+  Stream &operator<<(Stream &s, const DelayInChain &delay) {
+    return s << delay.subchain_lenght;
   }
 
-  /**
-   * @brief decodes ScheduledChange instance from stream
-   * @tparam Stream stream type
-   * @param s reference to stream
-   * @param v value to decode
-   * @return reference to stream
-   */
-  template <class Stream,
-            typename = std::enable_if_t<Stream::is_decoder_stream>>
-  Stream &operator>>(Stream &s, ScheduledChange &v) {
-    return s >> v.next_authorities >> v.delay;
+  template <class Stream>
+  Stream &operator>>(Stream &s, DelayInChain &delay) {
+    return s >> delay.subchain_lenght;
+  }
+
+  template <class Stream>
+  Stream &operator<<(Stream &s, const OnDisabled &target) {
+    return s << target.authority_index;
+  }
+
+  template <class Stream>
+  Stream &operator>>(Stream &s, OnDisabled &target) {
+    return s >> target.authority_index;
+  }
+
+  template <class Stream>
+  Stream &operator<<(Stream &s, const AuthorityListChange &alc) {
+    return s << alc.authorities << alc.subchain_lenght;
+  }
+
+  template <class Stream>
+  Stream &operator>>(Stream &s, AuthorityListChange &alc) {
+    return s >> alc.authorities >> alc.subchain_lenght;
   }
 }  // namespace kagome::primitives
 
-#endif  // KAGOME_CORE_PRIMITIVES_SCHEDULED_CHANGE_HPP
+#endif  // KAGOME_CORE_PRIMITIVES_SCHEDULED_CHANGE
