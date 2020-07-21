4a97e1a19cb74dbfe4cb2a24c7d980e061e30e95
iceseer <iceseer@gmail.com>
2020-07-21 10:17:48 +0300

    formatting
    Signed-off-by: iceseer <iceseer@gmail.com>

--- test/core/subscription/subscription_engine_test.cpp
+++ test/core/subscription/subscription_engine_test.cpp
@@ -15,15 +15,24 @@ using kagome::subscription::SubscriptionTargetMock;
 
 class SubscriptionEngineTest : public testing::Test {
  public:
-  using EnginePtr = std::shared_ptr<SubscriptionEngine<std::string_view, SubscriptionTargetMock, std::string_view, int32_t>>;
-  using TargetPtr = std::shared_ptr<Subscriber<std::string_view, SubscriptionTargetMock, std::string_view, int32_t>>;
+  using EnginePtr = std::shared_ptr<SubscriptionEngine<std::string_view,
+                                                       SubscriptionTargetMock,
+                                                       std::string_view,
+                                                       int32_t>>;
+  using TargetPtr = std::shared_ptr<Subscriber<std::string_view,
+                                               SubscriptionTargetMock,
+                                               std::string_view,
+                                               int32_t>>;
 
   EnginePtr engine_;
   std::string test_data = "test_123";
   std::string key = "key";
 
   void SetUp() override {
-    engine_ = std::make_shared<SubscriptionEngine<std::string_view, SubscriptionTargetMock, std::string_view, int32_t>>();
+    engine_ = std::make_shared<SubscriptionEngine<std::string_view,
+                                                  SubscriptionTargetMock,
+                                                  std::string_view,
+                                                  int32_t>>();
   }
 };
 
@@ -35,18 +44,21 @@ class SubscriptionEngineTest : public testing::Test {
 TEST_F(SubscriptionEngineTest, SubscriberRegistration) {
   SubscriptionTargetMock target;
 
-    std::string_view data_1(test_data);
-    int32_t data_2 = 105;
+  std::string_view data_1(test_data);
+  int32_t data_2 = 105;
 
-    auto subscriber = std::make_shared<Subscriber<std::string_view, SubscriptionTargetMock, std::string_view, int32_t>>(engine_);
-    subscriber->set_callback([&](std::string_view data_1, int32_t data_2) {
-      target.test_call(data_1, data_2);
-    });
+  auto subscriber = std::make_shared<Subscriber<std::string_view,
+                                                SubscriptionTargetMock,
+                                                std::string_view,
+                                                int32_t>>(engine_);
+  subscriber->set_callback([&](std::string_view data_1, int32_t data_2) {
+    target.test_call(data_1, data_2);
+  });
 
-    EXPECT_CALL(target, test_call(data_1, data_2));
+  EXPECT_CALL(target, test_call(data_1, data_2));
 
-    subscriber->subscribe(key);
-    engine_->notify(key, data_1, data_2);
+  subscriber->subscribe(key);
+  engine_->notify(key, data_1, data_2);
 }
 
 /**
@@ -60,10 +72,12 @@ TEST_F(SubscriptionEngineTest, NegSubscriberRegistration) {
   std::string_view data_1(test_data);
   int32_t data_2 = 105;
 
-  auto subscriber = std::make_shared<Subscriber<std::string_view, SubscriptionTargetMock, std::string_view, int32_t>>(engine_);
-  subscriber->set_callback([&](std::string_view data_1, int32_t data_2) {
-    ASSERT_FALSE(true);
-  });
+  auto subscriber = std::make_shared<Subscriber<std::string_view,
+                                                SubscriptionTargetMock,
+                                                std::string_view,
+                                                int32_t>>(engine_);
+  subscriber->set_callback(
+      [&](std::string_view data_1, int32_t data_2) { ASSERT_FALSE(true); });
 
   subscriber->subscribe("100");
   engine_->notify(key, data_1, data_2);
--- test/mock/core/subscription/subscriber_mock.hpp
+++ test/mock/core/subscription/subscriber_mock.hpp
@@ -10,11 +10,9 @@ namespace kagome::subscription {
 
   class SubscriptionTargetMock final {
    public:
-    MOCK_METHOD2(test_call,
-    void(std::string_view data_1,
-    int32_t data_2));
+    MOCK_METHOD2(test_call, void(std::string_view data_1, int32_t data_2));
   };
 
-}  // namespace kagome::transaction_pool
+}  // namespace kagome::subscription
 
 #endif  // KAGOME_TEST_MOCK_SUBSCRIBER_MOCK_HPP
