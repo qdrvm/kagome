4be342f721d1d25f8893cf0643aa14f28949b837
Harrm <shcherba@soramitsu.co.jp>
2020-08-10 13:36:16 +0300

    Try to fix travis ci

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -5,5 +5,6 @@ pip3 install --user pyyaml
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install scikit-build
 sudo python3 -m pip install cmake requests gitpython gcovr
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-05-30 --profile minimal
 export PATH="$HOME/.cargo/bin:$PATH"
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-05-30 --profile minimal
+source $HOME/.cargo/env
