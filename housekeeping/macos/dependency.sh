0e283ff1fe906184ee3a33ca999bab4d88efd885
Harrm <shcherba@soramitsu.co.jp>
2020-08-10 12:25:35 +0300

    Fix typo in github actions config and try to fix macos build on travis

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -5,5 +5,5 @@ pip3 install --user pyyaml
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install scikit-build
 sudo python3 -m pip install cmake requests gitpython gcovr
-export PATH="$HOME/.cargo/bin:$PATH"
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-05-30 --profile minimal
+export PATH="$HOME/.cargo/bin:$PATH"
