acee53ad09e406f729bf11b81eeeee76738f8152
Harrm <shcherba@soramitsu.co.jp>
2020-08-10 11:11:10 +0300

    Return soramitsu-hunter master

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -31,7 +31,7 @@ set(
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
 
 HunterGate(
-    URL https://github.com/soramitsu/soramitsu-hunter/archive/fix/disable-kagome-testing.zip
+    URL https://github.com/soramitsu/soramitsu-hunter/archive/v0.23.254-soramitsu1.tar.gz
     SHA1 7819e7dd14f58b6df2fd7f2bc835265d60add3f5
     FILEPATH "${CMAKE_SOURCE_DIR}/cmake/Hunter/config.cmake"
 )
