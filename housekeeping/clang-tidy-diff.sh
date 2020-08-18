5af61932b0f202f7874f948abdb52dbaa39b9b9a
Roman Gavrilov <gavrilov@soramitsu.co.jp>
2020-08-18 19:03:07 +0300

    fixed clang-tidy for CI

--- housekeeping/clang-tidy-diff.sh
+++ housekeeping/clang-tidy-diff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
 cd $(dirname $0)/..
-git diff -U0 HEAD^ | clang-tidy-diff.py -p1 -path $BUILD_DIR -header-filter "core/.*\.hpp"
+git diff -U0 HEAD^ | clang-tidy-diff.py -p1 -path $BUILD_DIR -regex "core/.*\.hpp"
