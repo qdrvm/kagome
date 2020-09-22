8d08cb72fe181c985ca2fdd4918545b784c4763b
Harrm <shcherba@soramitsu.co.jp>
2020-09-22 17:15:01 +0300

    Fix getNode method in trie (#534)
    * Fix error on node startup
    
    * Fix error on node startup
    
    * Revert debug changes
    
    * Revert "Revert "Refactor Trie Cursor (#510)" (#533)"
    
    This reverts commit a6959ff8a8bce179dc15de36ff9517a8fda18819.
    
    * Fix merge issues
    
    * Add test for this bug
    
    * Add whitespace

--- core/storage/trie/impl/trie_storage_impl.cpp
+++ core/storage/trie/impl/trie_storage_impl.cpp
@@ -77,7 +77,7 @@ namespace kagome::storage::trie {
         serializer_,
         changes_,
         std::move(trie_res.value()),
-        [this](auto const &new_root) {
+        [this](const auto &new_root) {
           root_hash_ = new_root;
           logger_->debug("Update state root: {}", root_hash_);
         });
@@ -106,7 +106,7 @@ namespace kagome::storage::trie {
         serializer_,
         changes_,
         std::move(trie_res.value()),
-        [this](auto const &new_root) {
+        [this](const auto &new_root) {
           root_hash_ = new_root;
           logger_->debug("Update state root: {}", root_hash_);
         });
--- core/storage/trie/polkadot_trie/polkadot_trie_cursor.hpp
+++ core/storage/trie/polkadot_trie/polkadot_trie_cursor.hpp
@@ -9,8 +9,7 @@
 #include "storage/face/map_cursor.hpp"
 
 #include "common/buffer.hpp"
-#include "storage/trie/polkadot_trie/polkadot_trie.hpp"
-#include "storage/trie/serialization/polkadot_codec.hpp"
+#include "storage/trie/polkadot_trie/polkadot_node.hpp"
 
 namespace kagome::storage::trie {
 
@@ -19,73 +18,22 @@ namespace kagome::storage::trie {
    public:
     using NodePtr = std::shared_ptr<PolkadotNode>;
     using BranchPtr = std::shared_ptr<BranchNode>;
-    using NodeType = PolkadotNode::Type;
 
-    enum class Error {
-      INVALID_CURSOR_POSITION =
-          1,  // operation cannot be performed for cursor position is not valid
-      // due to an error, reaching the end or not calling next() after
-      // initialization
-      NOT_FOUND,  // the sought value is not found
-      NULL_ROOT,  // the root of the supplied trie is null
-      METHOD_NOT_IMPLEMENTED
-    };
-
-    explicit PolkadotTrieCursor(const PolkadotTrie &trie);
     ~PolkadotTrieCursor() override = default;
 
-    static outcome::result<std::unique_ptr<PolkadotTrieCursor>> createAt(
-        const common::Buffer &key, const PolkadotTrie &trie);
-
-    outcome::result<void> seekToFirst() override;
-    outcome::result<void> seek(const common::Buffer &key) override;
-    outcome::result<void> seekToLast() override;
-    bool isValid() const override;
-    outcome::result<void> next() override;
-    outcome::result<void> prev() override;
-    outcome::result<common::Buffer> key() const override;
-    outcome::result<common::Buffer> value() const override;
-
-   private:
-    static int8_t getNextChildIdx(const BranchPtr &parent, uint8_t child_idx);
-    static bool hasNextChild(const BranchPtr &parent, uint8_t child_idx);
-
-    static int8_t getPrevChildIdx(const BranchPtr &parent, uint8_t child_idx);
-    static bool hasPrevChild(const BranchPtr &parent, uint8_t child_idx);
-
-    // will either put a new entry or update the top entry (in case that parent
-    // in the top entry is the same as \param parent
-    void updateLastVisitedChild(const BranchPtr &parent, uint8_t child_idx);
+    /**
+     * Seek the first element with key not less than \arg key
+     * @return true if the trie is not empty
+     */
+    virtual outcome::result<void> seekLowerBound(const common::Buffer &key) = 0;
 
     /**
-     * An element of a path in trie. A node that is a part of the path and the
-     * index of its child which is the next node in the path
+     * Seek the first element with key greater than \arg key
+     * @return true if the trie is not empty
      */
-    struct TriePathEntry {
-      TriePathEntry(BranchPtr parent, int8_t child_idx)
-          : parent{std::move(parent)}, child_idx{child_idx} {}
-      BranchPtr parent;
-      int8_t child_idx;
-    };
-
-    /**
-     * Constructs a list of branch nodes on the path from the root to the node
-     * with the given \arg key
-     */
-    auto constructLastVisitedChildPath(const common::Buffer &key)
-        -> outcome::result<std::list<TriePathEntry>>;
-
-    common::Buffer collectKey() const;
-
-    const PolkadotTrie &trie_;
-    PolkadotCodec codec_;
-    NodePtr current_;
-    bool visited_root_ = false;
-    std::list<TriePathEntry> last_visited_child_;
+    virtual outcome::result<void> seekUpperBound(const common::Buffer &key) = 0;
   };
 
 }  // namespace kagome::storage::trie
-
-OUTCOME_HPP_DECLARE_ERROR(kagome::storage::trie, PolkadotTrieCursor::Error);
 
 #endif  // KAGOME_CORE_STORAGE_TRIE_POLKADOT_TRIE_POLKADOT_TRIE_CURSOR
