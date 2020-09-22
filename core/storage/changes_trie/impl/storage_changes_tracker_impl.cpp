ae6d2a949609a792c8304dc0640ca9aac7b029a6
Harrm <abrehchs@gmail.com>
2020-09-22 15:28:29 +0300

    Revert debug changes

--- core/storage/changes_trie/impl/storage_changes_tracker_impl.cpp
+++ core/storage/changes_trie/impl/storage_changes_tracker_impl.cpp
@@ -52,8 +52,7 @@ namespace kagome::storage::changes_trie {
       const common::Buffer &value,
       bool is_new_entry) {
     auto change_it = extrinsics_changes_.find(key);
-    auto r = get_extrinsic_index_();
-    OUTCOME_TRY(idx_bytes, r);
+    OUTCOME_TRY(idx_bytes, get_extrinsic_index_());
     OUTCOME_TRY(idx, scale::decode<primitives::ExtrinsicIndex>(idx_bytes));
 
     // if key was already changed in the same block, just add extrinsic to
