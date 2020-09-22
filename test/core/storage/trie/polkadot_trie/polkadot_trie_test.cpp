64e3b32758145fb02e4f0389c9ea75182d8ae9fa
Harrm <abrehchs@gmail.com>
2020-09-22 15:56:56 +0300

    Add test for this bug

--- test/core/storage/trie/polkadot_trie/polkadot_trie_test.cpp
+++ test/core/storage/trie/polkadot_trie/polkadot_trie_test.cpp
@@ -383,3 +383,23 @@ TEST_F(TrieTest, GetPathToInvalid) {
   EXPECT_OUTCOME_SOME_ERROR(
       path, trie->getPath(trie->getRoot(), KeyNibbles{"0a0b0c0d0e0f"_hex2buf}));
 }
+
+/**
+ * @given a trie
+ * @when searching with getNode() for a non present key
+ * @then nullptr is returned
+ */
+TEST_F(TrieTest, GetNodeReturnsNullptrWhenNotFound) {
+  const std::vector<std::pair<Buffer, Buffer>> data = {
+      {"123456"_hex2buf, "42"_hex2buf},
+      {"1234"_hex2buf, "1234"_hex2buf},
+      {"010203"_hex2buf, "0a0b"_hex2buf},
+      {"010a0b"_hex2buf, "1337"_hex2buf},
+      {"0a0b0c"_hex2buf, "deadbeef"_hex2buf}};
+
+  for (auto &entry : TrieTest::data) {
+    EXPECT_OUTCOME_TRUE_1(trie->put(entry.first, entry.second));
+  }
+  EXPECT_OUTCOME_TRUE(res, trie->getNode(trie->getRoot(), KeyNibbles{"01020304050607"_hex2buf}));
+  ASSERT_EQ(res, nullptr) << res->value->toHex();
+}
