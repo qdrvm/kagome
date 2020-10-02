3ede49ea5261f1e046bb10cec2791f14a8cc5a73
Dmitriy Khaustov <khaustov.dm@gmail.com>
2020-10-02 11:36:30 +0300

    Transactions in runtime APIs (#547)
    * feature: transaction apis
    * fix: outcome testutil and affected tests
    * feature: trie_storage_provider_test
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/extensions/impl/extension_impl.hpp
+++ core/extensions/impl/extension_impl.hpp
@@ -82,6 +82,12 @@ namespace kagome::extensions {
 
     void ext_storage_root(runtime::WasmPointer result) const override;
 
+    void ext_storage_start_transaction() override;
+
+    void ext_storage_rollback_transaction() override;
+
+    void ext_storage_commit_transaction() override;
+
     // ------------------------ Storage extensions v1 ------------------------
 
     void ext_storage_set_version_1(runtime::WasmSpan key,
