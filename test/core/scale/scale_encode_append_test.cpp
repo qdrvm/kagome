c90dda026112c6aedcaa308b61e7f5f0e2ffcf8b
kamilsa <kamilsa16@gmail.com>
2020-10-06 18:26:41 +0300

    Fix tests
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

--- test/core/scale/scale_encode_append_test.cpp
+++ test/core/scale/scale_encode_append_test.cpp
@@ -14,22 +14,22 @@ using ::testing::ElementsAre;
 namespace kagome::scale {
 
   TEST(EncodeAppend, Append) {
-    std::vector<uint32_t> inp1({1, 2, 3, 4, 5});
-    EncodeOpaqueValue inp1_eov{scale::encode(inp1).value()};
+    auto inp1 = scale::encode(std::vector<uint32_t>({1, 2, 3, 4, 5})).value();
+    EncodeOpaqueValue inp1_eov{inp1};
 
     std::vector<uint8_t> res{};
-    auto encoded_concat_res =
-        append_or_new_vec(res, scale::encode(inp1_eov).value());
+    auto inp1_encoded = scale::encode(inp1_eov).value();
+    auto encoded_concat_res = append_or_new_vec(res, inp1_encoded);
 
     ASSERT_TRUE(encoded_concat_res.has_value());
     ASSERT_THAT(res, ContainerEq(std::vector<uint8_t>({4, 20, 1, 0, 0, 0, 2, 0,
                                                        0, 0,  3, 0, 0, 0, 4, 0,
                                                        0, 0,  5, 0, 0, 0})));
 
-    EncodeOpaqueValue inp2_eov{scale::encode(uint32_t{2}).value()};
-    auto aa = scale::encode(inp2_eov).value();
-    encoded_concat_res =
-        append_or_new_vec(res, aa);
+    auto inp2 = scale::encode(uint32_t{2}).value();
+    EncodeOpaqueValue inp2_eov{inp2};
+    auto inp2_encoded = scale::encode(inp2_eov).value();
+    encoded_concat_res = append_or_new_vec(res, inp2_encoded);
 
     ASSERT_TRUE(encoded_concat_res.has_value());
     ASSERT_THAT(res,
