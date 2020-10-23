f51f5847858830d050b0d507fdb317d79716ce16
iceseer <iceseer@gmail.com>
2020-10-23 12:00:55 +0300

    refactoring messages
    Signed-off-by: iceseer <iceseer@gmail.com>

---
+++ core/network/messages/messages.hpp
@@ -0,0 +1,36 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_MESSAGES_MESSAGES_HPP
+#define KAGOME_MESSAGES_MESSAGES_HPP
+
+#include <functional>
+#include <memory>
+#include <gsl/span>
+#include <boost/system/error_code.hpp>
+
+#include "network/adapters/adapter_errors.hpp"
+#include "network/protobuf/api.v1.pb.h"
+#include "outcome/outcome.hpp"
+
+namespace kagome::network {
+
+  template <typename T, typename RW>
+  struct Message {
+    using ReadWriter = RW;
+    T data;
+  };
+
+  template <typename... T>
+  struct MessagesSequence {
+    std::tuple<T...> msgs;
+
+    template <typename Q>
+    MessagesSequence(Q &&... args) : msgs{std::forward<Q>(args)...} {}
+  };
+
+}  // namespace kagome::network
+
+#endif  // KAGOME_MESSAGES_MESSAGES_HPP
