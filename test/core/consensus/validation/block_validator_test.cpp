b11b596950f33f97ab39471fe3f86e4176eb940d
Igor Krasheninnikov <igor.krash@gmail.com>
2020-10-29 18:30:53 +0300

    - verifyProcer in block_validator
    Signed-off-by: Igor Krasheninnikov <igor.krash@gmail.com>

--- test/core/consensus/validation/block_validator_test.cpp
+++ test/core/consensus/validation/block_validator_test.cpp
@@ -369,60 +369,6 @@ TEST_F(BlockValidatorTest, ThresholdGreater) {
 
 /**
  * @given block validator
- * @when validating block, produced by a peer, which has already produced
- * another block in that slot
- * @then validation fails
- */
-TEST_F(BlockValidatorTest, TwoBlocksByOnePeer) {
-  // GIVEN
-  auto block_copy = valid_block_;
-  block_copy.header.digest.pop_back();
-  auto encoded_block_copy = scale::encode(block_copy.header).value();
-  Hash256 encoded_block_copy_hash{};
-  std::copy(encoded_block_copy.begin(),
-            encoded_block_copy.begin() + Hash256::size(),
-            encoded_block_copy_hash.begin());
-
-  auto [seal, pubkey] = sealBlock(valid_block_, encoded_block_copy_hash);
-
-  EXPECT_CALL(*hasher_, blake2b_256(_))
-      .Times(2)
-      .WillRepeatedly(Return(encoded_block_copy_hash));
-
-  EXPECT_CALL(*sr25519_provider_, verify(_, _, pubkey))
-      .Times(2)
-      .WillRepeatedly(Return(outcome::result<bool>(true)));
-
-  babe_epoch_.authorities.emplace_back();
-  auto authority = Authority{{pubkey}, 42};
-  babe_epoch_.authorities.emplace_back(authority);
-
-  auto randomness_with_slot =
-      Buffer{}.put(babe_epoch_.randomness).put(uint64_t_to_bytes(slot_number_));
-  EXPECT_CALL(*vrf_provider_, verify(randomness_with_slot, _, pubkey, _))
-      .Times(2)
-      .WillRepeatedly(
-          Return(VRFVerifyOutput{.is_valid = true, .is_less = true}));
-
-  EXPECT_CALL(*tx_queue_,
-              validate_transaction(TransactionSource::InBlock, ext_))
-      .WillOnce(Return(ValidTransaction{}));
-
-  // WHEN
-  auto validate_res = validator_.validateBlock(
-      valid_block_, authority.id, threshold_, randomness_);
-  ASSERT_TRUE(validate_res) << validate_res.error().message();
-
-  // THEN
-  EXPECT_OUTCOME_FALSE(
-      err,
-      validator_.validateBlock(
-          valid_block_, authority.id, threshold_, randomness_));
-  ASSERT_EQ(err, BabeBlockValidator::ValidationError::TWO_BLOCKS_IN_SLOT);
-}
-
-/**
- * @given block validator
  * @when validating block, which contains an invalid extrinsic
  * @then validation fails
  */
