cfa9568818b8ab9405fecf48effab2727821132a
Harrm <abrehchs@gmail.com>
2020-10-29 13:44:47 +0300

    Update rust version in docker

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -6,4 +6,4 @@ sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install scikit-build
 sudo python3 -m pip install cmake requests gitpython gcovr
 export PATH="$HOME/.cargo/bin:$PATH"
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2019-10-16 --profile minimal
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-09-24 --profile minimal
