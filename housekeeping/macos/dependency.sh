235283c783d9c612ab0c6b0814bc0f96a7513f04
Harrm <abrehchs@gmail.com>
2020-10-30 11:26:56 +0300

    Fixes for CI

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -7,3 +7,6 @@ sudo python3 -m pip install scikit-build
 sudo python3 -m pip install cmake requests gitpython gcovr
 export PATH="$HOME/.cargo/bin:$PATH"
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-09-24 --profile minimal
+# suppress shellcheck warning
+# shellcheck source=/dev/null
+source $HOME/.cargo/env
