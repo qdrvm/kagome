5453f4ed10d842337c0a29bf6976e4567d667ab7
Harrm <abrehchs@gmail.com>
2020-10-30 11:58:51 +0300

    Add rust to Travis PATH

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -5,8 +5,4 @@ pip3 install --user pyyaml
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install scikit-build
 sudo python3 -m pip install cmake requests gitpython gcovr
-export PATH="$HOME/.cargo/bin:$PATH"
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-09-24 --profile minimal
-# suppress shellcheck warning
-# shellcheck source=/dev/null
-source $HOME/.cargo/env
