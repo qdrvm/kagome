0651a474c0e09aba755ddb9f4f667e548891b278
Harrm <abrehchs@gmail.com>
2020-11-16 17:26:15 +0300

    Fix authorities

--- core/primitives/session_key.hpp
+++ core/primitives/session_key.hpp
@@ -3,14 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef KAGOME_CORE_PRIMITIVES_SESSION_KEY_HPP
-#define KAGOME_CORE_PRIMITIVES_SESSION_KEY_HPP
+#ifndef KAGOME_SESSION_KEY_HPP
+#define KAGOME_SESSION_KEY_HPP
 
-#include "common/blob.hpp"
-
-namespace kagome::primitives {
-  // TODO(akvinikym): must be a SR25519 key
-  using SessionKey = common::Blob<32>;
-}  // namespace kagome::primitives
-
-#endif  // KAGOME_CORE_PRIMITIVES_SESSION_KEY_HPP
+#endif //KAGOME_SESSION_KEY_HPP
