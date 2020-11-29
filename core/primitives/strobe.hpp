de31439d2f2887e454b3509c9d60a15e5eb42f11
iceseer <iceseer@gmail.com>
2020-11-30 00:09:49 +0300

    strobe
    Signed-off-by: iceseer <iceseer@gmail.com>

--- core/primitives/strobe.hpp
+++ core/primitives/strobe.hpp
@@ -17,17 +17,13 @@
 
 namespace kagome::primitives {
 
-#ifndef KECCAKF_ROUNDS
-#define KECCAKF_ROUNDS 24
-#endif
-
 #ifndef ROTL64
 #define ROTL64(x, y) (((x) << (y)) | ((x) >> (64 - (y))))
 #endif
 
   void sha3_keccakf(uint64_t st[25]) {
     // constants
-    const uint64_t keccakf_rndc[24] = {
+    constexpr uint64_t keccakf_rndc[24] = {
         0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
         0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
         0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
@@ -36,11 +32,12 @@ namespace kagome::primitives {
         0x8000000000008003, 0x8000000000008002, 0x8000000000000080,
         0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
         0x8000000000008080, 0x0000000080000001, 0x8000000080008008};
-    const int keccakf_rotc[24] = {1,  3,  6,  10, 15, 21, 28, 36,
-                                  45, 55, 2,  14, 27, 41, 56, 8,
-                                  25, 43, 62, 18, 39, 61, 20, 44};
-    const int keccakf_piln[24] = {10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4,
-                                  15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1};
+    constexpr int keccakf_rotc[24] = {1,  3,  6,  10, 15, 21, 28, 36,
+                                      45, 55, 2,  14, 27, 41, 56, 8,
+                                      25, 43, 62, 18, 39, 61, 20, 44};
+    constexpr int keccakf_piln[24] = {10, 7,  11, 17, 18, 3,  5,  16,
+                                      8,  21, 24, 4,  15, 23, 19, 13,
+                                      12, 2,  20, 14, 22, 9,  6,  1};
 
     // variables
     int i, j, r;
@@ -60,7 +57,7 @@ namespace kagome::primitives {
 #endif
 
     // actual iteration
-    for (r = 0; r < KECCAKF_ROUNDS; r++) {
+    for (r = 0; r < 24; r++) {
       // Theta
       for (i = 0; i < 5; i++)
         bc[i] = st[i] ^ st[i + 5] ^ st[i + 10] ^ st[i + 15] ^ st[i + 20];
