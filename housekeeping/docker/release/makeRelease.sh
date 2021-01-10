a7cf73aa43d45c8539ec7f5ae409a6e2ded048cc
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2021-01-10 19:50:43 +0300

    fix: docker scripts
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- housekeeping/docker/release/makeRelease.sh
+++ housekeeping/docker/release/makeRelease.sh
@@ -6,4 +6,4 @@ BUILD_THREADS="${BUILD_THREADS:-$(( $(nproc 2>/dev/null || sysctl -n hw.ncpu) + 
 cd "$(dirname $0)/../../.."
 
 cmake . -B${BUILD_DIR} -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=Release
-cmake --build "${BUILD_DIR}" --target kagome_full_syncing kagome_validating kagome_block_producing -- -j${BUILD_THREADS}
+cmake --build "${BUILD_DIR}" --target kagome_full_syncing kagome_validating -- -j${BUILD_THREADS}
