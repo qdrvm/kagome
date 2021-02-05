d5318c1c1899c2d6e4a37af1117952d33e204fb3
Harrm <abrehchs@gmail.com>
2021-02-05 11:32:07 +0300

    Fix from review

--- core/runtime/binaryen/runtime_api/transaction_payment_api_impl.cpp
+++ core/runtime/binaryen/runtime_api/transaction_payment_api_impl.cpp
@@ -7,4 +7,14 @@
 
 namespace kagome::runtime {
 
+outcome::result<primitives::RuntimeDispatchInfo> TransactionPaymentApiImpl::query_info(
+        const primitives::Extrinsic &ext, uint32_t len) {
+      // len = 139
+      // 0x84..0f, 137 bytes
+      return execute<primitives::RuntimeDispatchInfo>(
+          "TransactionPaymentApi_query_info",
+          CallPersistency::EPHEMERAL,
+          gsl::span<const uint8_t>(ext.data.data(), ext.data.size()),
+          len);
+    }
 }
