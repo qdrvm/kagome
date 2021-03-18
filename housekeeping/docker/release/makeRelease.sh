a1e2d55f03f80fd7a09d7579412ec7510fe35094
Harrm <abrehchs@gmail.com>
2021-03-18 19:22:54 +0300

    Fix makeRelease

--- housekeeping/docker/release/makeRelease.sh
+++ housekeeping/docker/release/makeRelease.sh
@@ -5,5 +5,5 @@ BUILD_THREADS="${BUILD_THREADS:-$(( $(nproc 2>/dev/null || sysctl -n hw.ncpu) + 
 
 cd "$(dirname $0)/../../.."
 
-cmake . -B${BUILD_DIR} -G 'Ninja' -DCMAKE_BUILD_TYPE=Release
+cmake . -B${BUILD_DIR} -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE=Release
 cmake --build "${BUILD_DIR}" --target kagome_full_syncing kagome_validating -- -j${BUILD_THREADS}
