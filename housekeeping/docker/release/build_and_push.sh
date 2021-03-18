4e0c7360eae4a11968062cf3bfa7a878fdebeb65
Harrm <abrehchs@gmail.com>
2021-03-18 19:14:04 +0300

    Added dockerfiles for minideb-based images

--- housekeeping/docker/release/build_and_push.sh
+++ housekeeping/docker/release/build_and_push.sh
@@ -14,7 +14,7 @@ else
   VERSION=$VERSION
 fi
 
-TAG=soramitsu/kagome:$VERSION
+TAG=harrm/kagome:$VERSION
 
 CTX_DIR=${BUILD_DIR}/docker_context
 
@@ -27,11 +27,11 @@ cp -a ${BUILD_DIR}/node/kagome_full_syncing/kagome_full_syncing ${CTX_DIR}/
 cp -a ${BUILD_DIR}/node/kagome_validating/kagome_validating  ${CTX_DIR}/
 
 # Strip binaries
-strip ${CTX_DIR}/kagome_full_syncing
-strip ${CTX_DIR}/kagome_validating
+# strip ${CTX_DIR}/kagome_full_syncing
+# strip ${CTX_DIR}/kagome_validating
 
 # Make build
-docker build -t $TAG -f housekeeping/docker/release/Dockerfile ${CTX_DIR}
+docker build -t $TAG -f housekeeping/docker/release/Dockerfile-minideb ${CTX_DIR}
 docker push $TAG
 
 rm -R ${CTX_DIR}
