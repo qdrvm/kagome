09ff7fae6e7120f05408667dcb2bc5e64b69305b
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2021-03-19 09:37:09 +0300

    fix: checksum
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -17,6 +17,6 @@
 
 hunter_config(libp2p
     URL  https://github.com/libp2p/cpp-libp2p/archive/ceff681e0b3dbfab4792be3dabcb064fff14a8ff.tar.gz
-    SHA1 78fb38f212fa49029aff24c669a39648d9b4e68b
+    SHA1 e917ca20e1adec5a8ac0c4bc3969fe4fab3ddf5d
     CMAKE_ARGS TESTING=OFF EXAMPLES=OFF
     )
