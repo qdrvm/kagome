06b8c6c39da8f8ec73c802cace074b1ebe69f441
Harrm <abrehchs@gmail.com>
2021-03-25 12:19:44 +0300

    Replace ubuntu wth minideb

--- housekeeping/docker/kagome-dev/build_and_push.sh
+++ housekeeping/docker/kagome-dev/build_and_push.sh
@@ -5,8 +5,5 @@ cd "$(dirname "$0")"
 VERSION=1
 TAG=harrm/kagome-dev:$VERSION
 
-docker build -t ${TAG}-ubuntu -f ubuntu.Dockerfile .
-docker push ${TAG}-ubuntu
-
-docker build -t ${TAG}-minideb -f minideb.Dockerfile .
+docker build -t ${TAG}-minideb -f minideb-full.Dockerfile .
 docker push ${TAG}-minideb
