eb6cafd89f785e2470e481d265294d9ad9a6d6d5
Harrm <abrehchs@gmail.com>
2021-06-01 09:17:38 +0300

    Fixing CI

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -6,5 +6,3 @@ sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install scikit-build
 sudo python3 -m pip install cmake requests gitpython gcovr
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-09-24 --profile minimal
-
-cd /usr/local && find . -name "LLVMConfig.cmake"
