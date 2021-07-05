4719b0267ecf50b78bfa2d3552e349de1cbc7a0a
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2021-07-05 12:24:12 +0300

    feature: logging protocols
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/network/protocols/gossip_protocol.hpp
+++ core/network/protocols/gossip_protocol.hpp
@@ -70,7 +70,7 @@ namespace kagome::network {
     const OwnPeerInfo &own_info_;
     std::shared_ptr<StreamEngine> stream_engine_;
     const libp2p::peer::Protocol protocol_;
-    log::Logger log_ = log::createLogger("GossipProtocol");
+    log::Logger log_ = log::createLogger("GossipProtocol", "protocols");
   };
 
 }  // namespace kagome::network
