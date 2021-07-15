38d7c8e6f7131fc4705753a79dada0cdf0eef67e
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2021-07-15 14:44:53 +0300

    fix: return trace log messages in release buil
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -14,3 +14,11 @@
 #     SHA1 1234567890abcdef1234567890abcdef12345678
 #     CMAKE_ARGS "CMAKE_VARIABLE=value"
 # )
+
+hunter_config(
+    soralog
+    URL https://github.com/xDimon/soralog/archive/e9f538034324c022869e6c1fe0cce3014737200b.tar.gz
+    SHA1 d1a49c9f0c06d6bb07e5ab865af528351923ee9e
+    CMAKE_ARGS TESTING=OFF EXAMPLES=OFF EXPOSE_MOCKS=ON
+    KEEP_PACKAGE_SOURCES
+)
