64af52df6d6b686faba921ddf5048e5d51be96da
Alexander Krutikov <krutikov@soramitsu.co.jp>
2021-07-27 14:56:55 +0300

    fix comments

--- core/runtime/common/uncompress_code_if_needed.cpp
+++ core/runtime/common/uncompress_code_if_needed.cpp
@@ -13,24 +13,25 @@ namespace kagome::runtime {
 
   // @see
   // https://github.com/paritytech/substrate/blob/polkadot-v0.9.8/primitives/maybe-compressed-blob/src/lib.rs#L28
-  constexpr uint8_t kZstdPrefix[8] = {
+  constexpr uint8_t kZstdPrefixSize = 8;
+  constexpr uint8_t kZstdPrefix[kZstdPrefixSize] = {
       0x52, 0xBC, 0x53, 0x76, 0x46, 0xDB, 0x8E, 0x05};
   // @see
   // https://github.com/paritytech/substrate/blob/polkadot-v0.9.8/primitives/maybe-compressed-blob/src/lib.rs#L35
   constexpr size_t kCodeBlobBombLimit = 50 * 1024 * 1024;
 
   void uncompressCodeIfNeeded(const common::Buffer &buf, common::Buffer &res) {
-    if (buf.size() > 8
+    if (buf.size() > kZstdPrefixSize
         && std::equal(buf.begin(),
-                      buf.begin() + 8,
+                      buf.begin() + kZstdPrefixSize,
                       std::begin(kZstdPrefix),
                       std::end(kZstdPrefix))) {
       auto logger = log::createLogger("UncompressCodeIfNeeded", "wasm");
       // here we can check that blob is really ZSTD compressed
       // but we don't use the result size, it's unknown for the WASM blob
       // @see ZSTD_CONTENTSIZE_UNKNOWN
-      auto check_size =
-          ZSTD_getFrameContentSize(buf.data() + 8, buf.size() - 8);
+      auto check_size = ZSTD_getFrameContentSize(buf.data() + kZstdPrefixSize,
+                                                 buf.size() - kZstdPrefixSize);
       // error here would mean an update of codebase
       // so we leave it to try the previous runtime if any
       if (check_size == ZSTD_CONTENTSIZE_ERROR) {
@@ -38,8 +39,10 @@ namespace kagome::runtime {
         return;
       }
       res.resize(kCodeBlobBombLimit);
-      auto size = ZSTD_decompress(
-          res.data(), kCodeBlobBombLimit, buf.data() + 8, buf.size() - 8);
+      auto size = ZSTD_decompress(res.data(),
+                                  kCodeBlobBombLimit,
+                                  buf.data() + kZstdPrefixSize,
+                                  buf.size() - kZstdPrefixSize);
       res.resize(size);
     } else {
       res = buf;
