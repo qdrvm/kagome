d01206769f1cc887584cc5ab3130a74170a22890
kamilsa <kamilsa16@gmail.com>
2021-08-19 16:18:58 +0300

    Fix llvm version in mac

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -6,4 +6,5 @@ sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install scikit-build
 sudo python3 -m pip install cmake requests gitpython gcovr
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-09-24 --profile minimal
-brew install llvm
+
+brew install llvm@11
