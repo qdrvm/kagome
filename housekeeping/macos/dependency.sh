45b3dc731b1302b45f07c674598560bcb6f1d418
Alexander Krutikov <a.krutikov@innopolis.ru>
2021-08-19 15:27:24 +0300

    compile wavm statically

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -6,3 +6,4 @@ sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install scikit-build
 sudo python3 -m pip install cmake requests gitpython gcovr
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2020-09-24 --profile minimal
+brew install llvm
