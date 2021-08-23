203df191227ce0027867990a3cd9e14290162075
Harrm <abrehchs@gmail.com>
2021-08-23 14:22:12 +0300

    Remove redundant profiler logs

--- core/runtime/binaryen/instance_environment_factory.cpp
+++ core/runtime/binaryen/instance_environment_factory.cpp
@@ -29,27 +29,17 @@ namespace kagome::runtime::binaryen {
   }
 
   BinaryenInstanceEnvironment InstanceEnvironmentFactory::make() const {
-    SL_PROFILE_START(memory_provider)
     auto memory_factory = std::make_shared<BinaryenMemoryFactory>();
     auto new_memory_provider =
         std::make_shared<BinaryenMemoryProvider>(memory_factory);
-    SL_PROFILE_END(memory_provider)
-    SL_PROFILE_START(storage_provider)
     auto new_storage_provider =
         std::make_shared<TrieStorageProviderImpl>(storage_);
-    SL_PROFILE_END(storage_provider)
-    SL_PROFILE_START(core_factory)
     auto core_factory = std::make_shared<CoreApiFactoryImpl>(
         shared_from_this(), block_header_repo_, changes_tracker_);
-    SL_PROFILE_END(core_factory)
-    SL_PROFILE_START(host_api)
     auto host_api = std::shared_ptr<host_api::HostApi>(host_api_factory_->make(
         core_factory, new_memory_provider, new_storage_provider));
-    SL_PROFILE_END(host_api)
-    SL_PROFILE_START(rei)
     auto rei = std::make_shared<RuntimeExternalInterface>(host_api);
     new_memory_provider->setExternalInterface(rei);
-    SL_PROFILE_END(rei)
     return BinaryenInstanceEnvironment{
         InstanceEnvironment{std::move(new_memory_provider),
                             std::move(new_storage_provider),
