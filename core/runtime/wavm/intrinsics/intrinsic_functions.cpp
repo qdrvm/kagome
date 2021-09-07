e3fb8fc0601b5a9b6184b4aa975c4077973165af
Harrm <abrehchs@gmail.com>
2021-09-07 11:57:03 +0300

    Add support for deprecated sr25519 signatures

--- core/runtime/wavm/intrinsics/intrinsic_functions.cpp
+++ core/runtime/wavm/intrinsics/intrinsic_functions.cpp
@@ -113,6 +113,15 @@ namespace kagome::runtime::wavm {
   }
 
   WAVM_DEFINE_INTRINSIC_FUNCTION(WAVM::I32,
+                                 ext_crypto_sr25519_verify_version_1,
+                                 WAVM::I32 sig_data,
+                                 WAVM::I64 msg,
+                                 WAVM::I32 pubkey_data) {
+    return peekHostApi()->ext_crypto_sr25519_verify_version_1(
+        sig_data, msg, pubkey_data);
+  }
+
+  WAVM_DEFINE_INTRINSIC_FUNCTION(WAVM::I32,
                                  ext_crypto_sr25519_verify_version_2,
                                  WAVM::I32 sig_data,
                                  WAVM::I64 msg,
@@ -435,6 +444,7 @@ namespace kagome::runtime::wavm {
     REGISTER_HOST_INTRINSIC(I32, ext_crypto_ed25519_verify_version_1, I32, I64, I32)
     REGISTER_HOST_INTRINSIC(I32, ext_crypto_finish_batch_verify_version_1)
     REGISTER_HOST_INTRINSIC(I32, ext_crypto_sr25519_generate_version_1, I32, I64)
+    REGISTER_HOST_INTRINSIC(I32, ext_crypto_sr25519_verify_version_1, I32, I64, I32)
     REGISTER_HOST_INTRINSIC(I32, ext_crypto_sr25519_verify_version_2, I32, I64, I32)
     REGISTER_HOST_INTRINSIC(I32, ext_hashing_blake2_128_version_1, I64)
     REGISTER_HOST_INTRINSIC(I32, ext_hashing_blake2_256_version_1, I64)
