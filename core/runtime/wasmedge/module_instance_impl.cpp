8245195649edd6d52896a307d082bd06cc8e736b
Alexander Krutikov <krutikov@soramitsu.co.jp>
2021-09-08 16:02:45 +0300

    add wasmedge module instance

---
+++ core/runtime/wasmedge/module_instance_impl.cpp
@@ -0,0 +1,57 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "runtime/wasmedge/module_instance_impl.hpp"
+
+#include <wasmedge.h>
+
+namespace kagome::runtime::wasmedge {
+
+  ModuleInstanceImpl::ModuleInstanceImpl(WasmEdge_ASTModuleContext *parent,
+                                         WasmEdge_ImportObjectContext *rei)
+      : logger_{log::createLogger("ModuleInstance", "wasmedge")} {
+    interpreter_ = WasmEdge_InterpreterCreate(nullptr, nullptr);
+    store_ = WasmEdge_StoreCreate();
+    auto ModName = WasmEdge_StringCreateByCString("ext");
+    auto Res = WasmEdge_InterpreterRegisterModule(
+        interpreter_, store_, parent, ModName);
+    Res = WasmEdge_InterpreterRegisterImport(interpreter_, store_, rei);
+    WasmEdge_StringDelete(ModName);
+  }
+
+  outcome::result<PtrSize> ModuleInstanceImpl::callExportFunction(
+      std::string_view name, PtrSize args) const {
+    auto FuncName = WasmEdge_StringCreateByCString(name.data());
+    WasmEdge_Value Params[2] = {WasmEdge_ValueGenI32(args.ptr),
+                                WasmEdge_ValueGenI32(args.size)};
+    WasmEdge_Value Returns[1];
+    auto Res = WasmEdge_InterpreterInvoke(
+        interpreter_, store_, FuncName, Params, 2, Returns, 1);
+    WasmEdge_StringDelete(FuncName);
+    return PtrSize{WasmEdge_ValueGetI64(Returns[0])};
+  }
+
+  outcome::result<boost::optional<WasmValue>> ModuleInstanceImpl::getGlobal(
+      std::string_view name) const {
+    auto GlobalName = WasmEdge_StringCreateByCString(name.data());
+    auto res = WasmEdge_StoreFindGlobal(store_, GlobalName);
+    auto type = WasmEdge_GlobalInstanceGetValType(res);
+    auto val = WasmEdge_GlobalInstanceGetValue(res);
+    WasmEdge_StringDelete(GlobalName);
+    switch (type) {
+      case WasmEdge_ValType_I32:
+        return WasmValue{WasmEdge_ValueGetI32(val)};
+      case WasmEdge_ValType_I64:
+        return WasmValue{WasmEdge_ValueGetI64(val)};
+      case WasmEdge_ValType_F32:
+        return WasmValue{WasmEdge_ValueGetF32(val)};
+      case WasmEdge_ValType_F64:
+        return WasmValue{WasmEdge_ValueGetF64(val)};
+      default:
+        return boost::none;
+    }
+  }
+
+}  // namespace kagome::runtime::wasmedge
---
+++ core/runtime/wasmedge/module_instance_impl.hpp
@@ -0,0 +1,38 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_CORE_RUNTIME_WASMEDGE_MODULE_INSTANCE_IMPL
+#define KAGOME_CORE_RUNTIME_WASMEDGE_MODULE_INSTANCE_IMPL
+
+#include "runtime/module_instance.hpp"
+
+#include "log/logger.hpp"
+
+struct WasmEdge_ASTModuleContext;
+struct WasmEdge_ImportObjectContext;
+struct WasmEdge_InterpreterContext;
+struct WasmEdge_StoreContext;
+
+namespace kagome::runtime::wasmedge {
+
+  class ModuleInstanceImpl final : public ModuleInstance {
+   public:
+    ModuleInstanceImpl(WasmEdge_ASTModuleContext *parent,
+                       WasmEdge_ImportObjectContext *rei);
+    outcome::result<PtrSize> callExportFunction(std::string_view name,
+                                                PtrSize args) const override;
+
+    outcome::result<boost::optional<WasmValue>> getGlobal(
+        std::string_view name) const override;
+
+   private:
+    WasmEdge_InterpreterContext *interpreter_;
+    WasmEdge_StoreContext *store_;
+    log::Logger logger_;
+  };
+
+}  // namespace kagome::runtime::wasmedge
+
+#endif  // KAGOME_CORE_RUNTIME_WASMEDGE_MODULE_INSTANCE_IMPL
