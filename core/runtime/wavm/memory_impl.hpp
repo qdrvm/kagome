7817d9b9ccf47e2a1f0e218a841389121275014e
Harrm <abrehchs@gmail.com>
2021-09-16 11:22:27 +0300

    Fix memory resize

--- core/runtime/wavm/memory_impl.hpp
+++ core/runtime/wavm/memory_impl.hpp
@@ -30,8 +30,7 @@ namespace kagome::runtime::wavm {
    public:
     MemoryImpl(WAVM::Runtime::Memory *memory,
                std::unique_ptr<MemoryAllocator> &&allocator);
-    MemoryImpl(WAVM::Runtime::Memory *memory,
-               WasmSize heap_base);
+    MemoryImpl(WAVM::Runtime::Memory *memory, WasmSize heap_base);
     MemoryImpl(const MemoryImpl &copy) = delete;
     MemoryImpl &operator=(const MemoryImpl &copy) = delete;
     MemoryImpl(MemoryImpl &&move) = delete;
@@ -106,7 +105,9 @@ namespace kagome::runtime::wavm {
        */
       if (new_size >= size()) {
         auto new_page_number = (new_size / kPageSize) + 1;
-        WAVM::Runtime::growMemory(memory_, new_page_number);
+        WAVM::Runtime::growMemory(
+            memory_,
+            new_page_number - WAVM::Runtime::getMemoryNumPages(memory_));
       }
     }
 
