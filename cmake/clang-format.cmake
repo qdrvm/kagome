8ae8cb28042ac4435e9ea62a47036fb7d765b66e
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2021-09-27 16:08:32 +0300

    feature: provide recommended version of clang-format
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -10,20 +10,44 @@ file(GLOB_RECURSE
 
 # Adding clang-format target if executable is found
 if(NOT CLANG_FORMAT_BIN)
-  foreach(CLANG_FORMAT_VERSION RANGE 20 7 -1)
-    find_program(CLANG_FORMAT_BIN NAMES clang-format-${CLANG_FORMAT_VERSION})
-    if(CLANG_FORMAT_BIN)
-      message(STATUS "Found clang-format ver. ${CLANG_FORMAT_VERSION}")
-      break()
-    endif()
-  endforeach()
+  # Find particular version
+  find_program(CLANG_FORMAT_BIN NAMES clang-format-${RECOMMENDED_CLANG_FORMAT_VERSION})
   if(NOT CLANG_FORMAT_BIN)
+    # Find default version and check it
     find_program(CLANG_FORMAT_BIN NAMES clang-format)
+    if(CLANG_FORMAT_BIN)
+      execute_process(
+          COMMAND ${CLANG_FORMAT_BIN} --version
+          COMMAND sed -r "s/.*version \([[:digit:]]+\).*/\\1/"
+          OUTPUT_VARIABLE DEFAULT_CLANG_FORMAT_VERSION
+      )
+      math(EXPR DEFAULT_CLANG_FORMAT_VERSION "0 + 0${DEFAULT_CLANG_FORMAT_VERSION}")
+      #Try to find newest version
+      foreach(CLANG_FORMAT_VERSION RANGE ${RECOMMENDED_CLANG_FORMAT_VERSION} ${DEFAULT_CLANG_FORMAT_VERSION} -1)
+        find_program(CLANG_FORMAT_BIN_ NAMES clang-format-${CLANG_FORMAT_VERSION})
+        if(CLANG_FORMAT_BIN_)
+          message(STATUS "Found clang-format version ${CLANG_FORMAT_VERSION}")
+          set(CLANG_FORMAT_BIN ${CLANG_FORMAT_BIN_})
+          break()
+        endif()
+      endforeach()
+    endif()
   endif()
 endif()
 
+execute_process(
+    COMMAND ${CLANG_FORMAT_BIN} --version
+    COMMAND sed -r "s/.*version \([[:digit:]]+\).*/\\1/"
+    OUTPUT_VARIABLE CLANG_FORMAT_VERSION
+)
+math(EXPR CLANG_FORMAT_VERSION "0 + 0${CLANG_FORMAT_VERSION}")
+if(NOT ${CLANG_FORMAT_VERSION} EQUAL ${RECOMMENDED_CLANG_FORMAT_VERSION})
+  message(WARNING "Found clang-format version ${CLANG_FORMAT_VERSION}, "
+      "but version ${RECOMMENDED_CLANG_FORMAT_VERSION} is recommended")
+endif()
+
 if(CLANG_FORMAT_BIN)
-  message(STATUS "Target clang-format enabled")
+  message(STATUS "Target clang-format is enabled")
   add_custom_target(
       clang-format
       COMMAND "${CLANG_FORMAT_BIN}"
