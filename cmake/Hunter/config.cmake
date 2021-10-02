ab0ebbc60be777a3641084c71215c6e6d406da58
Harrm <shcherba@soramitsu.co.jp>
2021-10-02 20:08:47 +0300

    Upgrade docker scripts to Debian bullseye  (#927)

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,6 +1,6 @@
 ## Template for a custom hunter configuration
 # Useful when there is a need for a non-default version or arguments of a dependency,
-# or when a project not registered in soramistu-hunter should be added.
+# or when a project not registered in soramitsu-hunter should be added.
 #
 # hunter_config(
 #     package-name
