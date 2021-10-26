80e2e80ff4a7750ee146b531bddce47e88ae1ee3
Harrm <shcherba@soramitsu.co.jp>
2021-10-26 17:12:14 +0300

    Update init.cmake

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -31,7 +31,7 @@ set(
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
 
 HunterGate(
-    URL  https://github.com/soramitsu/soramitsu-hunter/archive/00dc7fb89db760a3c367a1e158caa785184f58d2.tar.gz
+    URL  https://github.com/soramitsu/soramitsu-hunter/archive/v0.23.257-soramitsu14.tar.gz
     SHA1 73b42ce511af1e030b1f1f04d57ec2fd08af086e
     LOCAL
 )
