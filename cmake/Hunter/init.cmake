6cacfb8de39527439c1e8c3b6da3a08df5e69728
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2021-11-01 11:08:39 +0300

    update: hunter pm (final)
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -31,7 +31,7 @@ set(
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
 
 HunterGate(
-    URL  https://github.com/soramitsu/soramitsu-hunter/archive/d90a40a148bfa548b81e33aa0a44ad2011bea6e7.tar.gz
-    SHA1  4da4b35d670bdfee6d96cd08879cfd205fcca939
+    URL  https://github.com/soramitsu/soramitsu-hunter/archive/v0.23.257-soramitsu15.tar.gz
+    SHA1  6275b53983398688f467ae6358194ca407b65446
     LOCAL
 )
