6c91aa1fde8947023e12039b16c03024c1d410d3
Alexander Krutikov <krutikov@soramitsu.co.jp>
2021-11-01 18:37:02 +0300

    build LLVM statically (#920)

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -34,3 +34,15 @@ hunter_config(
     VERSION 0.0.4
     KEEP_PACKAGE_SOURCES
 )
+
+hunter_config(
+  wavm
+  URL https://github.com/soramitsu/WAVM/archive/7efbcced0d41d5f7bc6cd254d624e5f7174b54fc.tar.gz
+  SHA1 dd22c11c5faf95a6f6b05ecff18f0e8cab475771
+  CMAKE_ARGS
+      WAVM_ENABLE_FUZZ_TARGETS=OFF
+      WAVM_ENABLE_STATIC_LINKING=ON
+      WAVM_BUILD_EXAMPLES=OFF
+      WAVM_BUILD_TESTS=OFF
+  KEEP_PACKAGE_SOURCES
+)
