ce7682f8ed4ae3d5a312eafefcfc32d9ae340505
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2022-04-29 19:15:34 +0300

    refactor: improve Tagged to support non-scalar types
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/common/tagged.hpp
+++ core/common/tagged.hpp
@@ -7,24 +7,38 @@
 #define KAGOME_TAGGED
 
 #include <type_traits>
+#include <utility>
 
 namespace kagome {
 
-  /// Special sized wrapping type for differentiate some similar types
+  template <typename T, typename = std::enable_if<std::is_scalar_v<T>>>
+  struct Wrapper {
+    template <typename... Args>
+    explicit Wrapper(Args &&...args) : value(std::forward<T>(args)...) {}
+
+   protected:
+    T value;
+  };
+
   template <typename T,
             typename Tag,
-            typename = std::enable_if_t<std::is_scalar_v<T>>>
-  class Tagged {
+            typename Base =
+                std::conditional_t<std::is_scalar_v<T>, Wrapper<T>, T>>
+  class Tagged final : public Base {
    public:
-    explicit Tagged(T v) : v(v) {}
-
-    operator T() const {
-      return v;
-    }
-
-   private:
     typedef Tag tag;
-    T v;
+
+    template <typename... Args>
+    Tagged(Args &&...args) : Base(std::forward<Args>(args)...) {}
+
+    template <typename Out>
+    explicit operator Out() {
+      if constexpr (std::is_scalar_v<T>) {
+        return this->Wrapper<T>::value;
+      } else {
+        return *this;
+      }
+    }
   };
 }  // namespace kagome
 
