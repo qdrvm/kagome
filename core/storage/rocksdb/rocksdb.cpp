05366dc8a3e9c027866e06d8f23fcf0e6b8b4dd0
Igor Egorov <igor@soramitsu.co.jp>
2022-05-14 01:49:06 +0300

    RockDB wrapper impl
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

---
+++ core/storage/rocksdb/rocksdb.cpp
@@ -0,0 +1,158 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "storage/rocksdb/rocksdb.hpp"
+
+#include <boost/filesystem.hpp>
+
+#include "filesystem/directories.hpp"
+#include "storage/database_error.hpp"
+#include "storage/rocksdb/rocksdb_batch.hpp"
+#include "storage/rocksdb/rocksdb_cursor.hpp"
+#include "storage/rocksdb/rocksdb_util.hpp"
+
+namespace kagome::storage {
+  namespace fs = boost::filesystem;
+
+  outcome::result<std::unique_ptr<RocksDB>> RocksDB::create(
+      const boost::filesystem::path &path, rocksdb::Options options) {
+    if (!filesystem::createDirectoryRecursive(path)) {
+      return DatabaseError::DB_PATH_NOT_CREATED;
+    }
+
+    rocksdb::DB *db = nullptr;
+
+    auto log = log::createLogger("RocksDB", "storage");
+    auto absolute_path = fs::absolute(path, fs::current_path());
+
+    boost::system::error_code ec;
+    if (not fs::create_directory(absolute_path.native(), ec) and ec.value()) {
+      log->error("Can't create directory {} for database: {}",
+                 absolute_path.native(),
+                 ec.message());
+      return DatabaseError::IO_ERROR;
+    }
+    if (not fs::is_directory(absolute_path.native())) {
+      log->error("Can't open {} for database: is not a directory",
+                 absolute_path.native());
+      return DatabaseError::IO_ERROR;
+    }
+
+    auto status = rocksdb::DB::Open(options, path.native(), &db);
+    if (status.ok()) {
+      std::unique_ptr<RocksDB> l{new RocksDB{}};
+      l->db_ = std::unique_ptr<rocksdb::DB>(db);
+      l->logger_ = std::move(log);
+      return l;
+    }
+
+    SL_ERROR(log,
+             "Can't open database in {}: {}",
+             absolute_path.native(),
+             status.ToString());
+
+    return status_as_error(status);
+  }
+
+  std::unique_ptr<BufferBatch> RocksDB::batch() {
+    return std::make_unique<Batch>(*this);
+  }
+
+  size_t RocksDB::size() const {
+    size_t usage_bytes = 0;
+    if (db_) {
+      std::string usage;
+      bool result = db_->GetProperty("rocksdb.cur-size-all-mem-tables", &usage);
+      if (result) {
+        try {
+          usage_bytes = std::stoul(usage);
+        } catch (...) {
+          logger_->error("Unable to parse memory usage value");
+        }
+      } else {
+        logger_->error("Unable to retrieve memory usage value");
+      }
+    }
+    return usage_bytes;
+  }
+
+  std::unique_ptr<RocksDB::Cursor> RocksDB::cursor() {
+    auto it = std::unique_ptr<rocksdb::Iterator>(db_->NewIterator(ro_));
+    return std::make_unique<RocksDBCursor>(std::move(it));
+  }
+
+  outcome::result<bool> RocksDB::contains(const BufferView &key) const {
+    std::string value;
+    auto status = db_->Get(ro_, make_slice(key), &value);
+    if (status.ok()) {
+      return true;
+    }
+
+    if (status.IsNotFound()) {
+      return false;
+    }
+
+    return status_as_error(status);
+  }
+
+  bool RocksDB::empty() const {
+    auto it = std::unique_ptr<rocksdb::Iterator>(db_->NewIterator(ro_));
+    it->SeekToFirst();
+    return it->Valid();
+  }
+
+  outcome::result<Buffer> RocksDB::load(const BufferView &key) const {
+    std::string value;
+    auto status = db_->Get(ro_, make_slice(key), &value);
+    if (status.ok()) {
+      // cannot move string content to a buffer
+      return Buffer(
+          reinterpret_cast<uint8_t *>(value.data()),                  // NOLINT
+          reinterpret_cast<uint8_t *>(value.data()) + value.size());  // NOLINT
+    }
+    return status_as_error(status);
+  }
+
+  outcome::result<std::optional<Buffer>> RocksDB::tryLoad(
+      const BufferView &key) const {
+    std::string value;
+    auto status = db_->Get(ro_, make_slice(key), &value);
+    if (status.ok()) {
+      return std::make_optional(Buffer(
+          reinterpret_cast<uint8_t *>(value.data()),                   // NOLINT
+          reinterpret_cast<uint8_t *>(value.data()) + value.size()));  // NOLINT
+    }
+
+    if (status.IsNotFound()) {
+      return std::nullopt;
+    }
+
+    return status_as_error(status);
+  }
+
+  outcome::result<void> RocksDB::put(const BufferView &key,
+                                     const Buffer &value) {
+    auto status = db_->Put(wo_, make_slice(key), make_slice(value));
+    if (status.ok()) {
+      return outcome::success();
+    }
+
+    return status_as_error(status);
+  }
+
+  outcome::result<void> RocksDB::put(const BufferView &key, Buffer &&value) {
+    Buffer copy(std::move(value));
+    return put(key, copy);
+  }
+
+  outcome::result<void> RocksDB::remove(const BufferView &key) {
+    auto status = db_->Delete(wo_, make_slice(key));
+    if (status.ok()) {
+      return outcome::success();
+    }
+
+    return status_as_error(status);
+  }
+}  // namespace kagome::storage
---
+++ core/storage/rocksdb/rocksdb_cursor.cpp
@@ -0,0 +1,48 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "storage/rocksdb/rocksdb_cursor.hpp"
+
+#include "rocksdb_util.hpp"
+
+namespace kagome::storage {
+
+  RocksDBCursor::RocksDBCursor(std::shared_ptr<rocksdb::Iterator> it)
+      : i_{std::move(it)} {}
+
+  outcome::result<bool> RocksDBCursor::seekFirst() {
+    i_->SeekToFirst();
+    return isValid();
+  }
+
+  outcome::result<bool> RocksDBCursor::seek(const BufferView &key) {
+    i_->Seek(make_slice(key));
+    return isValid();
+  }
+
+  outcome::result<bool> RocksDBCursor::seekLast() {
+    i_->SeekToLast();
+    return isValid();
+  }
+
+  bool RocksDBCursor::isValid() const {
+    return i_->Valid();
+  }
+
+  outcome::result<void> RocksDBCursor::next() {
+    i_->Next();
+    return outcome::success();
+  }
+
+  std::optional<Buffer> RocksDBCursor::key() const {
+    return isValid() ? std::make_optional(make_buffer(i_->key()))
+                     : std::nullopt;
+  }
+
+  std::optional<Buffer> RocksDBCursor::value() const {
+    return isValid() ? std::make_optional(make_buffer(i_->value()))
+                     : std::nullopt;
+  }
+}  // namespace kagome::storage
