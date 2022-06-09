ac8d7cc0a39f7ca10e18da716e7417fde162901a
turuslan <turuslan.devbox@gmail.com>
2022-06-09 14:28:20 +0300

    set fd limit

---
+++ core/common/fd_limit.cpp
@@ -0,0 +1,63 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "common/fd_limit.hpp"
+
+#include <sys/resource.h>
+#include <algorithm>
+#include <boost/iterator/counting_iterator.hpp>
+
+#include "log/logger.hpp"
+
+namespace kagome::common {
+  auto log = log::createLogger("FdLimit", log::defaultGroupName);
+
+  bool getFdLimit(rlimit &r) {
+    if (getrlimit(RLIMIT_NOFILE, &r) != 0) {
+      SL_WARN(log,
+              "Error: getrlimit(RLIMIT_NOFILE) errno={} {}",
+              errno,
+              strerror(errno));
+      return false;
+    }
+    return true;
+  }
+
+  bool setFdLimit(const rlimit &r) {
+    return setrlimit(RLIMIT_NOFILE, &r) == 0;
+  }
+
+  void setFdLimit(size_t limit) {
+    rlimit r{};
+    if (!getFdLimit(r)) {
+      return;
+    }
+    if (r.rlim_max == RLIM_INFINITY) {
+      SL_DEBUG(log, "current={} max=unlimited", r.rlim_cur);
+    } else {
+      SL_DEBUG(log, "current={} max={}", r.rlim_cur, r.rlim_max);
+    }
+    if (limit <= r.rlim_cur) {
+      return;
+    }
+    rlim_t current = r.rlim_cur;
+    r.rlim_cur = limit;
+    if (!setFdLimit(r)) {
+      std::upper_bound(boost::counting_iterator{current},
+                       boost::counting_iterator{rlim_t{limit}},
+                       nullptr,
+                       [&](std::nullptr_t, rlim_t current) {
+                         r.rlim_cur = current;
+                         return !setFdLimit(r);
+                       });
+    }
+    if (!getFdLimit(r)) {
+      return;
+    }
+    if (r.rlim_cur != current) {
+      SL_DEBUG(log, "changed current={}", r.rlim_cur);
+    }
+  }
+}  // namespace kagome::common
