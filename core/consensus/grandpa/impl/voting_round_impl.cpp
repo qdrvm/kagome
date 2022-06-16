884c2b578a5693851dd9d03c1cb30446c0f91c85
kamilsa <kamilsa16@gmail.com>
2022-06-16 18:44:50 +0300

    Update grandpa docs (#1165)
    Signed-off-by: kamilsa <kamilsa16@gmail.com>
    
    Co-authored-by: Harrm <abrehchs@gmail.com>

--- core/consensus/grandpa/impl/voting_round_impl.cpp
+++ core/consensus/grandpa/impl/voting_round_impl.cpp
@@ -1079,7 +1079,7 @@ namespace kagome::consensus::grandpa {
           [grandpa_wp = std::move(grandpa_), round_wp = weak_from_this()] {
             if (auto grandpa = grandpa_wp.lock()) {
               if (auto round = round_wp.lock()) {
-                grandpa->executeNextRound(round);
+                grandpa->tryExecuteNextRound(round);
               }
             }
           });
