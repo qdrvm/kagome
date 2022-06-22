23ddcad8c898ed5337f640b8aaca60ba06ff77d1
turuslan <turuslan.devbox@gmail.com>
2022-06-22 14:42:42 +0300

    comment

--- core/application/modes/print_chain_info_mode.hpp
+++ core/application/modes/print_chain_info_mode.hpp
@@ -15,7 +15,17 @@ namespace kagome::blockchain {
 }  // namespace kagome::blockchain
 
 namespace kagome::application::mode {
-  /// Prints chain info
+  /**
+   * Prints chain info JSON.
+   * Example:
+   *   {
+   *     "genesis_hash": "0x91...",
+   *     "finalized_hash": "0x46...",
+   *     "finalized_number": 100,
+   *     "best_hash": "0x75..",
+   *     "best_number": 105
+   *   }
+   */
   class PrintChainInfoMode final : public Mode {
    public:
     PrintChainInfoMode(std::shared_ptr<blockchain::BlockTree> block_tree)
