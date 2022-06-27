96a280db86d56e8d63219e44e716a78afddc0b42
turuslan <turuslan.devbox@gmail.com>
2022-06-27 10:27:55 +0300

    try update soralog
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -31,7 +31,7 @@ set(
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
 
 HunterGate(
-    URL  "https://github.com/soramitsu/soramitsu-hunter/archive/7a31557759d8926629e67c96582f530bf5c9c821.tar.gz"
-    SHA1 "4c6edbf5785c4973cc7f45c55954beff3e85620f"
+    URL  "https://github.com/soramitsu/soramitsu-hunter/archive/199d077603393242c4a7e6d60deac99d3f9bb86c.tar.gz"
+    SHA1 "6524c0133ec462eaf42cab485fc9e8e85e74aee0"
     LOCAL
 )
