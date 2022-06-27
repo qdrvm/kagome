c57a5d71ae1ad49d5f7898e13dadcd5958b6fe7e
turuslan <turuslan.devbox@gmail.com>
2022-06-27 11:45:29 +0300

    update external project
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -29,9 +29,4 @@ set(
 )
 
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
-
-HunterGate(
-    URL  "https://github.com/soramitsu/soramitsu-hunter/archive/199d077603393242c4a7e6d60deac99d3f9bb86c.tar.gz"
-    SHA1 "6524c0133ec462eaf42cab485fc9e8e85e74aee0"
-    LOCAL
-)
+include(${CMAKE_CURRENT_LIST_DIR}/hunter-gate-url.cmake)
