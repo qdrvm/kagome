ed002319b4661003e8e0e69f37f95563ca86b2d2
turuslan <turuslan.devbox@gmail.com>
2022-08-10 16:44:50 +0300

    pr comments
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- housekeeping/scale_tie.py
+++ housekeeping/scale_tie.py
@@ -5,19 +5,16 @@ import sys
 N = int(sys.argv[1])
 S = ""
 
-S += "  template <typename T, size_t N = std::remove_reference_t<T>::scale_tie>\n"
-S += "  auto as_tie(T &&v) {\n"
-S += "    if constexpr (N == 0) {\n"
-S += "      // can't create zero sized binding to check zero size\n"
-S += "      return std::tie();\n"
+S += "  template <typename T, typename F, size_t N = std::remove_reference_t<T>::scale_tie>\n"
+S += "  auto as_tie(T &&v, F &&f) {\n"
 for n in range(1, N + 1):
-  S += "    } else if constexpr (N == %d) {\n" % n
+  S += "    %sif constexpr (N == %d) {\n" % ("" if n == 1 else "} else ", n)
   vs = ", ".join("v%d" % i for i in range(n))
   S += "      auto &[%s] = v;\n" % vs
-  S += "      return std::tie(%s);\n" % vs
+  S += "      return std::forward<F>(f)(std::tie(%s));\n" % vs
 S += "    } else {\n"
 S += "      // generate code for bigger tuples\n"
-S += "      (void)0;\n"
+S += "      static_assert(1 <= N && N <= %d);\n" % N
 S += "    }\n"
 S += "  }\n"
 
