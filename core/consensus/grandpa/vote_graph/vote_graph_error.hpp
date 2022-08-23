ba0b00ac1858ab277f2a5c987c9061fd5540c934
kamilsa <kamilsa16@gmail.com>
2022-08-23 11:26:17 +0300

    Fix inserting block before base (#1325)
    * Fix inserting block before base
    
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

---
+++ core/consensus/grandpa/vote_graph/vote_graph_error.hpp
@@ -0,0 +1,21 @@
+/**
+* Copyright Soramitsu Co., Ltd. All Rights Reserved.
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+#ifndef KAGOME_VOTE_GRAPH_ERROR_HPP
+#define KAGOME_VOTE_GRAPH_ERROR_HPP
+
+#include <outcome/outcome.hpp>
+
+namespace kagome::consensus::grandpa {
+
+  enum VoteGraphError {
+    RECEIVED_BLOCK_LESS_THAN_BASE = 1
+  };
+
+}
+
+OUTCOME_HPP_DECLARE_ERROR(kagome::consensus::grandpa, VoteGraphError);
+
+#endif  // KAGOME_VOTE_GRAPH_ERROR_HPP
