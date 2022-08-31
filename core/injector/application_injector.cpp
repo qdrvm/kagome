0f450765ce17ad341cc692a5cdee96e02d353f3e
turuslan <turuslan.devbox@gmail.com>
2022-08-31 15:02:19 +0300

    split some code from injector
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- core/injector/application_injector.cpp
+++ core/injector/application_injector.cpp
@@ -83,6 +83,7 @@
 #include "crypto/vrf/vrf_provider_impl.hpp"
 #include "host_api/impl/host_api_factory_impl.hpp"
 #include "host_api/impl/host_api_impl.hpp"
+#include "injector/get_peer_keypair.hpp"
 #include "log/configurator.hpp"
 #include "log/logger.hpp"
 #include "metrics/impl/exposer_impl.hpp"
@@ -475,92 +476,6 @@ namespace {
     initialized = std::move(key_file_storage_res.value());
     initialized_path = std::move(path);
 
-    return initialized.value();
-  }
-
-  const sptr<libp2p::crypto::KeyPair> &get_peer_keypair(
-      const application::AppConfiguration &app_config,
-      const crypto::Ed25519Provider &crypto_provider,
-      crypto::CryptoStore &crypto_store) {
-    static auto initialized =
-        std::optional<sptr<libp2p::crypto::KeyPair>>(std::nullopt);
-
-    if (initialized) {
-      return initialized.value();
-    }
-
-    auto log = log::createLogger("Injector", "injector");
-
-    if (app_config.nodeKey()) {
-      log->info("Will use LibP2P keypair from config or 'node-key' CLI arg");
-
-      auto provided_keypair =
-          crypto_provider.generateKeypair(app_config.nodeKey().value());
-      BOOST_ASSERT(provided_keypair.secret_key == app_config.nodeKey().value());
-
-      auto key_pair = std::make_shared<libp2p::crypto::KeyPair>(
-          crypto::ed25519KeyToLibp2pKeypair(provided_keypair));
-
-      initialized.emplace(std::move(key_pair));
-      return initialized.value();
-    }
-
-    if (app_config.nodeKeyFile()) {
-      const auto &path = app_config.nodeKeyFile().value();
-      log->info(
-          "Will use LibP2P keypair from config or 'node-key-file' CLI arg");
-      auto key = crypto_store.loadLibp2pKeypair(path);
-      if (key.has_error()) {
-        log->error("Unable to load user provided key from {}. Error: {}",
-                   path,
-                   key.error().message());
-        common::raise(key.error());
-      } else {
-        auto key_pair =
-            std::make_shared<libp2p::crypto::KeyPair>(std::move(key.value()));
-        initialized.emplace(std::move(key_pair));
-        return initialized.value();
-      }
-    }
-
-    if (crypto_store.getLibp2pKeypair().has_value()) {
-      log->info(
-          "Will use LibP2P keypair from config or args (loading from base "
-          "path)");
-
-      auto stored_keypair = crypto_store.getLibp2pKeypair().value();
-
-      auto key_pair =
-          std::make_shared<libp2p::crypto::KeyPair>(std::move(stored_keypair));
-
-      initialized.emplace(std::move(key_pair));
-      return initialized.value();
-    }
-
-    log->warn(
-        "Can not obtain a libp2p keypair from crypto storage. "
-        "A unique one will be generated");
-
-    kagome::crypto::Ed25519Keypair generated_keypair;
-    auto save = app_config.shouldSaveNodeKey();
-    if (save) {
-      auto res = crypto_store.generateEd25519KeypairOnDisk(
-          crypto::KnownKeyTypeId::KEY_TYPE_LP2P);
-      if (res.has_error()) {
-        log->warn("Can't save libp2p keypair: {}", res.error());
-        save = false;
-      } else {
-        generated_keypair = res.value();
-      }
-    }
-    if (not save) {
-      generated_keypair = crypto_provider.generateKeypair();
-    }
-
-    auto key_pair = std::make_shared<libp2p::crypto::KeyPair>(
-        crypto::ed25519KeyToLibp2pKeypair(generated_keypair));
-
-    initialized.emplace(std::move(key_pair));
     return initialized.value();
   }
 
@@ -1108,7 +1023,8 @@ namespace {
               injector.template create<const crypto::Ed25519Provider &>();
           auto &crypto_store =
               injector.template create<crypto::CryptoStore &>();
-          return get_peer_keypair(app_config, crypto_provider, crypto_store);
+          return injector::get_peer_keypair(
+              app_config, crypto_provider, crypto_store);
         })[boost::di::override],
 
         // bind io_context: 1 per injector
@@ -1354,7 +1270,7 @@ namespace {
       auto &crypto_store = injector.template create<crypto::CryptoStore &>();
 
       auto &local_pair =
-          get_peer_keypair(config, crypto_provider, crypto_store);
+          injector::get_peer_keypair(config, crypto_provider, crypto_store);
 
       public_key = local_pair->publicKey;
     } else {
