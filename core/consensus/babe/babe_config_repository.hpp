b0b9325b6ea46708a2faf342d48cb17b6f2eed3d
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2022-09-27 23:30:14 +0800

    feature: babe config repo
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

---
+++ core/consensus/babe/babe_config_repository.hpp
@@ -0,0 +1,28 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_CONSENSUS_BABE_BABECONFIGREPOSITORY
+#define KAGOME_CONSENSUS_BABE_BABECONFIGREPOSITORY
+
+#include "primitives/babe_configuration.hpp"
+
+namespace kagome::consensus::babe {
+
+  /// Keeps actual babe configuration
+  class BabeConfigRepository {
+   public:
+    virtual ~BabeConfigRepository() = default;
+
+    /// Returns actual babe configuration, obtaining it from runtime if needed
+    /// @return actual babe configuration
+    virtual const primitives::BabeConfiguration &config();
+
+    /// Invalidates internally cached config, to enforce take in from runtime
+    virtual void invalidate();
+  };
+
+}  // namespace kagome::consensus::babe
+
+#endif  // KAGOME_CONSENSUS_BABE_BABECONFIGREPOSITORY
