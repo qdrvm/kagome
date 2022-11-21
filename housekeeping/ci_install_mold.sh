0135594d4501aadfdd2a823db17f85ec04168b06
turuslan <turuslan.devbox@gmail.com>
2022-11-22 02:09:39 +0300

    mold
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

---
+++ housekeeping/ci_install_mold.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+
+if [ "$CI" != "true" ] || [ "$GITHUB_ACTIONS" != "true" ]; then
+  echo "CI=true GITHUB_ACTIONS=true are required"
+  exit -1
+fi
+
+wget -q -O- https://github.com/rui314/mold/releases/download/v1.3.1/mold-1.3.1-x86_64-linux.tar.gz | tar -C /usr/local --strip-components=1 -xzf -
+if [ "$1" == "--make-default" ]; then
+  ln -sf /usr/local/bin/mold $(realpath /usr/bin/ld)
+fi
