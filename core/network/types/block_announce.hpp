937f829ad507f094756507bb9986ba3a349a57fc
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2023-02-10 16:21:51 +0800

    feature: extend BlockAnnounce by BlockState and data (like in substrate)
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/network/types/block_announce.hpp
+++ core/network/types/block_announce.hpp
@@ -10,14 +10,29 @@
 #include "scale/tie.hpp"
 
 namespace kagome::network {
-  /**
-   * Announce a new complete block on the network
-   */
-  struct BlockAnnounce {
-    SCALE_TIE(1);
 
-    primitives::BlockHeader header;
+  /// Block state in the chain.
+  enum class BlockState : uint8_t {
+    /// Block is not part of the best chain.
+    Normal,
+    /// Latest best block.
+    Best,
   };
+
+  /// Announce a new complete relay chain block on the network.
+  struct BlockAnnounce {
+    SCALE_TIE(3);
+
+    /// New block header.
+    primitives::BlockHeader header;
+
+    /// Block state.
+    std::optional<BlockState> state = std::nullopt;
+
+    /// Data associated with this block announcement, e.g. a candidate message.
+    std::optional<std::vector<uint8_t>> data = std::nullopt;
+  };
+
 }  // namespace kagome::network
 
 #endif  // KAGOME_BLOCK_ANNOUNCE_HPP
