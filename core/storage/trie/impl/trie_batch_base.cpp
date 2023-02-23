f7964f8fba7d9f2bac517f911d78752c1e38035d
Harrm <abrehchs@gmail.com>
2023-02-23 12:39:44 +0300

    Make a common base for batch types

---
+++ core/storage/trie/impl/trie_batch_base.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vladimir on 23.02.2023.
+//
+
+#include "trie_batch_base.hpp"
---
+++ core/storage/trie/impl/trie_batch_base.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Vladimir on 23.02.2023.
+//
+
+#ifndef KAGOME_TRIE_BATCH_BASE_HPP
+#define KAGOME_TRIE_BATCH_BASE_HPP
+
+
+
+class TrieBatchBase {
+
+};
+
+
+
+#endif //KAGOME_TRIE_BATCH_BASE_HPP
