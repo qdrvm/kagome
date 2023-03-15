40de82e0e93754fcd54d5c78e7daa731f11c626b
safinsaf <safinsaft@gmail.com>
2023-03-15 12:35:14 +0300

    Add CMAKE_UNITY_BUILD
    Signed-off-by: safinsaf <safinsaft@gmail.com>

--- housekeeping/docker/kagome-dev/make.sh
+++ housekeeping/docker/kagome-dev/make.sh
@@ -4,6 +4,7 @@ BUILD_DIR=build
 BUILD_THREADS="${BUILD_THREADS:-$(( $(nproc 2>/dev/null || sysctl -n hw.ncpu) / 2 + 1 ))}"
 
 BUILD_TYPE="${BUILD_TYPE:?BUILD_TYPE variable is not defined}"
+UNITY_BUILD="${UNITY_BUILD:?UNITY_BUILD variable is not defined}"
 
 if [ "$BUILD_TYPE" != "Debug" ] && [ "$BUILD_TYPE" != "Release" ] && [ "$BUILD_TYPE" != "RelWithDebInfo" ]; then
   echo "Invalid build type $BUILD_TYPE, should be either Debug, Release or RelWithDebInfo"
@@ -13,5 +14,5 @@ git submodule update --init
 
 cd "$(dirname $0)/../../.."
 
-cmake . -B"${BUILD_DIR}" -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"
+cmake . -B"${BUILD_DIR}" -G 'Unix Makefiles' -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DCMAKE_UNITY_BUILD="${UNITY_BUILD}"
 cmake --build "${BUILD_DIR}" --target kagome -- -j${BUILD_THREADS}
