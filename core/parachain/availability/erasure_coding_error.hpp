5ddd5678fd95d18fd8451c8fd4d340c96def6894
Alexander Lednev <57529355+iceseer@users.noreply.github.com>
2023-05-29 12:20:11 +0300

    Erasure-coding cpp integration (#1632)
    * integration
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * issues fixes
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    * ec_cpp hunter update
    
    Signed-off-by: iceseer <iceseer@gmail.com>
    
    ---------
    
    Signed-off-by: iceseer <iceseer@gmail.com>

--- core/parachain/availability/erasure_coding_error.hpp
+++ core/parachain/availability/erasure_coding_error.hpp
@@ -6,6 +6,7 @@
 #ifndef KAGOME_PARACHAIN_AVAILABILITY_ERASURE_CODING_ERROR_HPP
 #define KAGOME_PARACHAIN_AVAILABILITY_ERASURE_CODING_ERROR_HPP
 
+#include <ec-cpp/errors.hpp>
 #include "outcome/outcome.hpp"
 
 namespace kagome {
@@ -18,6 +19,15 @@ namespace kagome {
   enum class ErasureCodingRootError {
     MISMATCH = 1,
   };
+
+  constexpr int kErrorOffset = 0x01000000;
+  inline ErasureCodingError toCodeError(ec_cpp::Error code) {
+    return ErasureCodingError(kErrorOffset + (int)code);
+  }
+  inline ErasureCodingError fromCodeError(ErasureCodingError code) {
+    return ErasureCodingError((int)code - kErrorOffset);
+  }
+
 }  // namespace kagome
 
 OUTCOME_HPP_DECLARE_ERROR(kagome, ErasureCodingError)
