fbaf1512390884ab3375f990ee260f2f4eac079d
iceseer <iceseer@gmail.com>
2023-05-29 10:51:14 +0300

    ec_cpp hunter update
    Signed-off-by: iceseer <iceseer@gmail.com>

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  "https://github.com/soramitsu/soramitsu-hunter/archive/refs/tags/v0.23.257-soramitsu47.zip"
-  SHA1 "3f13f4157cc8f7edd48247417c3350bc16706b65"
+  URL  "https://github.com/soramitsu/soramitsu-hunter/archive/refs/tags/v0.23.257-soramitsu48.zip"
+  SHA1 "1f5e4196684cc8997bffcdd06715eede1070930d"
   LOCAL
 )
