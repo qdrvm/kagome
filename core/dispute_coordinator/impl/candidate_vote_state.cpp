5e3e261178f0b585f8675a148ae0a7dc628df2c3
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2023-06-08 18:56:16 +0300

    feature: candidate vote state

--- core/dispute_coordinator/impl/candidate_vote_state.cpp
+++ core/dispute_coordinator/impl/candidate_vote_state.cpp
@@ -4,45 +4,85 @@
  */
 
 #include "dispute_coordinator/impl/candidate_vote_state.hpp"
+#include <set>
 
 namespace kagome::dispute {
   CandidateVoteState CandidateVoteState::create(CandidateVotes votes,
                                                 CandidateEnvironment &env,
                                                 Timestamp now) {
-//
-//		let own_vote = OwnVoteState::new(&votes, env);
-//
-//		let n_validators = env.validators().len();
-//
-//		let supermajority_threshold = polkadot_primitives::supermajority_threshold(n_validators);
-//
-//		// We have a dispute, if we have votes on both sides:
-//		let is_disputed = !votes.invalid.is_empty() && !votes.valid.raw().is_empty();
-//
-//		let dispute_status = if is_disputed {
-//			let mut status = DisputeStatus::active();
-//			let byzantine_threshold = polkadot_primitives::byzantine_threshold(n_validators);
-//			let is_confirmed = votes.voted_indices().len() > byzantine_threshold;
-//			if is_confirmed {
-//				status = status.confirm();
-//			};
-//			let concluded_for = votes.valid.raw().len() >= supermajority_threshold;
-//			if concluded_for {
-//				status = status.conclude_for(now);
-//			};
-//
-//			let concluded_against = votes.invalid.len() >= supermajority_threshold;
-//			if concluded_against {
-//				status = status.conclude_against(now);
-//			};
-//			Some(status)
-//		} else {
-//			None
-//		};
-//
-//		Self { votes, own_vote, dispute_status }
+    CandidateVoteState res{.votes = std::move(votes),
+                           .own_vote = CannotVote{},
+                           .dispute_status = std::nullopt};
 
+    auto controlled_indices = env.controlled_indices;
+    if (not controlled_indices.empty()) {
+      Voted voted;
 
-    return CandidateVoteState(); // TODO unstab
+      for (auto index : controlled_indices) {
+        auto it = res.votes.valid.find(index);
+        if (it != res.votes.valid.end()) {
+          auto &[dispute_statement, validator_signature] = it->second;
+          voted.emplace_back(index, dispute_statement, validator_signature);
+        }
+      }
+
+      for (auto index : controlled_indices) {
+        auto it = res.votes.invalid.find(index);
+        if (it != res.votes.invalid.end()) {
+          auto &[dispute_statement, validator_signature] = it->second;
+          voted.emplace_back(index, dispute_statement, validator_signature);
+        }
+      }
+
+      res.own_vote = std::move(voted);
+    }
+
+    // Check if isn't disputed
+    // (We have a dispute, if we have votes on both sides)
+    if (res.votes.invalid.empty() or res.votes.valid.empty()) {
+      return res;
+    }
+
+    auto n_validators = env.session.validators.size();
+
+    auto byzantine_threshold = (std::max<size_t>(n_validators, 1) - 1) / 3;
+    auto supermajority_threshold = n_validators - byzantine_threshold;
+
+    DisputeStatus status = Active{};
+
+    // Check if concluded for
+    if (res.votes.valid.size() >= supermajority_threshold) {
+      res.dispute_status = ConcludedFor{now};
+      return res;
+    }
+
+    // Check if concluded against
+    if (res.votes.invalid.size() >= supermajority_threshold) {
+      res.dispute_status = ConcludedAgainst{now};
+      return res;
+    }
+
+    // Check if confirmed
+    if (res.votes.valid.size() > byzantine_threshold
+        or res.votes.invalid.size() > byzantine_threshold) {
+      res.dispute_status = Confirmed{};
+      return res;
+    }
+    std::set<ValidatorIndex> v;
+    for (auto &[i, _] : res.votes.valid) {
+      v.emplace(i);
+    }
+    for (auto &[i, _] : res.votes.invalid) {
+      v.emplace(i);
+      if (v.size() > byzantine_threshold) {
+        res.dispute_status = Confirmed{};
+        return res;
+      }
+    }
+
+    // Active otherwise
+    res.dispute_status = Active{};
+    return res;
   }
-}
+
+}  // namespace kagome::dispute
