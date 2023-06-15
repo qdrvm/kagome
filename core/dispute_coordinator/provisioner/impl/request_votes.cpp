950ce18dc83702a1c37472c347696a18642cd0ad
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2023-06-16 01:36:38 +0300

    feature: request votes for selections

--- core/dispute_coordinator/provisioner/impl/request_votes.cpp
+++ core/dispute_coordinator/provisioner/impl/request_votes.cpp
@@ -5,13 +5,47 @@
 
 #include "dispute_coordinator/provisioner/impl/request_votes.hpp"
 
+#include <future>
+
+#include "dispute_coordinator/dispute_coordinator.hpp"
+
 namespace kagome::dispute {
 
   std::vector<std::tuple<SessionIndex, CandidateHash, CandidateVotes>>
   request_votes(
-      std::vector<std::tuple<SessionIndex, CandidateHash>> disputes_to_query) {
-    //
-    return {};
+      const std::shared_ptr<dispute::DisputeCoordinator> &dispute_coordinator,
+      const std::vector<std::tuple<SessionIndex, CandidateHash>> &disputes) {
+    // Bounded by block production - `ProvisionerMessage::RequestInherentData`.
+
+    auto promise_res = std::promise<
+        outcome::result<DisputeCoordinator::OutputCandidateVotes>>();
+    auto res_future = promise_res.get_future();
+
+    // https://github.com/paritytech/polkadot/blob/40974fb99c86f5c341105b7db53c7aa0df707d66/node/core/provisioner/src/disputes/mod.rs#L37
+    dispute_coordinator->handle_incoming_QueryCandidateVotes(
+        disputes,
+        [promise_res = std::ref(promise_res)](
+            outcome::result<DisputeCoordinator::OutputCandidateVotes> res) {
+          promise_res.get().set_value(std::move(res));
+        });
+
+    DisputeCoordinator::OutputCandidateVotes candidate_votes;
+    if (not res_future.valid()) {
+      // SL_WARN(log_,
+      //         "Fetch for approval votes got cancelled, "
+      //         "only expected during shutdown!");
+    } else {
+      auto res = res_future.get();
+      if (res.has_error()) {
+        // SL_WARN(log_,
+        //         "Unable to query candidate votes: {}",
+        //         res.error());
+      } else {
+        candidate_votes = std::move(res.value());
+      }
+    }
+
+    return candidate_votes;
   }
 
 }  // namespace kagome::dispute
--- core/dispute_coordinator/provisioner/impl/request_votes.hpp
+++ core/dispute_coordinator/provisioner/impl/request_votes.hpp
@@ -9,12 +9,17 @@
 #include "dispute_coordinator/types.hpp"
 
 namespace kagome::dispute {
+  class DisputeCoordinator;
+}
+
+namespace kagome::dispute {
 
   /// Request the relevant dispute statements for a set of disputes identified
   /// by `CandidateHash` and the `SessionIndex`.
   std::vector<std::tuple<SessionIndex, CandidateHash, CandidateVotes>>
   request_votes(
-      std::vector<std::tuple<SessionIndex, CandidateHash>> disputes_to_query);
+      const std::shared_ptr<dispute::DisputeCoordinator> &dispute_coordinator,
+      const std::vector<std::tuple<SessionIndex, CandidateHash>> &disputes);
 
 }  // namespace kagome::dispute
 
