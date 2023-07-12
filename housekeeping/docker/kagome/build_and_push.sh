8b579754db80340f5257bf065cf8ebaaa22cfb42
safinsaf <safinsaft@gmail.com>
2023-07-12 10:16:47 +0300

    Revert build, update release image
    Signed-off-by: safinsaf <safinsaft@gmail.com>

--- housekeeping/docker/kagome/build_and_push.sh
+++ housekeeping/docker/kagome/build_and_push.sh
@@ -21,7 +21,7 @@ if [ "$VERSION" = "refs/heads/master" ]; then
 elif [[ "$VERSION"  == refs/tags/* ]]; then
   VERSION="${VERSION#refs/tags/}"
 else
-  VERSION=devops
+  VERSION=$VERSION
 fi
 
 if [ "$BUILD_TYPE" = "Debug" ]; then
