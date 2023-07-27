333eddcbb8038085361331fc4291c97fba6ec38d
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2023-07-27 12:40:57 +0300

    experiment: temporary add debugging logs
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/dispute_coordinator/impl/dispute_coordinator_impl.cpp
+++ core/dispute_coordinator/impl/dispute_coordinator_impl.cpp
@@ -836,7 +836,12 @@ namespace kagome::dispute {
     }
     auto &active_disputes = active_disputes_res.value();
 
+    SL_INFO(log_, "DEBUG: active_disputes.empty={}", active_disputes.empty());
+
     /// Handle new active disputes response.
+
+    SL_INFO(
+        log_, "DEBUG: sending_disputes_.empty={}", sending_disputes_.empty());
 
     // https://github.com/paritytech/polkadot/blob/40974fb99c86f5c341105b7db53c7aa0df707d66/node/network/dispute-distribution/src/sender/mod.rs#L261
     std::unordered_set<CandidateHash> candidates;
@@ -849,6 +854,9 @@ namespace kagome::dispute {
     // Cleanup obsolete senders
     sending_disputes_.remove_if([&](const auto &x) {
       const auto &candidate_hash = std::get<0>(x);
+      SL_INFO(log_,
+              "DEBUG: sending_disputes_.remove_if candidate={}",
+              candidate_hash);
       return candidates.find(candidate_hash) == candidates.end();
     });
 
@@ -945,6 +953,9 @@ namespace kagome::dispute {
             ? boost::relaxed_get<CandidateReceipt>(candidate_receipt)
                   .hash(*hasher_)
             : boost::relaxed_get<CandidateHash>(candidate_receipt);
+
+    SL_TRACE(
+        log_, "DEBUG:  handle_import_statements, candidate {}", candidate_hash);
 
     auto env_opt = makeCandidateEnvironment(
         *session_keys_, *rolling_session_window_, session);
@@ -2321,6 +2332,9 @@ namespace kagome::dispute {
     BOOST_ASSERT_MSG(protocol,
                      "Router did not provide `send dispute` protocol");
 
+    SL_INFO(log_,
+            "DEBUG: sending_disputes_.emplace_back candidate={}",
+            candidate_hash);
     auto &[_, sending_dispute] = sending_disputes_.emplace_back(
         candidate_hash, std::make_unique<SendingDispute>(protocol, request));
 
