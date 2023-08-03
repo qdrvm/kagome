259017ce9ab0e603ff0be1f7159301c51538c404
Ruslan Tushov <turuslan@users.noreply.github.com>
2023-08-03 10:46:47 +0300

    update hunter (fix wavm) (#1716)
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  "https://github.com/soramitsu/soramitsu-hunter/archive/refs/tags/v0.23.257-soramitsu54.zip"
-  SHA1 "bc2c9dadebed46c0b0ac1d90b8cfab7f0b0dc35f"
+  URL  "https://github.com/soramitsu/soramitsu-hunter/archive/refs/tags/v0.23.257-soramitsu55.zip"
+  SHA1 "bba2ce96d9c092d38b8dd0eea7a670ab4063357c"
   LOCAL
 )
