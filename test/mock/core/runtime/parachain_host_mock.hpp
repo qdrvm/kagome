8de9e5d23d97db5b8fc1906b0fe89e25b4ec0767
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2023-08-11 18:55:23 +0300

    fix: test
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

---
+++ test/mock/core/runtime/parachain_host_mock.hpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef KAGOME_TEST_MOCK_CORE_RUNTIME_BABE_API_MOCK_HPP
+#define KAGOME_TEST_MOCK_CORE_RUNTIME_BABE_API_MOCK_HPP
+
+#include "runtime/runtime_api/babe_api.hpp"
+
+#include <gmock/gmock.h>
+
+namespace kagome::runtime {
+
+  class BabeApiMock : public BabeApi {
+   public:
+    MOCK_METHOD(outcome::result<primitives::BabeConfiguration>,
+                configuration,
+                (const primitives::BlockHash &block),
+                (override));
+  };
+
+}  // namespace kagome::runtime
+
+#endif  // KAGOME_TEST_MOCK_CORE_RUNTIME_BABE_API_MOCK_HPP
