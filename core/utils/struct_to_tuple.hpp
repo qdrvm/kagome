0d295318e7909882fa9e214a15f0940d5c224172
iceseer <iceseer@gmail.com>
2023-08-30 11:17:14 +0300

    encode builded
    Signed-off-by: iceseer <iceseer@gmail.com>

--- core/utils/struct_to_tuple.hpp
+++ core/utils/struct_to_tuple.hpp
@@ -25,9 +25,38 @@
 #define REP9(X) REP8(X), X
 #define REP10(X) REP9(X), X
 
-#define REPEAT(TENS,ONES,X) \
-  REP##TENS(REP10(X)) \
-  REP##ONES(X)
+#define REP0Y(X)
+#define REP1Y(X) X##1
+#define REP2Y(X) REP1Y(X), X##2
+#define REP3Y(X) REP2Y(X), X##3
+#define REP4Y(X) REP3Y(X), X##4
+#define REP5Y(X) REP4Y(X), X##5
+#define REP6Y(X) REP5Y(X), X##6
+#define REP7Y(X) REP6Y(X), X##7
+#define REP8Y(X) REP7Y(X), X##8
+#define REP9Y(X) REP8Y(X), X##9
+#define REP10Y(X) REP9Y(X), X##10
+
+#define REPEAT(TENS, ONES, X) REP##TENS(REP10(X)) REP##ONES(X)
+#define REPEATY(TENS, ONES, X) REP##TENS##Y(REP10Y(X)) REP##ONES##Y(X)
+#define TO_TUPLE_N(TENS, ONES)                                             \
+  if constexpr (is_braces_constructible<type,                              \
+                                        REPEAT(TENS, ONES, any_type)>{}) { \
+    auto &&[REPEATY(TENS, ONES, p)] = object;                              \
+    return std::make_tuple(REPEATY(TENS, ONES, p));                        \
+  }
+
+#define TO_TUPLE0 TO_TUPLE_N(0, 0) { return std::make_tuple(); } 
+#define TO_TUPLE1 TO_TUPLE_N(0, 1) else TO_TUPLE_N(0, 0) 
+#define TO_TUPLE2 TO_TUPLE_N(0, 2) else TO_TUPLE_N(0, 1) 
+#define TO_TUPLE3 TO_TUPLE_N(0, 3) else TO_TUPLE_N(0, 2) 
+#define TO_TUPLE4 TO_TUPLE_N(0, 4) else TO_TUPLE_N(0, 3) 
+#define TO_TUPLE5 TO_TUPLE_N(0, 5) else TO_TUPLE_N(0, 4) 
+#define TO_TUPLE6 TO_TUPLE_N(0, 6) else TO_TUPLE_N(0, 5) 
+#define TO_TUPLE7 TO_TUPLE_N(0, 7) else TO_TUPLE_N(0, 6) 
+#define TO_TUPLE8 TO_TUPLE_N(0, 8) else TO_TUPLE_N(0, 7) 
+#define TO_TUPLE9 TO_TUPLE_N(0, 9) else TO_TUPLE_N(0, 8) 
+#define TO_TUPLE10 TO_TUPLE_N(1, 0) else TO_TUPLE_N(0, 9) 
 
 namespace kagome::utils {
 
@@ -44,6 +73,12 @@ namespace kagome::utils {
     template <typename T>
     constexpr operator T();  // non explicit
   };
+
+  /*template <typename T>
+  auto to_tuple(T &&object) noexcept {
+    using type = std::decay_t<T>;
+    TO_TUPLE5;
+  }*/
 
   template <typename T>
   auto to_tuple(T &&object) noexcept {
@@ -79,40 +114,39 @@ namespace kagome::utils {
 namespace kagome::scale {
 
   template <typename T>
-  outcome::result<void> encode(T &&v);
+  void encode(T &&v);
 
   template <typename... Ts>
-  outcome::result<void> encode(std::tuple<Ts...> &&v);
+  void encode(std::tuple<Ts...> &&v);
 
   template <typename T, typename... Args>
-  outcome::result<void> encode(T &&t, Args &&...args);
+  void encode(T &&t, Args &&...args);
 
   void putByte(uint8_t val) {
     std::cout << std::hex << (uint32_t)val;
   }
 
   template <typename... Ts>
-  outcome::result<void> encode(std::tuple<Ts...> &&v) {
+  void encode(std::tuple<Ts...> &&v) {
     if constexpr (sizeof...(Ts) > 0) {
       std::apply([&](auto &&...s) { (..., encode(std::move(s))); },
                  std::move(v));
     }
-    return outcome::success();
   }
 
   template <typename T>
-  outcome::result<void> encode(T &&v) {
+  void encode(T &&v) {
     using I = std::decay_t<T>;
     if constexpr (std::is_integral_v<I>) {
       if constexpr (std::is_same_v<I, bool>) {
         const uint8_t byte = (v ? 1u : 0u);
         putByte(byte);
-        return outcome::success();
+        return;
       }
 
       if constexpr (sizeof(I) == 1u) {
         putByte(static_cast<uint8_t>(v));
-        return outcome::success();
+        return;
       }
 
       constexpr size_t size = sizeof(I);
@@ -131,16 +165,15 @@ namespace kagome::scale {
       for (size_t i = 0; i < size; ++i) {
         putByte((uint8_t)((v >> (i * 8)) & 0xff));
       }
-      return outcome::success();
     } else {
-      return encode(utils::to_tuple(std::move(v)));
+      encode(utils::to_tuple(std::move(v)));
     }
   }
 
   template <typename T, typename... Args>
-  outcome::result<void> encode(T &&t, Args &&...args) {
-    OUTCOME_TRY(encode(std::forward<T>(t)));
-    return encode(std::forward<Args>(args)...);
+  void encode(T &&t, Args &&...args) {
+    encode(std::forward<T>(t));
+    encode(std::forward<Args>(args)...);
   }
 
 }  // namespace kagome::scale
