446ede8ed746265d60eafe546c2208e0b89cdd80
Igor Egorov <igor.egorov@live.ru>
2023-09-29 11:45:50 +0300

    Add Missing Host APIs (#1811)
    * sr25519 & ed25519 batch_verify
    * ecdsa_verify_prehashed
    * ecdsa_verify_version_2
    * benchmark and evm api stubs
    * WAVM runtime test
    
    ---------
    
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>
    Signed-off-by: Igor Egorov <igor@qdrvm.io>
    Co-authored-by: Igor Egorov <igor@qdrvm.io>

--- core/runtime/binaryen/runtime_external_interface.cpp
+++ core/runtime/binaryen/runtime_external_interface.cpp
@@ -122,7 +122,7 @@ namespace kagome::runtime {
 
 namespace kagome::runtime::binaryen {
 
-  const static wasm::Name env = "env";
+  static const wasm::Name env = "env";
   /**
    * @note: some implementation details were taken from
    * https://github.com/WebAssembly/binaryen/blob/master/src/shell-interface.h
@@ -153,16 +153,19 @@ namespace kagome::runtime::binaryen {
     REGISTER_HOST_API_FUNC(ext_crypto_ed25519_generate_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_ed25519_sign_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_ed25519_verify_version_1);
+    REGISTER_HOST_API_FUNC(ext_crypto_ed25519_batch_verify_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_sr25519_public_keys_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_sr25519_generate_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_sr25519_sign_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_sr25519_verify_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_sr25519_verify_version_2);
+    REGISTER_HOST_API_FUNC(ext_crypto_sr25519_batch_verify_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_ecdsa_public_keys_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_ecdsa_sign_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_ecdsa_sign_prehashed_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_ecdsa_generate_version_1);
     REGISTER_HOST_API_FUNC(ext_crypto_ecdsa_verify_version_1);
+    REGISTER_HOST_API_FUNC(ext_crypto_ecdsa_verify_version_2);
     REGISTER_HOST_API_FUNC(ext_crypto_ecdsa_verify_prehashed_version_1);
     /**
      *  secp256k1 recovery algorithms version_1 and version_2
