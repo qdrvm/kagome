340a0ba9d0708f31028c0e67aa5fb153769f3db9
kamilsa <kamilsa16@gmail.com>
2023-11-30 17:22:00 +0600

    Remove comments

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
-#apt update
-#apt install --no-install-recommends -y build-essential git gcc ca-certificates python-is-python3 python3-pip python3-venv curl
+apt update
+apt install --no-install-recommends -y build-essential git gcc ca-certificates python-is-python3 python3-pip python3-venv curl
 
 current_dir=$(dirname "$0")
 parent_dir=$(dirname "$current_dir")
@@ -10,7 +10,7 @@ python3 -m venv "$parent_dir/venv"
 echo "Python environment created successfully in $parent_dir/venv"
 
 python -m venv venv
-./venv/bin/pip install --no-cache-dir cmake==3.25 gitpython requests
+$parent_dir/venv/bin/pip install --no-cache-dir cmake==3.25 gitpython requests
 
 
 RUST_VERSION=nightly-2022-11-20
