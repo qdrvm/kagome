d19494cffe76580d1429caada677a1d84461adb7
kamilsa <kamilsa16@gmail.com>
2023-12-05 20:59:59 +0600

    Add libgmp

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,7 +7,7 @@ cd $parent_dir
 set -a; source $current_dir/.env; set +a #include .env vars 
 
 apt update
-apt install --no-install-recommends -y build-essential git gcc ca-certificates python-is-python3 python3-pip python3-venv curl
+apt install --no-install-recommends -y build-essential git gcc ca-certificates python-is-python3 python3-pip python3-venv curl libgmp-dev
 
 python3 -m venv "$parent_dir/venv"
 
