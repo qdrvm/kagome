0e1aaee52d3327561a906204cb8eb5bf38585d12
kamilsa <kamilsa16@gmail.com>
2023-12-06 14:01:46 +0600

    Add cmake core number

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,4 +8,4 @@ set -a; source $current_dir/.env; set +a #include .env vars
 
 source $parent_dir/venv/bin/activate
 $parent_dir/venv/bin/cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Release
-$parent_dir/venv/bin/cmake --build build --target kagome -j 6
+$parent_dir/venv/bin/cmake --build build --target kagome -j ${CMAKE_CORE_NUMBER}
