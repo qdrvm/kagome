daa913fbb7c42638e401bf25932322582ee30618
Harrm <abrehchs@gmail.com>
2023-12-12 18:53:52 +0300

    Wasm Edge 0.13.3-qdrvm1 (#1899)
    * Update hunter URL to new version with WasmEdge 0.13.3-qdrvm1

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL "https://github.com/qdrvm/hunter/archive/refs/tags/v0.23.257-qdrvm14.tar.gz"
-  SHA1 "1d488224f6a06971a5b4166283bde16d1aa56458"
+  URL "https://github.com/qdrvm/hunter/archive/refs/tags/v0.23.257-qdrvm15.tar.gz"
+  SHA1 "7eede546a73aa8b5f637719439b9fb94f34e4094"
   LOCAL
 )
