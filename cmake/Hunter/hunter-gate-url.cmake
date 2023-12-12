da330e54e48983c5944e1cb02fd6e4f6192b6d3b
Harrm <abrehchs@gmail.com>
2023-12-12 18:41:57 +0300

    Update hunter URL

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL "https://github.com/qdrvm/hunter/archive/refs/heads/wasmedge-0.13.3-qdrvm1.zip"
-  SHA1 "f735e6a0e3edd5bc4afbac958894e0f417545896"
+  URL "https://github.com/qdrvm/hunter/archive/refs/tags/v0.23.257-qdrvm15.tar.gz"
+  SHA1 "7eede546a73aa8b5f637719439b9fb94f34e4094"
   LOCAL
 )
