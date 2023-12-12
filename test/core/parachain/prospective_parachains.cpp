83b0deda371619b341f4033c012f717bfbbea24c
iceseer <iceseer@gmail.com>
2023-12-12 10:49:18 +0300

    tests
    Signed-off-by: iceseer <iceseer@gmail.com>

--- test/core/parachain/prospective_parachains.cpp
+++ test/core/parachain/prospective_parachains.cpp
@@ -12,6 +12,7 @@
 
 #include "crypto/hasher/hasher_impl.hpp"
 #include "parachain/types.hpp"
+#include "parachain/validator/fragment_tree.hpp"
 #include "parachain/validator/prospective_parachains.hpp"
 #include "runtime/runtime_api/parachain_host_types.hpp"
 #include "scale/kagome_scale.hpp"
@@ -51,9 +52,37 @@ class ProspectiveParachainsTest : public testing::Test {
 
  protected:
   std::shared_ptr<kagome::crypto::Hasher> hasher_;
+
+  Hash hashFromStrData(std::span<const char> data) {
+    return hasher_->blake2b_256(data);
+  }
+
+  fragment::Constraints make_constraints(
+      BlockNumber min_relay_parent_number,
+      std::vector<BlockNumber> valid_watermarks,
+      HeadData required_parent) {
+    return fragment::Constraints{
+        .min_relay_parent_number = min_relay_parent_number,
+        .max_pov_size = 1000000,
+        .max_code_size = 1000000,
+        .ump_remaining = 10,
+        .ump_remaining_bytes = 1000,
+        .max_ump_num_per_candidate = 10,
+        .dmp_remaining_messages = std::vector<BlockNumber>(10, 0),
+        .hrmp_inbound = fragment::InboundHrmpLimitations{.valid_watermarks =
+                                                             valid_watermarks},
+        .hrmp_channels_out = {},
+        .max_hrmp_num_per_candidate = 0,
+        .required_parent = required_parent,
+        .validation_code_hash = hashFromStrData("42"),
+        .upgrade_restriction = std::nullopt,
+        .future_validation_code = std::nullopt,
+    };
+  }
 };
 
-TEST_F(ProspectiveParachainsTest, sendCandidatesAndCheckIfFound) {
+/*TEST_F(ProspectiveParachainsTest, sendCandidatesAndCheckIfFound) {
+  TestState test_state{};
   network::ExView{
       .view = {},
       .new_head =
@@ -69,4 +98,30 @@ TEST_F(ProspectiveParachainsTest, sendCandidatesAndCheckIfFound) {
           },
       .lost = {},
   };
+}*/
+
+TEST_F(ProspectiveParachainsTest,
+       FragmentTree_scopeRejectsAncestorsThatSkipBlocks) {
+  ParachainId para_id{5};
+  fragment::RelayChainBlockInfo relay_parent{
+      .hash = hashFromStrData("10"),
+      .number = 10,
+      .storage_root = hashFromStrData("69"),
+  };
+
+  std::vector<fragment::RelayChainBlockInfo> ancestors = {
+      fragment::RelayChainBlockInfo{
+          .hash = hashFromStrData("8"),
+          .number = 8,
+          .storage_root = hashFromStrData("69"),
+      }};
+
+  const size_t max_depth = 2ull;
+  fragment::Constraints base_constraints(
+      make_constraints(8, {8, 9}, {1, 2, 3}));
+  ASSERT_EQ(
+      fragment::Scope::withAncestors(
+          para_id, relay_parent, base_constraints, {}, max_depth, ancestors)
+          .error(),
+      fragment::Scope::Error::UNEXPECTED_ANCESTOR);
 }
