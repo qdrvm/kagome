0bed4b8ffbaf1a5ea8aca2a0f2dacf1a10cadb85
Ruslan Tushov <turuslan@users.noreply.github.com>
2023-12-12 14:05:41 +0300

    remove justification requests (#1898)
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- core/network/synchronizer.hpp
+++ core/network/synchronizer.hpp
@@ -43,14 +43,6 @@ namespace kagome::network {
                                    const libp2p::peer::PeerId &peer_id,
                                    SyncResultHandler &&handler) = 0;
 
-    /// Tries to request block justifications from {@param peer_id} for {@param
-    /// target_block} or a range of blocks up to {@param limit} count.
-    /// Calls {@param handler} when operation finishes
-    virtual void syncMissingJustifications(const libp2p::peer::PeerId &peer_id,
-                                           primitives::BlockInfo target_block,
-                                           std::optional<uint32_t> limit,
-                                           SyncResultHandler &&handler) = 0;
-
     virtual void syncState(const libp2p::peer::PeerId &peer_id,
                            const primitives::BlockInfo &block,
                            SyncResultHandler &&handler) = 0;
