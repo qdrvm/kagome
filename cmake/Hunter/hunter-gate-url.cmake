f028c422754c63c127f9b640d0d222458671881e
kamilsa <kamilsa16@gmail.com>
2024-02-01 11:25:32 +0600

    Remove user

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm1.tar.gz
-  SHA1 5fe289a63b716c51415476b531cb3648657f8485
+  URL  https://github.com/qdrvm/hunter/archive/7a2787913dc70b801582e4edd9dc3207963c65bd.tar.gz
+  SHA1 f81f932149cf59cd194a432580670d15ea776c0b
   LOCAL
 )
