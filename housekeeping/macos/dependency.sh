910148a88a3b6833275d22d45ac61562e93e175f
kamilsa <kamilsa16@gmail.com>
2024-02-01 12:13:40 +0600

    No sudo?

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -6,9 +6,9 @@
 #
 
 # install python pip3 deps
-sudo /opt/homebrew/opt/python@3.12/bin/python3.12 -m pip install --upgrade pip
-sudo /opt/homebrew/opt/python@3.12/bin/python3.12 -m pip install scikit-build
-sudo /opt/homebrew/opt/python@3.12/bin/python3.12 -m pip install cmake==3.25 requests gitpython gcovr
+/opt/homebrew/opt/python@3.12/bin/python3.12 -m pip install --upgrade pip
+/opt/homebrew/opt/python@3.12/bin/python3.12 -m pip install scikit-build
+/opt/homebrew/opt/python@3.12/bin/python3.12 -m pip install cmake==3.25 requests gitpython gcovr
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.75.0 --profile minimal
 
 which python3
