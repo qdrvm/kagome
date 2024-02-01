90ce655ad2827f3030ac48f6d89842a0aafecfbe
Harrm <abrehchs@gmail.com>
2024-02-01 18:33:21 +0300

    Try to fix

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -6,9 +6,9 @@
 #
 
 # install python pip3 deps
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install scikit-build
-sudo python3 -m pip install cmake==3.25 requests gitpython gcovr
+python3.12 -m venv /venv
+source /venv/bin/activate
+python3.12 -m pip install cmake==3.25 requests gitpython gcovr scikit-build pyyml
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.75.0 --profile minimal
 
 which python3
