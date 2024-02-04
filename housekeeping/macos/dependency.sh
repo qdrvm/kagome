7e8c7af674c027edecb970307aa23eeeef88a7e8
kamilsa <kamilsa16@gmail.com>
2024-02-04 21:55:25 +0600

    Update CI to Macos 14 (#1976)
    * Update to macos 14 (m1)
    
    * Update rust, remove llvm dep in macos build
    
    * Use ninja
    
    * Use ninja

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -14,6 +14,6 @@ fi
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install scikit-build
 sudo python3 -m pip install cmake==3.25 requests gitpython gcovr
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly-2022-11-20 --profile minimal
 
-brew install llvm@12
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.75.0 --profile minimal
+brew install ninja
