6055ecdf36c57e08b3d410d51aa91ddae145b932
kamilsa <kamilsa16@gmail.com>
2024-02-06 17:42:55 +0600

    Update hunter with new llvm, lld, wasmedge (#1978)
    * Update hunter with new llvm, lld, wasmedge

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm3.zip
-  SHA1 8989599eaa462f367805e2d36a30150c93b1d660
+  URL  https://github.com/qdrvm/hunter/archive/4ffeaf7c5e7eb26d5f50599bdf6be0316d02d053.zip
+  SHA1 095adf991b7bf3926a516053910be89fc1a19824
   LOCAL
 )
