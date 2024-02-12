64efa05d9d06a4ae93c45519494e6fbb8c7d7349
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2024-02-12 18:53:33 +0300

    feature: main thread pool handler
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/common/main_thread_pool.hpp
+++ core/common/main_thread_pool.hpp
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include "application/app_state_manager.hpp"
 #include "utils/thread_pool.hpp"
 #include "utils/watchdog.hpp"
 
@@ -17,5 +18,16 @@ namespace kagome::common {
     MainThreadPool(std::shared_ptr<Watchdog> watchdog,
                    std::shared_ptr<boost::asio::io_context> ctx)
         : ThreadPool(std::move(watchdog), "main_runner", 1, std::move(ctx)) {}
+  };
+
+  class MainPoolHandler final : public ThreadHandler {
+   public:
+    MainPoolHandler(
+        std::shared_ptr<application::AppStateManager> app_state_manager,
+        std::shared_ptr<MainThreadPool> thread_pool)
+        : ThreadHandler(thread_pool->io_context()) {
+      BOOST_ASSERT(app_state_manager);
+      app_state_manager->takeControl(*this);
+    }
   };
 }  // namespace kagome::common
