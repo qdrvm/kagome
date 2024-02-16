617e43b5e2d94b748f4787b16564caefaf9d51d0
Harrm <abrehchs@gmail.com>
2024-02-16 11:26:01 +0300

    Comment

--- core/runtime/common/stack_limiter.hpp
+++ core/runtime/common/stack_limiter.hpp
@@ -21,6 +21,14 @@ namespace kagome::runtime {
     return std::make_exception_ptr(std::runtime_error{e.msg});
   }
 
+  /**
+   * Implements the same logic as substrate's https://github.com/paritytech/wasm-instrument
+   * Patches the wasm code, wrapping each function call in a check that
+   * this call is not going to exceed the global stack limit
+   * @param uncompressed_wasm - uncompressed wasm code
+   * @param stack_limit - the global stack limit
+   * @return patched code or error
+   */
   [[nodiscard]] outcome::result<common::Buffer, StackLimiterError>
   instrumentWithStackLimiter(common::BufferView uncompressed_wasm,
                              size_t stack_limit);
