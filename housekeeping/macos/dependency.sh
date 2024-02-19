025be81f52f071e1f7d56ea16d7febeb064b7068
Harrm <abrehchs@gmail.com>
2024-02-19 21:46:20 +0300

    Debug macos build

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -18,4 +18,4 @@ sudo python3 -m pip install cmake==3.25 requests gitpython gcovr
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.75.0 --profile minimal
 brew install ninja
 
-sudo xcode-select -s /Applications/Xcode_15.0.1.app/Contents/Developer
+sudo xcode-select -s /Applications/Xcode_15.2.app/Contents/Developer
