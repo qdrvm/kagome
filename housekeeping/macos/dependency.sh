2e707cc026a4eac6de342bcdaac1420e2a9dc7e0
Harrm <abrehchs@gmail.com>
2024-02-19 21:01:27 +0300

    Debug macos build

--- housekeeping/macos/dependency.sh
+++ housekeeping/macos/dependency.sh
@@ -17,3 +17,8 @@ sudo python3 -m pip install cmake==3.25 requests gitpython gcovr
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.75.0 --profile minimal
 brew install ninja
+
+file  /Applications/Xcode_15.0.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc
+/Applications/Xcode_15.0.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc --version
+/Applications/Xcode_15.0.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc -print-search-dirs
+/Applications/Xcode_15.0.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc -v
