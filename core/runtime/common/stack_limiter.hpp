7db07f18bae107e00a01102dddad09c308eb1c99
Harrm <abrehchs@gmail.com>
2024-02-19 20:24:52 +0300

    Add tests

--- core/runtime/common/stack_limiter.hpp
+++ core/runtime/common/stack_limiter.hpp
@@ -9,22 +9,40 @@
 #include <string>
 
 #include "common/buffer.hpp"
+#include "log/logger.hpp"
 #include "outcome/outcome.hpp"
+
+namespace wabt {
+  class Module;
+  class Func;
+}  // namespace wabt
 
 namespace kagome::runtime {
 
   struct StackLimiterError {
+    [[nodiscard]] const std::string &message() const {
+      return msg;
+    }
     std::string msg;
   };
+
+  // for tests
+  namespace detail {
+    outcome::result<uint32_t, StackLimiterError> compute_stack_cost(
+        const log::Logger &logger,
+        const wabt::Func &func,
+        const wabt::Module &module);
+  }
 
   inline boost::exception_ptr make_exception_ptr(const StackLimiterError &e) {
     return std::make_exception_ptr(std::runtime_error{e.msg});
   }
 
   /**
-   * Implements the same logic as substrate's https://github.com/paritytech/wasm-instrument
-   * Patches the wasm code, wrapping each function call in a check that
-   * this call is not going to exceed the global stack limit
+   * Implements the same logic as substrate's
+   * https://github.com/paritytech/wasm-instrument Patches the wasm code,
+   * wrapping each function call in a check that this call is not going to
+   * exceed the global stack limit
    * @param uncompressed_wasm - uncompressed wasm code
    * @param stack_limit - the global stack limit
    * @return patched code or error
