201163ee90e46142d8bca6efb078d63fc6c02cd0
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2024-02-21 00:22:16 +0300

    feature: beefy thread pool
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/network/beefy/beefy_thread_pool.hpp
+++ core/network/beefy/beefy_thread_pool.hpp
@@ -6,13 +6,17 @@
 
 #pragma once
 
+#include "injector/inject.hpp"
 #include "utils/thread_pool.hpp"
 #include "utils/watchdog.hpp"
 
 namespace kagome::network {
   class BeefyThreadPool final : public ThreadPool {
    public:
-    BeefyThreadPool(std::shared_ptr<Watchdog> watchdog)
+    BeefyThreadPool(std::shared_ptr<Watchdog> watchdog, Inject)
         : ThreadPool(std::move(watchdog), "beefy", 1, std::nullopt) {}
+
+    // Ctor for test purposes
+    BeefyThreadPool(TestThreadPool test) : ThreadPool{test} {}
   };
-}  // namespace kagome::consensus::grandpa
+}  // namespace kagome::network
