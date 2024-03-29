b14aa235b420969439c416e94a370096168efd7f
Harrm <abrehchs@gmail.com>
2024-03-29 11:35:50 +0300

    Start implementing secure mode

--- core/parachain/pvf/kagome_pvf_worker.cpp
+++ core/parachain/pvf/kagome_pvf_worker.cpp
@@ -11,6 +11,12 @@
 #include <string>
 #include <vector>
 
+#ifdef __linux__
+#include <sched.h>
+#include <sys/mount.h>
+#include <linux/seccomp.h>
+#endif
+
 #include <fmt/format.h>
 #include <libp2p/log/configurator.hpp>
 
@@ -33,6 +39,99 @@
 #include "runtime/runtime_context.hpp"
 
 // rust reference: polkadot-sdk/polkadot/node/core/pvf/execute-worker/src/lib.rs
+
+bool check_env_vars_are_empty(const char **env) {
+  if (env != nullptr) {
+    std::cout << "Env variables are set!:\n";
+    while (env) {
+      std::cout << *env << "\n";
+      env++;
+    }
+    std::cout
+        << "All env variables should be unset for PVF workers for security reasons\n";
+    return false;
+  }
+  return true;
+}
+
+std::error_code get_last_err() {
+  return std::error_code{static_cast<std::errc>(errno)};
+}
+
+enum class WorkerKind { Prepare, Execute, CheckPivotRoot };
+
+// This should not be called in a multi-threaded context. `unshare(2)`:
+// "CLONE_NEWUSER requires that the calling process is not threaded."
+std::error_code change_root(const std::filesystem::path &worker_dir,
+                            WorkerKind kind) {
+  if (::unshare(CLONE_NEWUSER | CLONE_NEWNS) == -1) {
+    return get_last_err();
+  }
+  if (::mount(nullptr, "/", nullptr, MS_REC | MS_PRIVATE, nullptr) == -1) {
+    return get_last_err();
+  }
+  int additional_flags = 0;
+  if (kind == WorkerKind::Prepare || kind == WorkerKind::CheckPivotRoot) {
+    additional_flags = MS_RDONLY;
+  }
+  if (::mount(worker_dir.c_str(),
+              worker_dir.c_str(),
+              nullptr,
+              MS_BIND | MS_REC | MS_NOEXEC | MS_NODEV | MS_NOSUID | MS_NOATIME
+                  | additional_flags,
+              nullptr)) {
+    return get_last_err();
+  }
+  if (::chdir(worker_dir.c_str()) == -1) {
+    return get_last_err();
+  }
+  if (::syscall(SYS_pivot_root, ".", ".") == -1) {
+    return get_last_err();
+  }
+  if (::umount2(".", MNT_DETACH) == -1) {
+    return get_last_err();
+  }
+  if (std::filesystem::current_path() != "/") {
+    return Error("Failed to change process mount root");
+  }
+  std::error_code err{};
+  std::filesystem::current_path("..", err);
+  if (err) {
+    return err;
+  }
+  if (std::filesystem::current_path() != "/") {
+    return Error(
+        "Successfully broke out of new root using .., which must not happen");
+  }
+  return {};
+}
+
+void enable_seccomp() {
+  std::array forbidden_calls {
+    SYS_socketpair,
+    SYS_socket,
+    SYS_connect,
+    SYS_io_uring_setup,
+    SYS_io_uring_enter,
+    SYS_io_uring_register,
+  };
+
+  // maybe 
+  if(prctl(PR_SET_NO_NEW_PRIVS, 1) == -1) {
+    return get_last_err();
+  }
+  struct sock_filter filter[] {
+    {},
+  };
+  struct sock_fprog filter_rules {
+
+  };
+  if(syscall(SYS_seccomp, SECCOMP_SET_MODE_FILTER, 0, ) == -1) {
+    return get_last_err();
+  }
+}
+
+void enable_landlock() {}
 
 namespace kagome::parachain {
   static kagome::log::Logger logger;
@@ -104,7 +203,10 @@ namespace kagome::parachain {
     return outcome::success();
   }
 
-  int pvf_worker_main(int argc, const char **argv) {
+  int pvf_worker_main(int argc, const char **argv, const char **env) {
+    if (!check_env_vars_are_empty(env)) {
+      return -1;
+    }
     auto logging_system = std::make_shared<soralog::LoggingSystem>(
         std::make_shared<kagome::log::Configurator>(
             std::make_shared<libp2p::log::Configurator>()));
