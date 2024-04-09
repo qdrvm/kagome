6799f97eadbd1cdd665beba30e75436d5e777d37
kamilsa <kamilsa16@gmail.com>
2024-04-09 15:58:36 +0500

    Update hunter to v0.25.3-qdrvm7 (#2035)
    * Update hunter to v0.25.3-qdrvm7
    
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>
    
    ---------
    
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>
    Co-authored-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm5.zip
-  SHA1 ae75fdf1850bd21dead239b8580012b6091be855
+  URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm7.zip
+  SHA1 be5869134ef7448fe2420d60dbb9706596b1b8bd
   LOCAL
 )
