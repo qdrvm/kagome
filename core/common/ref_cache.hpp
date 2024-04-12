2590fb1844a6718eff4beaa6981df3cbb382b1f1
iceseer <iceseer@gmail.com>
2024-04-12 11:14:06 +0300

    cache
    Signed-off-by: iceseer <iceseer@gmail.com>

---
+++ core/common/ref_cache.hpp
@@ -0,0 +1,95 @@
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <map>
+#include <memory>
+
+namespace kagome {
+
+    /**
+     * @brief Cache keeps an item inside if any reference exists. If all references lost, then item removes from cache. If an item exists, then it returns new reference on it.
+    */
+   template<typename K, typename V>
+    struct RefCache : std::enable_shared_from_this<RefCache> {
+        using Container = std::map<K, std::weak_ptr<RefObj>>;
+        using Iterator = Container::iterator;
+        using SRefCache = std::shared_ptr<RefCache>;
+        using Value = std::decay_t<V>;
+        using Key = K;
+
+        struct RefObj {
+            template<typename...Args>
+            RefObj(Args &&...args) : obj_(std::forward<Args>(args)...) {
+
+            }
+
+            ~RefObj() {
+                BOOST_ASSERT(keeper_);
+                keeper_->remove(it_);
+            }
+
+        private:
+            void store_internal_refs(Iterator it, SRefCache keeper) {
+                BOOST_ASSERT(keeper);
+                BOOST_ASSERT(!keeper_);
+
+                it_ = it;
+                keeper_ = keeper;
+            }
+
+            friend struct RefCache;
+            Value obj_;
+            Iterator it_;
+            SRefCache keeper_;
+        };
+        using SRefObj = std::shared_ptr<RefObj>;
+
+        /**
+         * @brief returns an element, if exists. Otherway `nullopt`.
+         * @param k is a key of an element
+        */
+        std::optional<SRefObj> get(const Key &k) {
+            [[likely]] if (auto it = items_.find(k); it != items_.end()) {
+                auto obj = it->second.lock();
+                BOOST_ASSERT(obj);
+                return obj;
+            }
+            return std::nullopt;
+        }
+        
+        /**
+         * @brief returns an existed element or creates new one and store it in cache.
+         * @param k is a key of an element
+         * @param f is a functor to create RefObj
+        */
+        template<typename F>
+        SRefObj get_or_insert(const Key &k, F &&f) {
+            [[likely]] if (auto it = items_.find(k); it != items_.end()) {
+                auto obj = it->second.lock();
+                BOOST_ASSERT(obj);
+                return obj;
+            }
+
+            auto obj = std::make_shared(std::forward<F>(f)());
+            auto [it, inserted] = items_.emplace(k, obj);
+            BOOST_ASSERT(inserted);
+
+            obj->store_internal_refs(it, shared_from_this());
+            return obj;
+        }
+
+        private:
+        void remove(const Iterator &it) {
+            items_.erase(it);
+        }
+
+        friend struct RefObj;
+        Container items_;
+    };
+
+}
