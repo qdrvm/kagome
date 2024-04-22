0911a4d823d4d97e9c18a762a75f20f535e17fa7
Harrm <abrehchs@gmail.com>
2024-04-22 17:27:43 +0300

    Make key store optional in host api and fix wavm build

--- core/host_api/impl/host_api_factory_impl.cpp
+++ core/host_api/impl/host_api_factory_impl.cpp
@@ -27,14 +27,14 @@ namespace kagome::host_api {
         ed25519_provider_(std::move(ed25519_provider)),
         secp256k1_provider_(std::move(secp256k1_provider)),
         hasher_(std::move(hasher)),
-        key_store_(std::move(key_store)),
+        key_store_(key_store ? std::optional(key_store) : std::nullopt),
         offchain_persistent_storage_(std::move(offchain_persistent_storage)),
         offchain_worker_pool_(std::move(offchain_worker_pool)) {
     BOOST_ASSERT(sr25519_provider_ != nullptr);
     BOOST_ASSERT(ed25519_provider_ != nullptr);
     BOOST_ASSERT(secp256k1_provider_ != nullptr);
     BOOST_ASSERT(hasher_ != nullptr);
-    BOOST_ASSERT(key_store_ != nullptr);
+    BOOST_ASSERT(key_store_ == std::nullopt || *key_store_ != nullptr);
   }
 
   std::unique_ptr<HostApi> HostApiFactoryImpl::make(
