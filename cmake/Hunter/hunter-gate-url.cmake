9a59209d502bdefae0407adc00daa21565e2fcc9
Harrm <abrehchs@gmail.com>
2024-04-29 16:37:11 +0300

    Update hunter url

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  https://github.com/qdrvm/hunter/archive/update/libsecp-0.4.1.zip
-  SHA1 6dbd120e1ba9f543be86957c00fdee4546274eec
+  URL  https://github.com/qdrvm/hunter/archive/316a12e2a327d8dad8aa705f77da7d5b2c0474e3.zip
+  SHA1 e4d602114f77509316f5913a4be24b18c6a21b0e
   LOCAL
 )
