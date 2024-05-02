322057742e6e02873ae0ff5b33961c84b785b46e
turuslan <turuslan.devbox@gmail.com>
2024-05-02 10:52:25 +0500

    fix clear_prefix (westend)

---
+++ core/primitives/kill_storage_result.hpp
@@ -0,0 +1,21 @@
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "scale/tie.hpp"
+
+namespace kagome {
+  using ClearPrefixLimit = std::optional<uint32_t>;
+
+  // https://github.com/paritytech/polkadot-sdk/blob/e5a93fbcd4a6acec7ab83865708e5c5df3534a7b/substrate/primitives/io/src/lib.rs#L159
+  struct KillStorageResult {
+    SCALE_TIE(2);
+
+    bool more{};
+    uint32_t loops{};
+  };
+}  // namespace kagome
