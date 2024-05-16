bd44f0304a0728959f5e914cc2bc995108db6d40
kamilsa <kamilsa16@gmail.com>
2024-05-16 10:42:52 +0500

    Try TSAN fix

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm10.zip
-  SHA1 9571399d8d091420131eb81f884521326c9d3615
-  LOCAL
+    URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm11.zip
+    SHA1 20620ee1e2e9738b2d4561be2622d71e8c73bee9
+    LOCAL
 )
