4643ea469b07aca3de174310660af65371b2a99b
kamilsa <kamilsa16@gmail.com>
2024-05-20 17:17:26 +0500

    Revert "use print with flush"
    This reverts commit 9e3fd0c9f4c2bcb926ea94ab1eaeb40f12fcff28.

--- zombienet/docker/test_runner.py
+++ zombienet/docker/test_runner.py
@@ -8,35 +8,32 @@ def run_test(test_command):
         return result.returncode
     except subprocess.CalledProcessError as e:
         return e.returncode
-def print_with_flush(message):
-    print(message)
-    sys.stdout.flush()
 
 def main():
     if len(sys.argv) != 2:
-        print_with_flush("Usage: python test_runner.py <test_command>")
+        print("Usage: python test_runner.py <test_command>")
         sys.exit(1)
 
     test_command = sys.argv[1]
 
-    print_with_flush(f"Running test: {test_command} (warm-up)")
+    print(f"Running test: {test_command} (warm-up)")
     first_attempt = run_test(test_command)
 
     if first_attempt == 0:
-        print_with_flush("Test passed on the first attempt (warm-up).")
+        print("Test passed on the first attempt (warm-up).")
         sys.exit(0)
 
-    print_with_flush("Warm-up test failed, running test again...")
+    print("Warm-up test failed, running test again...")
     time.sleep(10)
 
-    print_with_flush(f"Running test: {test_command} (second attempt)")
+    print(f"Running test: {test_command} (second attempt)")
     second_attempt = run_test(test_command)
 
     if second_attempt == 0:
-        print_with_flush("Test passed on the second attempt.")
+        print("Test passed on the second attempt.")
         sys.exit(0)
     else:
-        print_with_flush("Test failed on the second attempt.")
+        print("Test failed on the second attempt.")
         sys.exit(1)
 
 if __name__ == "__main__":
