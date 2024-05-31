649ea5e0bdbdd809e1d771cb5308cb316e9e4501
Ruslan Tushov <turuslan@users.noreply.github.com>
2024-05-31 09:47:07 +0500

    historical votes (#2085)
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>
    Co-authored-by: kamilsa <kamilsa16@gmail.com>

--- core/consensus/grandpa/grandpa.hpp
+++ core/consensus/grandpa/grandpa.hpp
@@ -8,6 +8,7 @@
 
 #include "consensus/finality_consensus.hpp"
 #include "consensus/grandpa/common.hpp"
+#include "consensus/grandpa/historical_votes.hpp"
 
 #include <memory>
 
@@ -21,7 +22,7 @@ namespace kagome::consensus::grandpa {
    * Interface for launching new grandpa rounds. See more details in
    * kagome::consensus::grandpa::GrandpaImpl
    */
-  class Grandpa : public FinalityConsensus {
+  class Grandpa : public FinalityConsensus, public SaveHistoricalVotes {
    public:
     virtual ~Grandpa() = default;
 
