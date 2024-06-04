dd15528a5c92a038063113b3fe58bc35b8d74376
kamilsa <kamilsa16@gmail.com>
2024-06-04 14:57:50 +0500

    Update hunter (rocksb 9.2.1)

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  https://github.com/qdrvm/hunter/archive/b459f62b9777b03297f7aa2763ea8e3e8ad6c09c.zip
-  SHA1 c83019b045cbf9d844ac7fcffc1560bd6bae1d63
+  URL  https://github.com/qdrvm/hunter/archive/6d034341211c062fe539fa0f48c738d3216e2ee3.zip
+  SHA1 933d6803a951d4548d56c8152f7874841741738d
   LOCAL
 )
