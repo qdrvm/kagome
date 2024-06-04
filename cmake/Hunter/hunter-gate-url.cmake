f451bcc3ac0152186843bad9eb040ef73cdbcd7d
kamilsa <kamilsa16@gmail.com>
2024-06-04 12:28:26 +0500

    Update hunter (boost 1.85)

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm13.zip
-  SHA1 4c01caa778ac85226bba7cbef4e966e2c7031b3f
+  URL  https://github.com/qdrvm/hunter/archive/b459f62b9777b03297f7aa2763ea8e3e8ad6c09c.zip
+  SHA1 c83019b045cbf9d844ac7fcffc1560bd6bae1d63
   LOCAL
 )
