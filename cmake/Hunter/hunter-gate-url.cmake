7b6a846852743e2b6293e8b597075a5e31a799e7
kamilsa <kamilsa16@gmail.com>
2024-06-04 16:38:49 +0500

    Update hunter (spdlog)

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  https://github.com/qdrvm/hunter/archive/6d034341211c062fe539fa0f48c738d3216e2ee3.zip
-  SHA1 933d6803a951d4548d56c8152f7874841741738d
+  URL  https://github.com/qdrvm/hunter/archive/22523de5e9ef26c3cc0d4a1bb443d6f90448755f.zip
+  SHA1 d6b8baab83021e8d660ad01385d24ce11a3b073e
   LOCAL
 )
