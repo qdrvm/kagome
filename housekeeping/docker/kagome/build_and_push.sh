d622e767c340e3e51140c78935ad330d7841a5e7
Harrm <abrehchs@gmail.com>
2024-06-06 17:35:26 +0300

    Add libwasmedge.so to docker

--- housekeeping/docker/kagome/build_and_push.sh
+++ housekeeping/docker/kagome/build_and_push.sh
@@ -62,6 +62,9 @@ mkdir -p ${CTX_DIR}
 # Copy binaries
 cp -a ${BUILD_DIR}/node/kagome ${CTX_DIR}/
 
+# wasmedge shared library
+cp $(ldd cmake-build-debug-wasmedge-clang-16/node/kagome | grep -o /.*/lib/libwasmedge.so[.0-9]*) ${CTX_DIR}/
+
 if [ "$BUILD_TYPE" = "Release" ] || [ "$BUILD_TYPE" = "Custom" ]; then
   docker build -t $TAG -f housekeeping/docker/kagome/minideb-release.Dockerfile ${CTX_DIR}
 
