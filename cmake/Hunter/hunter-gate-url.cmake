7d9346897e068a882f70bc21b2502d977e042e82
turuslan <turuslan.devbox@gmail.com>
2024-07-02 17:08:21 +0500

    deprecated unary binary
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-  URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm17.zip
-  SHA1 7b2137466c9884a56e7ce6adca8aefbe25bb43d9
+  URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm18.zip
+  SHA1 22d842b448f84a39392d7835f4046da34f8dcd70
   LOCAL
 )
