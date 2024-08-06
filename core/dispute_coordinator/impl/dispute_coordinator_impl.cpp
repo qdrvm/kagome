9d7229c7245c579cb0f83d119ffdf8818f7bbcb6
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2024-08-06 14:10:20 +0300

    format: adjust by clang-format-16
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/dispute_coordinator/impl/dispute_coordinator_impl.cpp
+++ core/dispute_coordinator/impl/dispute_coordinator_impl.cpp
@@ -1205,12 +1205,12 @@ namespace kagome::dispute {
     auto is_old_concluded_for =
         intermediate_result.old_state.dispute_status.has_value()
             ? is_type<ConcludedFor>(
-                  intermediate_result.old_state.dispute_status.value())
+                intermediate_result.old_state.dispute_status.value())
             : false;
     auto is_new_concluded_for =
         intermediate_result.new_state.dispute_status.has_value()
             ? is_type<ConcludedFor>(
-                  intermediate_result.new_state.dispute_status.value())
+                intermediate_result.new_state.dispute_status.value())
             : false;
     auto is_freshly_concluded_for =
         not is_old_concluded_for and is_new_concluded_for;
@@ -1218,12 +1218,12 @@ namespace kagome::dispute {
     auto is_old_concluded_against =
         intermediate_result.old_state.dispute_status.has_value()
             ? is_type<ConcludedAgainst>(
-                  intermediate_result.old_state.dispute_status.value())
+                intermediate_result.old_state.dispute_status.value())
             : false;
     auto is_new_concluded_against =
         intermediate_result.new_state.dispute_status.has_value()
             ? is_type<ConcludedAgainst>(
-                  intermediate_result.new_state.dispute_status.value())
+                intermediate_result.new_state.dispute_status.value())
             : false;
     auto is_freshly_concluded_against =
         not is_old_concluded_against and is_new_concluded_against;
@@ -1234,12 +1234,12 @@ namespace kagome::dispute {
     auto is_old_confirmed_concluded =
         intermediate_result.old_state.dispute_status.has_value()
             ? not is_type<Active>(
-                  intermediate_result.old_state.dispute_status.value())
+                intermediate_result.old_state.dispute_status.value())
             : false;
     auto is_new_confirmed_concluded =
         intermediate_result.new_state.dispute_status.has_value()
             ? not is_type<Active>(
-                  intermediate_result.new_state.dispute_status.value())
+                intermediate_result.new_state.dispute_status.value())
             : false;
     auto is_freshly_confirmed =
         not is_old_confirmed_concluded and is_new_confirmed_concluded;
