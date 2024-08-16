cb417454c684bcddaafb269a0b2b66d03471a592
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2024-08-16 14:42:11 +0300

    feature: convenient router mock
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- test/mock/core/network/router_mock.hpp
+++ test/mock/core/network/router_mock.hpp
@@ -10,6 +10,8 @@
 
 #include <gmock/gmock.h>
 
+#include "mock/core/network/protocol_mocks.hpp"
+
 namespace kagome::network {
   /**
    * Router, which reads and delivers different network messages to the
@@ -26,6 +28,9 @@ namespace kagome::network {
                 getFetchAvailableDataProtocol,
                 (),
                 (const, override));
+    auto getMockedFetchAvailableDataProtocol() const {
+      return fetch_available_data;
+    }
 
     MOCK_METHOD(std::shared_ptr<ValidationProtocol>,
                 getValidationProtocol,
@@ -66,11 +71,17 @@ namespace kagome::network {
                 getFetchChunkProtocol,
                 (),
                 (const, override));
+    auto getMockedFetchChunkProtocol() const {
+      return fetch_chunk;
+    }
 
     MOCK_METHOD(std::shared_ptr<FetchChunkProtocolObsolete>,
                 getFetchChunkProtocolObsolete,
                 (),
                 (const, override));
+    auto getMockedFetchChunkProtocolObsolete() const {
+      return fetch_chunk_obsolete;
+    }
 
     MOCK_METHOD(std::shared_ptr<FetchAttestedCandidateProtocol>,
                 getFetchAttestedCandidateProtocol,
@@ -112,9 +123,62 @@ namespace kagome::network {
                 (),
                 (const, override));
 
-    MOCK_METHOD(std::shared_ptr<libp2p::protocol::Ping>,
-                getPingProtocol,
-                (),
-                (const, override));
+    MOCK_METHOD(std::shared_ptr<Ping>, getPingProtocol, (), (const, override));
+
+    void setReturningMockedProtocols() {
+      // clang-format off
+      // ON_CALL(*this, getBlockAnnounceProtocol()).WillByDefault(testing::Return(block_announce));
+      // ON_CALL(*this, getGrandpaProtocol()).WillByDefault(testing::Return(grandpa));
+      // ON_CALL(*this, getSyncProtocol()).WillByDefault(testing::Return(sync));
+      // ON_CALL(*this, getStateProtocol()).WillByDefault(testing::Return(state));
+      // ON_CALL(*this, getWarpProtocol()).WillByDefault(testing::Return(warp));
+      // ON_CALL(*this, getBeefyProtocol()).WillByDefault(testing::Return(beefy));
+      // ON_CALL(*this, getBeefyJustificationProtocol()).WillByDefault(testing::Return(beefy_justifications));
+      // ON_CALL(*this, getLightProtocol()).WillByDefault(testing::Return(light));
+      // ON_CALL(*this, getPropagateTransactionsProtocol()).WillByDefault(testing::Return(propagate_transactions));
+      // ON_CALL(*this, getValidationProtocol()).WillByDefault(testing::Return(validation));
+      // ON_CALL(*this, getCollationProtocol()).WillByDefault(testing::Return(collation));
+      // ON_CALL(*this, getCollationProtocolVStaging()).WillByDefault(testing::Return(collation_vstaging));
+      // ON_CALL(*this, getValidationProtocolVStaging()).WillByDefault(testing::Return(validation_vstaging));
+      // ON_CALL(*this, getReqCollationProtocol()).WillByDefault(testing::Return(req_collation));
+      // ON_CALL(*this, getReqPovProtocol()).WillByDefault(testing::Return(req_pov));
+      fetch_chunk = std::make_shared<FetchChunkProtocolMock>();
+      ON_CALL(*this, getFetchChunkProtocol()).WillByDefault(testing::Return(fetch_chunk));
+      fetch_chunk_obsolete = std::make_shared<FetchChunkProtocolObsoleteMock>();
+      ON_CALL(*this, getFetchChunkProtocolObsolete()).WillByDefault(testing::Return(fetch_chunk_obsolete));
+      fetch_available_data = std::make_shared<FetchAvailableDataProtocolMock>();
+      ON_CALL(*this, getFetchAvailableDataProtocol()).WillByDefault(testing::Return(fetch_available_data));
+      // ON_CALL(*this, getStatementFetchingProtocol()).WillByDefault(testing::Return(statement_fetching));
+      // ON_CALL(*this, getSendDisputeProtocol()).WillByDefault(testing::Return(send_dispute));
+      // ON_CALL(*this, getPing()).WillByDefault(testing::Return(ping));
+      // ON_CALL(*this, getFetchAttestedCandidateProtocol()).WillByDefault(testing::Return(fetch_attested_candidate));
+      // clang-format on
+    }
+
+   private:
+    // clang-format off
+    // std::shared_ptr<BlockAnnounceProtocolMock> block_announce;
+    // std::shared_ptr<GrandpaProtocolMock> grandpa;
+    // std::shared_ptr<SyncProtocolMock> sync;
+    // std::shared_ptr<StateProtocolMock> state;
+    // std::shared_ptr<WarpProtocolMock> warp;
+    // std::shared_ptr<BeefyProtocolMock> beefy;
+    // std::shared_ptr<BeefyJustificationProtocolMock> beefy_justifications;
+    // std::shared_ptr<LightProtocolMock> light;
+    // std::shared_ptr<PropagateTransactionsProtocolMock> propagate_transactions;
+    // std::shared_ptr<ValidationProtocolMock> validation;
+    // std::shared_ptr<CollationProtocolMock> collation;
+    // std::shared_ptr<CollationProtocolVStagingMock> collation_vstaging;
+    // std::shared_ptr<ValidationProtocolVStagingMock> validation_vstaging;
+    // std::shared_ptr<ReqCollationProtocolMock> req_collation;
+    // std::shared_ptr<ReqPovProtocolMock> req_pov;
+    std::shared_ptr<FetchChunkProtocolMock> fetch_chunk;
+    std::shared_ptr<FetchChunkProtocolObsoleteMock> fetch_chunk_obsolete;
+    std::shared_ptr<FetchAvailableDataProtocolMock> fetch_available_data;
+    // std::shared_ptr<StatementFetchingProtocolMock> statement_fetching;
+    // std::shared_ptr<SendDisputeProtocolMock> send_dispute;
+    // std::shared_ptr<PingMock> ping;
+    // std::shared_ptr<FetchAttestedCandidateProtocolMock> fetch_attested_candidate;
+    // clang-format on
   };
 }  // namespace kagome::network
