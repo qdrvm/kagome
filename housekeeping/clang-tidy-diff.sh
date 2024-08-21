92c45e0630dbd9ad637737ba1c5c83a7b9aba9df
Kirill Azovtsev <k.azovtsev@qdrvm.io>
2024-08-21 10:33:36 +0300

    test new tidy workflow

--- housekeeping/clang-tidy-diff.sh
+++ housekeeping/clang-tidy-diff.sh
@@ -10,5 +10,6 @@ CLANG_TIDY_BIN="${CLANG_TIDY_BIN:-clang-tidy}"
 
 cd $(dirname $0)/..
 # exclude WAVM because on CI clang-tidy is run on a WasmEdge build
+git fetch origin master
 git diff -U0 origin/master -- . ':!core/runtime/wavm' | clang-tidy-diff.py -p1 -path ${BUILD_DIR} -iregex '(core|node)\/.*\.(h|c|hpp|cpp)' -clang-tidy-binary ${CLANG_TIDY_BIN} | tee clang-tidy.log
 ! grep ': error:' clang-tidy.log
