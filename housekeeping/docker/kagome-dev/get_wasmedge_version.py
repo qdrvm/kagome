f267f439f440c93d1cdf6829cc14124b72e34837
Kirill Azovtsev <k.azovtsev@qdrvm.io>
2024-08-28 17:21:47 +0300

    get wasmedge version fix

--- housekeeping/docker/kagome-dev/get_wasmedge_version.py
+++ housekeeping/docker/kagome-dev/get_wasmedge_version.py
@@ -1,6 +1,7 @@
 import re
 import sys
 
+
 def extract_version_and_sha(config_file_path, package_name):
     with open(config_file_path, 'r') as file:
         content = file.read()
@@ -9,20 +10,29 @@ def extract_version_and_sha(config_file_path, package_name):
         r'hunter_config\(\s*' + re.escape(package_name) + r'\s*.*?URL\s+.*?/refs/heads/[^/]+/([\d\.]+)\.zip',
         re.DOTALL
     )
-    sha_pattern = re.compile(r'set\(\s*WASMEDGE_ID\s+(\w+)\)', re.DOTALL)
-
     version_match = version_pattern.search(content)
+    version = version_match.group(1) if version_match else None
+
+    return version
+
+
+def extract_sha(config_file_path):
+    with open(config_file_path, 'r') as file:
+        content = file.read()
+
+    sha_pattern = re.compile(r'set\(\s*WASMEDGE_ID\s+(\w+)\)', re.DOTALL)
     sha_match = sha_pattern.search(content)
 
-    version = version_match.group(1) if version_match else None
     sha = sha_match.group(1)[:7] if sha_match else None
 
-    return version, sha
+    return sha
+
 
 if __name__ == "__main__":
     config_file_path = '../../../cmake/Hunter/config.cmake'
+    cmake_list_path = '../../../CMakeLists.txt'
     package_name = 'WasmEdge'
 
-    version, sha = extract_version_and_sha(config_file_path, package_name)
+    version = extract_version_and_sha(config_file_path, package_name)
+    sha = extract_sha(cmake_list_path)
     print(f"{version}-{sha}")
-    
