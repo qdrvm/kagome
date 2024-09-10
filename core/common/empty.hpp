b2c43a419004ec11cea528a44e7de647c19bfcdb
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2024-09-10 10:29:42 +0300

    Revert "refactor: Empty"
    This reverts commit 47a1133f9d308557458d1c84b6fd0a69e308ecbe.

--- core/common/empty.hpp
+++ core/common/empty.hpp
@@ -6,12 +6,26 @@
 
 #pragma once
 
-#include <tuple>
+#include <scale/scale.hpp>
 
 namespace kagome {
 
   /// Special zero-size-type for some things
   /// (e.g. unsupported, experimental or empty).
-  using Empty = std::tuple<>;
+  struct Empty {
+    inline constexpr bool operator==(const Empty &) const {
+      return true;
+    }
+
+    template <class Stream>
+    friend inline Stream &operator<<(Stream &s, const Empty &) {
+      return s;
+    }
+
+    template <class Stream>
+    friend inline Stream &operator>>(Stream &s, const Empty &) {
+      return s;
+    }
+  };
 
 }  // namespace kagome
