c45570ad4740b58c4b4e6453ce976a023d75edee
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2024-09-24 15:17:16 +0300

    fix: formatter of optional
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/log/formatters/optional.hpp
+++ core/log/formatters/optional.hpp
@@ -14,8 +14,10 @@
 
 #include <boost/optional.hpp>
 
-template <typename T>
-struct fmt::formatter<std::optional<T>> {
+template <template <typename> class Optional, typename T>
+  requires std::is_same_v<Optional<T>, std::optional<T>>
+        or std::is_same_v<Optional<T>, boost::optional<T>>
+struct fmt::formatter<Optional<T>> {
   constexpr auto parse(format_parse_context &ctx) -> decltype(ctx.begin()) {
     // Parse the presentation format and store it in the formatter:
     auto it = ctx.begin(), end = ctx.end();
@@ -30,38 +32,7 @@ struct fmt::formatter<std::optional<T>> {
   }
 
   template <typename FormatContext>
-  auto format(const std::optional<T> &opt, FormatContext &ctx) const
-      -> decltype(ctx.out()) {
-    // ctx.out() is an output iterator to write to.
-
-    if (opt.has_value()) {
-      return fmt::format_to(ctx.out(), "{}", opt.value());
-    }
-    static constexpr std::string_view message("<none>");
-    return std::copy(std::begin(message), std::end(message), ctx.out());
-  }
-};
-
-template <typename T>
-struct fmt::formatter<boost::optional<T>> {
-  // Parses format specifications of the form ['s' | 'l'].
-  constexpr auto parse(format_parse_context &ctx) -> decltype(ctx.begin()) {
-    // Parse the presentation format and store it in the formatter:
-    auto it = ctx.begin(), end = ctx.end();
-
-    // Check if reached the end of the range:
-    if (it != end && *it != '}') {
-      throw fmt::format_error("invalid format");
-    }
-
-    // Return an iterator past the end of the parsed range:
-    return it;
-  }
-
-  // Formats the BlockInfo using the parsed format specification (presentation)
-  // stored in this formatter.
-  template <typename FormatContext>
-  auto format(const boost::optional<T> &opt, FormatContext &ctx) const
+  auto format(const Optional<T> &opt, FormatContext &ctx) const
       -> decltype(ctx.out()) {
     // ctx.out() is an output iterator to write to.
 
