adb61668765b4e5940bdeee0f50f19af75026cd6
kamilsa <kamilsa16@gmail.com>
2024-10-07 14:48:31 +0500

    Add lsquic

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -111,6 +111,12 @@ hunter_config(
     SHA1 695f57f257451e54a50f910063aa4c32a13e2afe
 )
 
+hunter_config(lsquic
+    URL  https://github.com/qdrvm/lsquic/archive/b79ff5c4be9936089d32dc1bc2dac70d54651e80.zip
+    SHA1 d628f8d7ec68ec33d7f404fbb7b433c62980aa13
+    KEEP_PACKAGE_SOURCES
+)
+
 hunter_config(
     erasure_coding_crust
 #    VERSION 0.0.8
