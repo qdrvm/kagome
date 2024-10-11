d7884d18e23782eefb26a2c98854b4e41bee4eea
iceseer <iceseer@gmail.com>
2024-10-11 15:32:13 +0300

    per session/relay parent
    Signed-off-by: iceseer <iceseer@gmail.com>

---
+++ core/parachain/validator/statement_distribution/per_relay_parent_state.hpp
@@ -0,0 +1,70 @@
+
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <libp2p/peer/peer_id.hpp>
+#include <memory>
+#include <unordered_map>
+
+#include "parachain/backing/cluster.hpp"
+#include "parachain/backing/grid_tracker.hpp"
+#include "parachain/types.hpp"
+#include "parachain/validator/impl/statements_store.hpp"
+
+namespace kagome::parachain::statement_distribution {
+
+  /// polkadot/node/network/statement-distribution/src/v2/mod.rs
+  struct ActiveValidatorState {
+    // The index of the validator.
+    ValidatorIndex index;
+    // our validator group
+    GroupIndex group;
+    // the assignment of our validator group, if any.
+    std::optional<ParachainId> assignment;
+    // the 'direct-in-group' communication at this relay-parent.
+    ClusterTracker cluster_tracker;
+  };
+
+  struct LocalValidatorState {
+    // the grid-level communication at this relay-parent.
+    grid::GridTracker grid_tracker;
+    // additional fields in case local node is an active validator.
+    std::optional<ActiveValidatorState> active;
+  };
+
+  struct RelayParentState {
+    std::optional<LocalValidatorState> local_validator;
+    StatementStore statement_store;
+    size_t seconding_limit;
+    SessionIndex session;
+    std::unordered_map<ParachainId, std::vector<GroupIndex>> groups_per_para;
+    std::unordered_set<ValidatorIndex> disabled_validators;
+
+    std::optional<const ActiveValidatorState &> active_validator_state() const {
+      if (local_validator && local_validator->active) {
+        return local_validator->active.value();
+      }
+      return std::nullopt;
+    }
+
+    bool is_disabled(ValidatorIndex validator_index) const {
+      return disabled_validators.contains(validator_index);
+    }
+
+    scale::BitVec disabled_bitmask(
+        const std::span<const ValidatorIndex> &group) const {
+      scale::BitVec v;
+      v.bits.resize(group.size());
+      for (size_t ix = 0; ix < group.size(); ++ix) {
+        v.bits[ix] = is_disabled(group[ix]);
+      }
+      return v;
+    }
+  };
+
+}  // namespace kagome::parachain::statement_distribution
---
+++ core/parachain/validator/statement_distribution/per_session_state.hpp
@@ -0,0 +1,103 @@
+
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+
+#include <libp2p/peer/peer_id.hpp>
+#include "parachain/groups.hpp"
+#include "parachain/types.hpp"
+#include "utils/safe_object.hpp"
+
+namespace kagome::parachain::statement_distribution {
+
+  using LocalValidatorIndex = std::optional<ValidatorIndex>;
+
+  struct PerSessionState final {
+    PerSessionState(const PerSessionState &) = delete;
+    PerSessionState &operator=(const PerSessionState &) = delete;
+
+    PerSessionState(PerSessionState &&) = default;
+    PerSessionState &operator=(PerSessionState &&) = delete;
+
+    SessionIndex session;
+    runtime::SessionInfo session_info;
+    Groups groups;
+    std::optional<grid::Views> grid_view;
+    LocalValidatorIndex local_validator;
+
+    PerSessionState(SessionIndex _session,
+                    runtime::SessionInfo _session_info,
+                    Groups &&_groups,
+                    grid::Views &&_grid_view,
+                    std::optional<ValidatorIndex> _our_index,
+                    std::shared_ptr<PeerUseCount> peers)
+        : session{_session},
+          session_info{std::move(_session_info)},
+          groups{std::move(_groups)},
+          grid_view{std::move(_grid_view)},
+          our_index{_our_index},
+          peers{std::move(peers)} {
+      if (our_index) {
+        our_group = groups.byValidatorIndex(*our_index);
+      }
+      if (our_group) {
+        BOOST_ASSERT(*our_group < session_info.validator_groups.size());
+        if (grid_view) {
+          BOOST_ASSERT(*our_group < grid_view->size());
+        }
+      }
+      updatePeers(true);
+    }
+
+    ~PerSessionState() {
+      updatePeers(false);
+    }
+
+    void updatePeers(bool add) const {
+      if (not our_index or not our_group or not this->peers) {
+        return;
+      }
+      auto &peers = *this->peers;
+      SAFE_UNIQUE(peers) {
+        auto f = [&](ValidatorIndex i) {
+          auto &id = session_info.discovery_keys[i];
+          auto it = peers.find(id);
+          if (add) {
+            if (it == peers.end()) {
+              it = peers.emplace(id, 0).first;
+            }
+            ++it->second;
+          } else {
+            if (it == peers.end()) {
+              throw std::logic_error{"inconsistent PeerUseCount"};
+            }
+            --it->second;
+            if (it->second == 0) {
+              peers.erase(it);
+            }
+          }
+        };
+        for (auto &i : session_info.validator_groups[*our_group]) {
+          f(i);
+        }
+        if (grid_view) {
+          auto &view = grid_view->at(*our_group);
+          for (auto &i : view.sending) {
+            f(i);
+          }
+          for (auto &i : view.receiving) {
+            f(i);
+          }
+        }
+      };
+    }
+  };
+
+}  // namespace kagome::parachain::statement_distribution
