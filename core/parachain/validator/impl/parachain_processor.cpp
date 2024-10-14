bc3d20ed8b506539688ec50f889de5b4ae247e96
kamilsa <kamilsa16@gmail.com>
2024-10-14 16:42:24 +0500

    Reduce logs (send my view)

--- core/parachain/validator/impl/parachain_processor.cpp
+++ core/parachain/validator/impl/parachain_processor.cpp
@@ -4504,7 +4504,7 @@ namespace kagome::parachain {
     BOOST_ASSERT(protocol);
     CHECK_OR_RET(canProcessParachains().has_value());
 
-    SL_INFO(logger_,
+    SL_DEBUG(logger_,
             "Send my view.(peer={}, protocol={})",
             peer_id,
             protocol->protocolName());
