3b6ac1f633b18927dfecf857a82329e0a9dfabc5
Kirill Azovtsev <k.azovtsev@qdrvm.io>
2024-10-22 10:41:32 +0300

    ci - polkadot sdk tag support

--- zombienet/docker/version.py
+++ zombienet/docker/version.py
@@ -52,17 +52,22 @@ def select_release_tags_stable(all_tags):
     return tags
 
 
-def get_version(repo_url):
+def get_version(repo_url, version=None):
     repo_short_name = repo_url.split("/")[-1].split(".")[0]
-    all_tags = list_all_tags_for_remote_git_repo(repo_url)
-    print(f"All tags received: {all_tags}")
 
-    release_tags_stable = select_release_tags_stable(all_tags)
-    print(f"Filtered stable release tags: {release_tags_stable}")
-    last_tag_stable = get_last_tag_stable(release_tags_stable)
-    print(f"Latest stable release: {last_tag_stable}")
+    if version:
+        print(f"Using provided version: {version}")
+        write_file(repo_short_name, version)
+    else:
+        all_tags = list_all_tags_for_remote_git_repo(repo_url)
+        print(f"All tags received: {all_tags}")
 
-    write_file(repo_short_name, last_tag_stable)
+        release_tags_stable = select_release_tags_stable(all_tags)
+        print(f"Filtered stable release tags: {release_tags_stable}")
+        last_tag_stable = get_last_tag_stable(release_tags_stable)
+        print(f"Latest stable release: {last_tag_stable}")
+
+        write_file(repo_short_name, last_tag_stable)
 
 
 def help():
@@ -70,15 +75,17 @@ def help():
     This script: 
       - retrieves tags from a remote Git repository,
       - filters and sorts these tags to identify the latest stable release tag (e.g., polkadot-stable2409),
+      - or uses a provided version if specified,
       - writes detailed version information to a file.
 
     Usage:
-      python version.py <repository_url>
+      python version.py <repository_url> [version]
       - <repository_url>: URL of the Git repository to process (e.g., https://github.com/paritytech/polkadot.git).
+      - [version]: Optional, specify a version to use instead of fetching the latest from the repo.
 
     Result:
       The script generates a file named [repo_short_name]-versions.txt containing key-value pairs of version information:
-        - 'version': the latest stable version tag (e.g., polkadot-stable2409),
+        - 'version': the specified or latest stable version tag (e.g., polkadot-stable2409),
         - 'short_version': the short version string (e.g., stable2409),
         - 'numeric_version': the numeric part of the version (e.g., 2409),
         - 'polkadot_format_version': the complete version tag if it includes 'polkadot-' prefix.
@@ -93,6 +100,10 @@ def main():
 
     if len(args) == 1:
         get_version(args[0])
+    elif len(args) == 2:
+        get_version(args[0], args[1])
+    else:
+        print("Invalid usage. Use --help for more information.")
 
 
 main()
