9e46a94829d8d13e9528f84ce986fa1616146914
Robert Hambrock <roberthambrock@gmail.com>
2024-11-01 02:19:24 +0100

    add rust support & inline relevant init.sh
    except python

--- flake.nix
+++ flake.nix
@@ -6,20 +6,19 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in {
       devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell.override
-          {
-            # Override stdenv in order to change compiler:
-            # stdenv = pkgs.clangStdenv;
-          }
-          {
-            packages = with pkgs; [
+        default = pkgs.mkShell.override {
+          # Override stdenv in order to change compiler:
+          # stdenv = pkgs.clangStdenv;
+        } {
+          packages = with pkgs;
+            [
               clang-tools
               cmake
               codespell
@@ -28,8 +27,41 @@
               doxygen
               gtest
               lcov
+
+              # Rust tools
+              rustup
+              cargo
+              rust-analyzer
+
             ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
-          };
+
+          shellHook = ''
+            export RUSTUP_HOME="$PWD/.rustup"
+            export CARGO_HOME="$PWD/.cargo"
+            export PATH="$CARGO_HOME/bin:$PATH"
+
+            echo "Using RUSTUP_HOME: $RUSTUP_HOME"
+            echo "Using CARGO_HOME: $CARGO_HOME"
+            echo "Current PATH: $PATH"
+
+            export RUST_VERSION=1.77.0
+
+            export CMAKE_VERSION="3.29"
+            export CMAKE_CORE_NUMBER=10
+
+
+            if ! rustup show | grep -q "$RUST_VERSION"; then
+                echo "Installing Rust version $RUST_VERSION"
+                    rustup toolchain install $RUST_VERSION
+                    rustup default $RUST_VERSION
+            else
+                echo "Rust version $RUST_VERSION already installed and set as default."
+                    fi
+
+                    rustup show
+          '';
+
+        };
       });
     };
 }
