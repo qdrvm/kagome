b7aab3c6168637138cf5d8debf6810090a797185
Robert Hambrock <roberthambrock@gmail.com>
2024-11-02 22:49:06 +0100

    temp hack: correct curses linking

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,6 @@ cd $parent_dir
 set -a; source $current_dir/.env; set +a #include .env vars 
 
 source $parent_dir/venv/bin/activate
-$parent_dir/venv/bin/cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Release
-$parent_dir/venv/bin/cmake --build build --target kagome -j ${CMAKE_CORE_NUMBER}
+cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Release
+sed -i 's/lcurses/lncurses/' build/node/CMakeFiles/kagome.dir/link.txt
+cmake --build build --target kagome -j ${CMAKE_CORE_NUMBER}
