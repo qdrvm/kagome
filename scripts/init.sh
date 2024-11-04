51cfeb0871edfce8684a876dc17638e6562e246b
Robert Hambrock <roberthambrock@gmail.com>
2024-11-04 12:31:33 +0100

    check if initiated via flake

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,15 +4,21 @@ current_dir=$(dirname $(readlink -f "$0"))
 parent_dir=$(dirname "$current_dir")
 cd $parent_dir
 
-set -a; source $current_dir/.env; set +a #include .env vars 
+set -a; source $current_dir/.env; set +a #include .env vars
 
-apt update
-apt install --no-install-recommends -y \
-      build-essential git gcc ca-certificates python-is-python3 python3-pip \
-      python3-venv curl libgmp-dev libncurses6 libncurses-dev libnsl-dev libseccomp-dev
+# if not using nix, install the following packages
+if [ -z "$FLAKE_INITIATED" ]; then
+    apt update
+    apt install --no-install-recommends -y \
+        build-essential git gcc ca-certificates python-is-python3 python3-pip \
+        python3-venv curl libgmp-dev libncurses6 libncurses6-dev libnsl-dev libseccomp-dev
+fi
 
 python3 -m venv "$parent_dir/venv"
-
 echo "Python environment created successfully in $parent_dir/venv"
 
-$parent_dir/venv/bin/pip install --no-cache-dir cmake==${CMAKE_VERSION} gitpython requests
+if [ -z "$FLAKE_INITIATED" ]; then
+    $parent_dir/venv/bin/pip install --no-cache-dir cmake==${CMAKE_VERSION} gitpython requests
+else
+    $parent_dir/venv/bin/pip install --no-cache-dir gitpython requests
+fi
