ce02de9d80a52a5905056fbde143c7035640923a
turuslan <turuslan.devbox@gmail.com>
2024-11-19 11:47:25 +0500

    check core index
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- core/parachain/candidate_descriptor_v2.hpp
+++ core/parachain/candidate_descriptor_v2.hpp
@@ -10,9 +10,16 @@
 
 #include "crypto/sr25519_provider.hpp"
 #include "parachain/pvf/pvf_error.hpp"
+#include "parachain/transpose_claim_queue.hpp"
 #include "parachain/types.hpp"
+#include "parachain/ump_signal.hpp"
 
 namespace kagome::network {
+  /// The default claim queue offset to be used if it's not
+  /// configured/accessible in the parachain
+  /// runtime
+  constexpr uint8_t kDefaultClaimQueueOffset = 0;
+
   inline bool isV1(const CandidateDescriptor &descriptor) {
     constexpr auto is_zero = [](BufferView xs) {
       return static_cast<size_t>(std::ranges::count(xs, 0)) == xs.size();
@@ -59,5 +66,72 @@ namespace kagome::network {
       return std::nullopt;
     }
     return boost::endian::load_little_u32(&descriptor.reserved_1[3]);
+  }
+
+  enum class CheckCoreIndexError {
+    InvalidCoreIndex,
+    CoreIndexMismatch,
+    NoAssignment,
+    UnknownVersion,
+  };
+  Q_ENUM_ERROR_CODE(CheckCoreIndexError) {
+    using E = decltype(e);
+    switch (e) {
+      case E::InvalidCoreIndex:
+        return "The specified core index is invalid";
+      case E::CoreIndexMismatch:
+        return "The core index in commitments doesn't match the one in "
+               "descriptor";
+      case E::NoAssignment:
+        return "The parachain is not assigned to any core at specified claim "
+               "queue offset";
+      case E::UnknownVersion:
+        return "Unknown internal version";
+    }
+    abort();
+  }
+
+  /// Checks if descriptor core index is equal to the committed core index.
+  /// Input `cores_per_para` is a claim queue snapshot at the candidate's relay
+  /// parent, stored as a mapping between `ParaId` and the cores assigned per
+  /// depth.
+  inline outcome::result<void> checkCoreIndex(
+      const CommittedCandidateReceipt &receipt,
+      const TransposedClaimQueue &claims) {
+    if (isV1(receipt.descriptor)) {
+      return outcome::success();
+    }
+    if (not isV2(receipt.descriptor)) {
+      return CheckCoreIndexError::UnknownVersion;
+    }
+    OUTCOME_TRY(selector, coreSelector(receipt.commitments));
+    auto offset =
+        selector ? selector->claim_queue_offset : kDefaultClaimQueueOffset;
+    auto it1 = claims.find(receipt.descriptor.para_id);
+    if (it1 == claims.end()) {
+      return CheckCoreIndexError::NoAssignment;
+    }
+    auto it2 = it1->second.find(offset);
+    if (it2 == it1->second.end()) {
+      return CheckCoreIndexError::NoAssignment;
+    }
+    auto &assigned_cores = it2->second;
+    if (assigned_cores.empty()) {
+      return CheckCoreIndexError::NoAssignment;
+    }
+    auto core = coreIndex(receipt.descriptor).value();
+    if (not selector and assigned_cores.size() > 1) {
+      if (not assigned_cores.contains(core)) {
+        return CheckCoreIndexError::InvalidCoreIndex;
+      }
+      return outcome::success();
+    }
+    auto expected_core = *std::next(
+        assigned_cores.begin(),
+        (selector ? selector->core_selector % assigned_cores.size() : 0));
+    if (core != expected_core) {
+      return CheckCoreIndexError::InvalidCoreIndex;
+    }
+    return outcome::success();
   }
 }  // namespace kagome::network
