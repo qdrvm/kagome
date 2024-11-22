a38a5ba892c4bd5d0b5fc42685275cfb5324b450
Harrm <abrehchs@gmail.com>
2024-11-22 15:41:37 +0300

    Migrate leaves

--- core/storage/migrations/migrations.cpp
+++ core/storage/migrations/migrations.cpp
@@ -1,11 +1,14 @@
 #include "storage/migrations/migrations.hpp"
 #include <qtils/outcome.hpp>
 #include <soralog/macro.hpp>
+#include <unordered_set>
 
 #include "blockchain/block_tree.hpp"
 #include "blockchain/block_tree_error.hpp"
+#include "common/blob.hpp"
 #include "injector/application_injector.hpp"
 #include "log/logger.hpp"
+#include "primitives/common.hpp"
 #include "storage/spaced_storage.hpp"
 #include "storage/trie/trie_storage.hpp"
 #include "storage/trie/trie_storage_backend.hpp"
@@ -26,14 +29,12 @@ namespace kagome::storage::migrations {
       return outcome::success();
     }
     auto current = block_tree.getLastFinalized().hash;
-    // TODO: an error other than just an absent block may occur
-    for (auto header = block_tree.getBlockHeader(current); header.has_value();
-         header = block_tree.getBlockHeader(current)) {
-      SL_VERBOSE(logger, "Migrating block {}...", header.value().blockInfo());
 
+    auto migrate_tree =
+        [&storage,
+         &trie_storage](common::Hash256 state_root) -> outcome::result<void> {
       auto batch = storage.createBatch();
-      OUTCOME_TRY(trie_batch,
-                  trie_storage.getEphemeralBatchAt(header.value().state_root));
+      OUTCOME_TRY(trie_batch, trie_storage.getEphemeralBatchAt(state_root));
       auto cursor = trie_batch->trieCursor();
       while (cursor->isValid()) {
         auto value_and_hash = cursor->value_and_hash();
@@ -41,13 +42,47 @@ namespace kagome::storage::migrations {
           continue;
         }
         auto &[value, hash] = *value_and_hash;
-        OUTCOME_TRY(batch->put(Space::kTrieValue, hash, value));
-        OUTCOME_TRY(batch->remove(Space::kTrieNode, hash));
+        OUTCOME_TRY(present,
+                    storage.getSpace(Space::kTrieValue)->contains(hash));
+        if (!present) {
+          OUTCOME_TRY(batch->put(Space::kTrieValue, hash, value));
+          OUTCOME_TRY(batch->remove(Space::kTrieNode, hash));
+        }
         OUTCOME_TRY(cursor->next());
       }
       OUTCOME_TRY(batch->commit());
+      return outcome::success();
+    };
+    // TODO: an error other than just an absent block may occur
+    for (auto header = block_tree.getBlockHeader(current); header.has_value();
+         header = block_tree.getBlockHeader(current)) {
+      SL_VERBOSE(logger, "Migrating block {}...", header.value().blockInfo());
+      OUTCOME_TRY(migrate_tree(header.value().state_root));
       current = header.value().parent_hash;
     }
+
+    std::unordered_set<primitives::BlockHash> pending;
+    for (auto leaf_hash : block_tree.getLeaves()) {
+      pending.insert(leaf_hash);
+    }
+    while (!pending.empty()) {
+      auto current_it = pending.begin();
+      primitives::BlockHash current = *current_it;
+      pending.erase(current_it);
+      auto header = block_tree.getBlockHeader(current);
+      // TODO: an error other than just an absent block may occur
+      if (!header) {
+        continue;
+      }
+      // already processed finalized blocks above
+      if (header.value().parent_hash != block_tree.getLastFinalized().hash) {
+        pending.insert(header.value().parent_hash);
+      }
+      SL_VERBOSE(logger, "Migrating block {}...", header.value().blockInfo());
+
+      OUTCOME_TRY(migrate_tree(header.value().state_root));
+    }
+
     SL_INFO(logger, "Trie storate migration ended successfully");
     return outcome::success();
   }
