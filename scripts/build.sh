55e1f3e5914245a78c367e057dab6d0487a95712
kamilsa <kamilsa16@gmail.com>
2024-12-11 15:18:13 +0500

    Update compiler flags

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@
 set -a; source $current_dir/.env; set +a #include .env vars 
 
 source $parent_dir/venv/bin/activate
-$parent_dir/venv/bin/cmake -H. -Bbuild -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_BUILD_TYPE=Release
+$parent_dir/venv/bin/cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Release
 $parent_dir/venv/bin/cmake --build build --target kagome -j ${CMAKE_CORE_NUMBER}
