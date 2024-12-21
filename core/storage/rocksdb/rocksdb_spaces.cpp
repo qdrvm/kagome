351068db8df9f293182882f7482cea1e8e75439c
kamilsa <kamilsa16@gmail.com>
2024-12-21 10:58:59 +0500

    Revert fixes

--- core/storage/rocksdb/rocksdb_spaces.cpp
+++ core/storage/rocksdb/rocksdb_spaces.cpp
@@ -31,7 +31,7 @@ namespace kagome::storage {
     static const std::vector<std::string> names = []() {
       std::vector<std::string> names;
       names.push_back(rocksdb::kDefaultColumnFamilyName);
-      names.insert(names.end(), kNames.begin(), kNames.end());
+      names.insert(names.end(), names.begin(), names.end());
       return names;
     }();
     BOOST_ASSERT(space < Space::kTotal);
