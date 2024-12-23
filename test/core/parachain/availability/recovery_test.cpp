52759ed616a29056ce786390f199068a4722e479
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2024-12-23 18:30:17 +0800

    feature: additional tests for recovery available data

--- test/core/parachain/availability/recovery_test.cpp
+++ test/core/parachain/availability/recovery_test.cpp
@@ -8,6 +8,9 @@
 
 #include <gtest/gtest.h>
 
+#include <libp2p/crypto/ed25519_provider.hpp>
+#include <mock/core/crypto/session_keys_mock.hpp>
+
 #include "crypto/random_generator/boost_generator.hpp"
 #include "mock/core/application/chain_spec_mock.hpp"
 #include "mock/core/authority_discovery/query_mock.hpp"
@@ -30,6 +33,7 @@ using kagome::blockchain::BlockTreeMock;
 using kagome::common::Buffer;
 using kagome::crypto::BoostRandomGenerator;
 using kagome::crypto::HasherMock;
+using kagome::crypto::SessionKeysMock;
 using kagome::network::CandidateHash;
 using kagome::network::CandidateReceipt;
 using kagome::network::Chunk;
@@ -52,12 +56,14 @@ using kagome::runtime::ParachainHostMock;
 using kagome::runtime::SessionInfo;
 
 using kagome::parachain::makeTrieProof;
+using kagome::parachain::minChunks;
 using kagome::parachain::toChunks;
 
 using libp2p::PeerId;
 using libp2p::peer::PeerInfo;
 
 using testing::_;
+using testing::AnyNumber;
 using testing::Args;
 using testing::Invoke;
 using testing::Return;
@@ -71,14 +77,23 @@ class RecoveryTest : public testing::Test {
     testutil::prepareLoggers(soralog::Level::TRACE);
   }
 
-  void SetUp() override {
-    size_t data_size = 8;
-    original_data.resize(data_size);
-    random_generator.fillRandomly(original_data);
+  void prepareAvailableData(size_t data_size) {
+    original_available_data.pov.payload.resize(data_size);
+    random_generator.fillRandomly(original_available_data.pov.payload);
 
     original_chunks = toChunks(n_validators, original_available_data).value();
     receipt.descriptor.erasure_encoding_root = makeTrieProof(original_chunks);
 
+    required_chunk_number = minChunks(n_validators).value();
+    tolerance_chunk_number = original_chunks.size() - required_chunk_number;
+
+    auto candidate_hash = Recovery::HashedCandidateReceipt{receipt}.getHash();
+
+    EXPECT_CALL(*av_store, getChunk(candidate_hash, 1))
+        .WillRepeatedly(Return(original_chunks[1]));
+  }
+
+  void SetUp() override {
     session = SessionInfo{};
 
     chain_spec = std::make_shared<ChainSpecMock>();
@@ -102,10 +117,13 @@ class RecoveryTest : public testing::Test {
         }));
 
     av_store = std::make_shared<AvailabilityStoreMock>();
+
     query_audi = std::make_shared<QueryMock>();
 
     router = std::make_shared<RouterMock>();
     router->setReturningMockedProtocols();
+    EXPECT_CALL(*router, getFetchAvailableDataProtocol).Times(AnyNumber());
+    EXPECT_CALL(*router, getFetchChunkProtocol).Times(AnyNumber());
 
     auto fetch_available_data = router->getMockedFetchAvailableDataProtocol();
     ON_CALL(*fetch_available_data, doRequest(_, _, _))
@@ -129,6 +147,11 @@ class RecoveryTest : public testing::Test {
 
     peer_manager = std::make_shared<PeerManagerMock>();
 
+    session_keys = std::make_shared<SessionKeysMock>();
+    ON_CALL(*session_keys, getParaKeyPair(_))
+        .WillByDefault(Return(
+            std::pair(std::shared_ptr<kagome::crypto::Sr25519Keypair>{}, 1)));
+
     recovery = std::make_shared<RecoveryImpl>(chain_spec,
                                               hasher,
                                               block_tree,
@@ -136,7 +159,8 @@ class RecoveryTest : public testing::Test {
                                               av_store,
                                               query_audi,
                                               router,
-                                              peer_manager);
+                                              peer_manager,
+                                              session_keys);
 
     auto &val_group_0 = session.validator_groups.emplace_back();
     for (size_t i = 0; i < n_validators; ++i) {
@@ -167,10 +191,15 @@ class RecoveryTest : public testing::Test {
         std::optional<outcome::result<AvailableData>>)>>();
   }
 
+  void TearDown() override {
+    original_chunks.clear();
+  }
+
   BoostRandomGenerator random_generator;
 
-  size_t n_validators = 6;
-  Buffer original_data;
+  size_t n_validators = 10;
+  size_t required_chunk_number;
+  size_t tolerance_chunk_number;
   AvailableData original_available_data{};
   std::vector<kagome::network::ErasureChunk> original_chunks;
 
@@ -209,6 +238,7 @@ class RecoveryTest : public testing::Test {
   std::shared_ptr<QueryMock> query_audi;
   std::shared_ptr<RouterMock> router;
   std::shared_ptr<PeerManagerMock> peer_manager;
+  std::shared_ptr<SessionKeysMock> session_keys;
 
   std::shared_ptr<testing::MockFunction<void(
       std::optional<outcome::result<AvailableData>>)>>
@@ -218,6 +248,8 @@ class RecoveryTest : public testing::Test {
 };
 
 TEST_F(RecoveryTest, FullFromBakers_NoGroup) {
+  prepareAvailableData(2048);
+
   std::optional<GroupIndex> backing_group = std::nullopt;
   std::optional<CoreIndex> core = std::nullopt;  // value doesn't matter
 
@@ -229,6 +261,8 @@ TEST_F(RecoveryTest, FullFromBakers_NoGroup) {
 }
 
 TEST_F(RecoveryTest, FullFromBakers_Success) {
+  prepareAvailableData(2048);
+
   std::optional<GroupIndex> backing_group = 0;   // any non nullopt value
   std::optional<CoreIndex> core = std::nullopt;  // value doesn't matter
 
@@ -257,6 +291,8 @@ TEST_F(RecoveryTest, FullFromBakers_Success) {
 }
 
 TEST_F(RecoveryTest, SystematicChunks_NoCore) {
+  prepareAvailableData(2048);
+
   peer_state.req_chunk_version = kagome::network::ReqChunkVersion::V2;
   std::optional<GroupIndex> backing_group{};  // nullopt to skip full recovery
   std::optional<CoreIndex> core = std::nullopt;
@@ -296,6 +332,8 @@ TEST_F(RecoveryTest, SystematicChunks_NoCore) {
 }
 
 TEST_F(RecoveryTest, SystematicChunks_Success) {
+  prepareAvailableData(2048);
+
   peer_state.req_chunk_version = kagome::network::ReqChunkVersion::V2;
   std::optional<GroupIndex> backing_group{};  // nullopt to skip full recovery
   std::optional<CoreIndex> core = 0;
@@ -335,6 +373,8 @@ TEST_F(RecoveryTest, SystematicChunks_Success) {
 }
 
 TEST_F(RecoveryTest, RegularChunks_Success) {
+  prepareAvailableData(2048);
+
   peer_state.req_chunk_version = kagome::network::ReqChunkVersion::V2;
   std::optional<GroupIndex> backing_group{};  // nullopt to skip full recovery
   std::optional<CoreIndex> core = 0;
@@ -377,7 +417,55 @@ TEST_F(RecoveryTest, RegularChunks_Success) {
   ASSERT_EQ(available_data, original_available_data);
 }
 
-TEST_F(RecoveryTest, Failure) {
+TEST_F(RecoveryTest, CorruptedChunk) {
+  prepareAvailableData(2048);
+
+  peer_state.req_chunk_version = kagome::network::ReqChunkVersion::V2;
+  std::optional<GroupIndex> backing_group = 0;
+  std::optional<CoreIndex> core = 0;
+
+  std::optional<outcome::result<AvailableData>> available_data_res_opt;
+
+  EXPECT_CALL(*callback, Call(_))
+      .WillOnce(WithArgs<0>(
+          [&](std::optional<outcome::result<AvailableData>> x) mutable {
+            available_data_res_opt = std::move(x);
+          }));
+
+  recovery->recover(
+      receipt, session_index, backing_group, core, callback->AsStdFunction());
+
+  // Trying to recover full from bakers, but all return no-data
+  while (not fetch_available_data_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_available_data_requests.front();
+    cb(kagome::network::Empty{});
+    fetch_available_data_requests.pop();
+  }
+
+  // Trying to do chunks recovery, but one chunk has returned corrupted
+  while (not fetch_chunk_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_chunk_requests.front();
+    const auto &ec_chunk = original_chunks[req.chunk_index];
+    Chunk chunk{
+        .data = ec_chunk.chunk,
+        .chunk_index = ec_chunk.index,
+        .proof = ec_chunk.proof,
+    };
+    if (chunk.chunk_index < tolerance_chunk_number) {
+      chunk.data = {"corrupted chunk"_bytes};
+    }
+    cb(chunk);
+    fetch_chunk_requests.pop();
+  }
+
+  testing::Mock::VerifyAndClear(callback.get());
+
+  ASSERT_TRUE(available_data_res_opt.has_value());
+}
+
+TEST_F(RecoveryTest, InsufficientChunks) {
+  prepareAvailableData(2048);
+
   peer_state.req_chunk_version = kagome::network::ReqChunkVersion::V2;
   std::optional<GroupIndex> backing_group = 0;
   std::optional<CoreIndex> core = 0;
@@ -404,7 +492,7 @@ TEST_F(RecoveryTest, Failure) {
   while (not fetch_chunk_requests.empty()) {
     auto &[peer_id, req, cb] = fetch_chunk_requests.front();
     const auto &ec_chunk = original_chunks[req.chunk_index];
-    if (ec_chunk.index == 0) {
+    if (ec_chunk.index < required_chunk_number - 1) {
       Chunk chunk{
           .data = ec_chunk.chunk,
           .chunk_index = ec_chunk.index,
@@ -420,4 +508,215 @@ TEST_F(RecoveryTest, Failure) {
   testing::Mock::VerifyAndClear(callback.get());
 
   ASSERT_FALSE(available_data_res_opt.has_value());
+}
+
+TEST_F(RecoveryTest, DelayedChunks) {
+  prepareAvailableData(2048);
+
+  peer_state.req_chunk_version = kagome::network::ReqChunkVersion::V2;
+  std::optional<GroupIndex> backing_group = 0;
+  std::optional<CoreIndex> core = 0;
+
+  std::optional<outcome::result<AvailableData>> available_data_res_opt;
+
+  recovery->recover(
+      receipt, session_index, backing_group, core, callback->AsStdFunction());
+
+  // Trying to recover full from bakers, but all return no-data
+  while (not fetch_available_data_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_available_data_requests.front();
+    cb(kagome::network::Empty{});
+    fetch_available_data_requests.pop();
+  }
+
+  size_t handled_counter = 0;
+
+  EXPECT_CALL(*callback, Call(_)).Times(0);
+
+  // Trying to do chunks recovery, but not enough number chunks
+  while (not fetch_chunk_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_chunk_requests.front();
+    const auto &ec_chunk = original_chunks[req.chunk_index];
+    Chunk chunk{
+        .data = ec_chunk.chunk,
+        .chunk_index = ec_chunk.index,
+        .proof = ec_chunk.proof,
+    };
+    cb(chunk);
+    fetch_chunk_requests.pop();
+
+    // Stop responding for required-1 for emulation of delay)
+    if (++handled_counter == required_chunk_number - 1) {
+      break;
+    }
+  }
+
+  // Actually stopped for emulation of delay
+  ASSERT_EQ(handled_counter, required_chunk_number - 1);
+
+  // Some requests still active (delayed)
+  ASSERT_FALSE(fetch_chunk_requests.empty());
+
+  // Available data still is not reconstructed
+  ASSERT_FALSE(available_data_res_opt.has_value());
+
+  testing::Mock::VerifyAndClear(callback.get());
+
+  EXPECT_CALL(*callback, Call(_))
+      .WillOnce(WithArgs<0>(
+          [&](std::optional<outcome::result<AvailableData>> x) mutable {
+            available_data_res_opt = std::move(x);
+          }));
+
+  while (not fetch_chunk_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_chunk_requests.front();
+    const auto &ec_chunk = original_chunks[req.chunk_index];
+    Chunk chunk{
+        .data = ec_chunk.chunk,
+        .chunk_index = ec_chunk.index,
+        .proof = ec_chunk.proof,
+    };
+    cb(chunk);
+    fetch_chunk_requests.pop();
+  }
+
+  testing::Mock::VerifyAndClear(callback.get());
+
+  ASSERT_TRUE(available_data_res_opt.has_value());
+}
+
+TEST_F(RecoveryTest, DuplicateChunk) {
+  prepareAvailableData(2048);
+
+  peer_state.req_chunk_version = kagome::network::ReqChunkVersion::V2;
+  std::optional<GroupIndex> backing_group = 0;
+  std::optional<CoreIndex> core = 0;
+
+  std::optional<outcome::result<AvailableData>> available_data_res_opt;
+
+  EXPECT_CALL(*callback, Call(_))
+      .WillOnce(WithArgs<0>(
+          [&](std::optional<outcome::result<AvailableData>> x) mutable {
+            available_data_res_opt = std::move(x);
+          }));
+
+  recovery->recover(
+      receipt, session_index, backing_group, core, callback->AsStdFunction());
+
+  // Trying to recover full from bakers, but all return no-data
+  while (not fetch_available_data_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_available_data_requests.front();
+    cb(kagome::network::Empty{});
+    fetch_available_data_requests.pop();
+  }
+
+  // Trying to do chunks recovery, but for
+  // three first chunks returned the same one
+  while (not fetch_chunk_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_chunk_requests.front();
+    const auto &ec_chunk =
+        original_chunks[req.chunk_index < tolerance_chunk_number
+                            ? 0
+                            : req.chunk_index];
+    Chunk chunk{
+        .data = ec_chunk.chunk,
+        .chunk_index = ec_chunk.index,
+        .proof = ec_chunk.proof,
+    };
+    cb(chunk);
+    fetch_chunk_requests.pop();
+  }
+
+  testing::Mock::VerifyAndClear(callback.get());
+
+  ASSERT_TRUE(available_data_res_opt.has_value());
+}
+
+TEST_F(RecoveryTest, FailureHandling) {
+  prepareAvailableData(2048);
+
+  peer_state.req_chunk_version = kagome::network::ReqChunkVersion::V2;
+  std::optional<GroupIndex> backing_group = 0;
+  std::optional<CoreIndex> core = 0;
+
+  std::optional<outcome::result<AvailableData>> available_data_res_opt;
+
+  EXPECT_CALL(*callback, Call(_))
+      .WillOnce(WithArgs<0>(
+          [&](std::optional<outcome::result<AvailableData>> x) mutable {
+            available_data_res_opt = std::move(x);
+          }));
+
+  recovery->recover(
+      receipt, session_index, backing_group, core, callback->AsStdFunction());
+
+  // Trying to recover full from bakers, but all return no-data
+  while (not fetch_available_data_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_available_data_requests.front();
+    cb(kagome::network::Empty{});
+    fetch_available_data_requests.pop();
+  }
+
+  size_t handled_counter = 0;
+
+  // Trying to do chunks recovery, but tolerated number of requests are failed
+  while (not fetch_chunk_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_chunk_requests.front();
+    if (handled_counter++ < tolerance_chunk_number) {
+      cb(kagome::network::ProtocolError::GONE);
+    } else {
+      const auto &ec_chunk = original_chunks[req.chunk_index];
+      Chunk chunk{
+          .data = ec_chunk.chunk,
+          .chunk_index = ec_chunk.index,
+          .proof = ec_chunk.proof,
+      };
+      cb(chunk);
+    }
+    fetch_chunk_requests.pop();
+  }
+
+  testing::Mock::VerifyAndClear(callback.get());
+
+  ASSERT_TRUE(available_data_res_opt.has_value());
+}
+
+TEST_F(RecoveryTest, LargeScaleRecovery) {
+  prepareAvailableData(2 << 20);  // 2Mb
+
+  std::optional<GroupIndex> backing_group = 0;   // any non nullopt value
+  std::optional<CoreIndex> core = std::nullopt;  // value doesn't matter
+
+  std::optional<outcome::result<AvailableData>> available_data_res_opt;
+
+  EXPECT_CALL(*callback, Call(_))
+      .WillOnce(WithArgs<0>(
+          [&](std::optional<outcome::result<AvailableData>> x) mutable {
+            available_data_res_opt = std::move(x);
+          }));
+
+  recovery->recover(
+      receipt, session_index, backing_group, core, callback->AsStdFunction());
+
+  // We must not try to obtain big data from backers
+  EXPECT_TRUE(fetch_available_data_requests.empty());
+
+  // Trying to do systematic chunks recovery
+  while (not fetch_chunk_requests.empty()) {
+    auto &[peer_id, req, cb] = fetch_chunk_requests.front();
+    const auto &ec_chunk = original_chunks[req.chunk_index];
+    Chunk chunk{
+        .data = ec_chunk.chunk,
+        .chunk_index = ec_chunk.index,
+        .proof = ec_chunk.proof,
+    };
+    cb(chunk);
+    fetch_chunk_requests.pop();
+  }
+
+  testing::Mock::VerifyAndClear(callback.get());
+
+  ASSERT_TRUE(available_data_res_opt.has_value());
+  ASSERT_OUTCOME_SUCCESS(available_data, available_data_res_opt.value());
+  ASSERT_EQ(available_data, original_available_data);
 }
