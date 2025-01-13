48bd5ec1a950a1b6b7c78a4e89d6f68567e884ff
turuslan <turuslan.devbox@gmail.com>
2025-01-13 19:37:41 +0500

    wasmedge compile tmp
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- core/utils/write_file.hpp
+++ core/utils/write_file.hpp
@@ -8,7 +8,9 @@
 
 #include <boost/filesystem/operations.hpp>
 #include <fstream>
+#include <optional>
 #include <qtils/bytestr.hpp>
+#include <qtils/option_take.hpp>
 #include <qtils/outcome.hpp>
 
 namespace kagome {
@@ -26,20 +28,44 @@ namespace kagome {
     return writeFile(path, qtils::byte2str(data));
   }
 
+  struct TmpFile {
+    static outcome::result<TmpFile> make(std::filesystem::path path) {
+      boost::system::error_code ec;
+      auto tmp =
+          boost::filesystem::unique_path(path.native() + ".%%%%", ec).native();
+      if (ec) {
+        return ec;
+      }
+      return TmpFile{std::move(path), std::move(tmp)};
+    }
+
+    TmpFile(std::filesystem::path target, std::filesystem::path tmp)
+        : target{std::move(target)}, tmp{std::move(tmp)} {}
+
+    std::filesystem::path path() const {
+      return tmp.value_or(target);
+    }
+
+    outcome::result<void> rename() {
+      if (auto tmp = qtils::optionTake(this->tmp)) {
+        std::error_code ec;
+        std::filesystem::rename(*tmp, target, ec);
+        if (ec) {
+          return ec;
+        }
+      }
+      return outcome::success();
+    }
+
+    std::filesystem::path target;
+    std::optional<std::filesystem::path> tmp;
+  };
+
   outcome::result<void> writeFileTmp(const std::filesystem::path &path,
                                      auto &&data) {
-    boost::system::error_code ec1;
-    auto tmp =
-        boost::filesystem::unique_path(path.native() + ".%%%%", ec1).native();
-    if (ec1) {
-      return ec1;
-    }
-    OUTCOME_TRY(writeFile(tmp, data));
-    std::error_code ec2;
-    std::filesystem::rename(tmp, path, ec2);
-    if (ec2) {
-      return ec2;
-    }
+    OUTCOME_TRY(tmp, TmpFile::make(path));
+    OUTCOME_TRY(writeFile(tmp.path(), data));
+    OUTCOME_TRY(tmp.rename());
     return outcome::success();
   }
 }  // namespace kagome
