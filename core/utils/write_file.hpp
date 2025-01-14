e829f8d248feebfd5e0d5f60c7a6e23a11e6bb95
turuslan <turuslan.devbox@gmail.com>
2025-01-14 14:02:59 +0500

    comments
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- core/utils/write_file.hpp
+++ core/utils/write_file.hpp
@@ -29,7 +29,17 @@ namespace kagome {
     return writeFile(path, qtils::byte2str(data));
   }
 
+  /**
+   * Wrapper to generate tmp file name and rename it later.
+   * Concurrent writers would corrupt file, so they write to tmp file first, and
+   * atomically rename it after file is completely written.
+   */
   struct TmpFile {
+    /**
+     * Generate tmp file name for path.
+     * Tmp file is created in same directory as target path,
+     * to avoid `EXDEV` error from `rename`.
+     */
     static outcome::result<TmpFile> make(std::filesystem::path path) {
       boost::system::error_code ec;
       auto tmp =
@@ -43,10 +53,16 @@ namespace kagome {
     TmpFile(std::filesystem::path target, std::filesystem::path tmp)
         : target{std::move(target)}, tmp{std::move(tmp)} {}
 
+    /**
+     * Get current file path.
+     */
     std::filesystem::path path() const {
       return tmp.value_or(target);
     }
 
+    /**
+     * Rename file to target name.
+     */
     outcome::result<void> rename() {
       if (auto tmp = qtils::optionTake(this->tmp)) {
         std::error_code ec;
