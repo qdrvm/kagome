500a40edda6dff9bea16790390a9f65103aebdbd
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-02-06 21:54:21 +0800

    refactor: explicit using of Tagged
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- core/consensus/grandpa/impl/environment_impl.cpp
+++ core/consensus/grandpa/impl/environment_impl.cpp
@@ -421,7 +421,7 @@ namespace kagome::consensus::grandpa {
   outcome::result<void> EnvironmentImpl::finalize(
       VoterSetId id, const GrandpaJustification &grandpa_justification) {
     auto voters_res = authority_manager_->authorities(
-        grandpa_justification.block_info, false);
+        grandpa_justification.block_info, IsBlockFinalized(false));
     if (not voters_res) {
       return VotingRoundError::NO_KNOWN_AUTHORITIES_FOR_BLOCK;
     }
