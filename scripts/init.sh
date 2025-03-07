db43c8dc61e540ac2eaaa87b10c965631f02e5ce
Robert Hambrock <roberthambrock@gmail.com>
2025-03-07 11:11:05 +0100

    libncurses6-dev -> libncurses-dev

--- scripts/init.sh
+++ scripts/init.sh
@@ -17,7 +17,7 @@ if [ -z "$FLAKE_INITIATED" ]; then
     apt update
     apt install --no-install-recommends -y \
         build-essential git gcc ca-certificates python-is-python3 python3-pip \
-        python3-venv curl libgmp-dev libncurses6 libncurses6-dev libnsl-dev libseccomp-dev
+        python3-venv curl libgmp-dev libncurses6 libncurses-dev libnsl-dev libseccomp-dev
 fi
 
 python3 -m venv "$parent_dir/venv"
