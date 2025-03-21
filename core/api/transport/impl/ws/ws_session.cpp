c7e0df1560af34d313e558b0f7c8b0c8f642dc08
Boris Erakhtin <boris.erakhtin@gmail.com>
2025-03-21 15:39:48 +0500

    Boost error code is used istead of exceptions in isUnsafeAllowed

--- core/api/transport/impl/ws/ws_session.cpp
+++ core/api/transport/impl/ws/ws_session.cpp
@@ -246,18 +246,21 @@ namespace kagome::api {
   }
 
   bool WsSession::isUnsafeAllowed() const {
-    try {
-      auto &socket = stream_.next_layer().next_layer().socket();
-      if (not socket.is_open()) {
-        return false;
-      }
-      return allow_unsafe_.allow(socket.remote_endpoint());
-    } catch (const boost::system::system_error &e) {
-      // If remote_endpoint() fails (e.g., socket not connected), default to not
-      // allowing unsafe
-      SL_WARN(logger_, "Failed to get remote endpoint: {}", e.what());
+    auto &socket = stream_.next_layer().next_layer().socket();
+    if (not socket.is_open()) {
       return false;
     }
+
+    boost::system::error_code ec;
+    auto endpoint = socket.remote_endpoint(ec);
+    if (ec) {
+      // If remote_endpoint() fails (e.g., socket not connected), default to not
+      // allowing unsafe
+      SL_WARN(logger_, "Failed to get remote endpoint: {}", ec.message());
+      return false;
+    }
+
+    return allow_unsafe_.allow(endpoint);
   }
 
   std::shared_ptr<WsSessionImpl> WsSession::sessionMake() {
