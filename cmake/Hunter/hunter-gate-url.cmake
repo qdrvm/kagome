745fbfe0a290ccd7069fbf30d3fcf3431d6622f2
turuslan <turuslan.devbox@gmail.com>
2025-03-24 11:33:06 +0500

    update hunter
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-    URL  https://github.com/qdrvm/hunter/archive/4ee216b3a5dcf43f20f7505f31d403e12d4e48b1.zip
-    SHA1 8b703e87ae473b97b5e2bc9523c0b8b7d7af1617
+    URL  https://github.com/qdrvm/hunter/archive/86e53c752977bf5a5efb6590c26941ed3fec8187.zip
+    SHA1 7121f2cbf052cc6cb0a526a89e9b4aca5bf3cd54
     LOCAL
 )
