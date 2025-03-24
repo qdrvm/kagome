ca252414a86c2534f0e220e02673aa5e0fd6aad7
turuslan <turuslan.devbox@gmail.com>
2025-03-24 11:03:17 +0500

    cmake_minimum_required 3.12
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-    URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm33.tar.gz
-    SHA1 aaccde981d2772511f0c32c3dc3f52f681956949
+    URL  https://github.com/qdrvm/hunter/archive/4ee216b3a5dcf43f20f7505f31d403e12d4e48b1.zip
+    SHA1 8b703e87ae473b97b5e2bc9523c0b8b7d7af1617
     LOCAL
 )
