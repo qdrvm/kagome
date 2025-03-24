931f969dd5381fb02a6b228581e5da701fd96807
kamilsa <kamilsa16@gmail.com>
2025-03-24 18:48:29 +0500

    Update hunter with new libp2p (#2419)

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-    URL  https://github.com/qdrvm/hunter/archive/86e53c752977bf5a5efb6590c26941ed3fec8187.zip
-    SHA1 7121f2cbf052cc6cb0a526a89e9b4aca5bf3cd54
+    URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm34.zip
+    SHA1 52ff3d2ce0c4290c874c31bf0621f852590fc239
     LOCAL
 )
