40a956ba61d5c9395ae328fb926eb591efc2d9f9
Ruslan Tushov <turuslan@users.noreply.github.com>
2025-03-25 11:35:19 +0500

    recursive mutex (#2421)
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>
    (cherry picked from commit dfc30a37944862f9f66fb0fc463142ed931461e5)

--- core/network/impl/peer_manager_impl.cpp
+++ core/network/impl/peer_manager_impl.cpp
@@ -190,7 +190,7 @@ namespace kagome::network {
           self->disconnectFromPeer(peer_id);
           return;
         }
-        self->processFullyConnectedPeer(lock, peer_id);
+        self->processFullyConnectedPeer(peer_id);
       }
     });
 
@@ -437,7 +437,7 @@ namespace kagome::network {
       if (remote_peer_id == peer_id) {
         SL_DEBUG(self->log_, "Connected to peer {}", peer_id);
 
-        self->processFullyConnectedPeer(lock, peer_id);
+        self->processFullyConnectedPeer(peer_id);
       }
     });
   }
@@ -572,8 +572,7 @@ namespace kagome::network {
              queue_to_connect_.size());
   }
 
-  void PeerManagerImpl::processFullyConnectedPeer(
-      std::unique_lock<std::mutex> &lock, const PeerId &peer_id) {
+  void PeerManagerImpl::processFullyConnectedPeer(const PeerId &peer_id) {
     // Skip connection to itself
     if (isSelfPeer(peer_id)) {
       connecting_peers_.erase(peer_id);
@@ -606,7 +605,6 @@ namespace kagome::network {
     peer_event_engine_->notify(primitives::events::PeerEventType::kConnected,
                                peer_info.id);
 
-    lock.unlock();
     auto addresses_res =
         host_.getPeerRepository().getAddressRepository().getAddresses(peer_id);
     if (addresses_res.has_value()) {
