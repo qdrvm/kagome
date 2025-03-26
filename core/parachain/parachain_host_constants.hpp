0bfb9a11f50429ee7f0fc25c0c38b8125b0396be
Boris <31869190+ErakhtinB@users.noreply.github.com>
2025-03-26 14:03:46 +0500

    Reference to polkadot-sdk DEFAULT_SCHEDULING_LOOKAHEAD

--- core/parachain/parachain_host_constants.hpp
+++ core/parachain/parachain_host_constants.hpp
@@ -12,5 +12,6 @@ namespace kagome::parachain {
   // Define the scheduling lookahead constant to match Polkadot SDK's default
   // This is the number of blocks ahead/behind in the relay chain that we look
   // for scheduling parachain blocks
+  // https://github.com/paritytech/polkadot-sdk/blob/1d5d8aac07d41f3c2aa57a80562af7955dcc8af3/polkadot/primitives/src/v8/mod.rs#L460
   constexpr uint32_t DEFAULT_SCHEDULING_LOOKAHEAD = 3;
 }  // namespace kagome::parachain
