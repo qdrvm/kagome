f7fcb6694e417573b3af6bc48e4de115547c79d0
kamilsa <kamilsa16@gmail.com>
2025-04-24 11:02:29 +0300

    Update to hunter with updated default zlib

--- cmake/Hunter/hunter-gate-url.cmake
+++ cmake/Hunter/hunter-gate-url.cmake
@@ -1,5 +1,5 @@
 HunterGate(
-    URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm35.zip
-    SHA1 b6ecd27a51298f8beeaed4835b9002ec18b5ea21
+    URL  https://github.com/qdrvm/hunter/archive/3f8542a13b91d9da8c39400955ebda924ae8f00d.tar.gz
+    SHA1 c8648ca9696b2f957db67acb1a2478bc443a557f
     LOCAL
 )
