b3fe3968e87d7a9eb281aa6bb2251fcb1f5797d8
iceseer <iceseer@gmail.com>
2025-05-19 13:10:40 +0300

    remove collations
    Signed-off-by: iceseer <iceseer@gmail.com>

--- core/parachain/validator/impl/parachain_observer_impl.cpp
+++ core/parachain/validator/impl/parachain_observer_impl.cpp
@@ -131,8 +131,8 @@ namespace kagome::parachain {
       primitives::BlockHash relay_parent,
       std::optional<std::pair<CandidateHash, Hash>> &&prospective_candidate,
       network::CollationVersion collator_protocol_version) {
-    processor_->handle_advertisement(
-        relay_parent, peer_id, std::move(prospective_candidate));
+    // processor_->handle_advertisement(
+    //     relay_parent, peer_id, std::move(prospective_candidate));
   }
 
   void ParachainObserverImpl::onDeclare(const libp2p::peer::PeerId &peer_id,
