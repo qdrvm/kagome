369cbf2c50627f5041d025bcf8a07d528ff2e2a3
kamilsa <kamilsa16@gmail.com>
2025-06-18 09:40:33 +0500

    [feat] add mock method for retrieving AUDI key pair

--- test/mock/core/crypto/session_keys_mock.hpp
+++ test/mock/core/crypto/session_keys_mock.hpp
@@ -43,6 +43,11 @@ namespace kagome::crypto {
                 (const std::vector<primitives::AuthorityDiscoveryId> &),
                 (override));
 
+    MOCK_METHOD(std::optional<Sr25519Keypair>,
+                getAudiKeyPair,
+                (),
+                (override));
+
     MOCK_METHOD(KeypairWithIndexOpt<EcdsaKeypair>,
                 getBeefKeyPair,
                 (const std::vector<EcdsaPublicKey> &),
